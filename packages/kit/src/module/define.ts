6b8af6121f9c9ba1d4597e3a58fa5682bbf43702
Pooya Parsa <pyapar@gmail.com>
2021-04-02 13:31:47 +0200

    fix type inference issues in defineNuxtModule

--- packages/kit/src/module/define.ts
+++ packages/kit/src/module/define.ts
@@ -4,36 +4,41 @@ import { useNuxt, nuxtCtx } from '../nuxt'
 import type { Nuxt } from '../types/nuxt'
 import type { NuxtModule, LegacyNuxtModule, ModuleOptions } from '../types/module'
 
-export function defineNuxtModule<OptionsT extends ModuleOptions> (m: NuxtModule<OptionsT> | ((nuxt: Nuxt) => NuxtModule<OptionsT>)): LegacyNuxtModule {
+export function defineNuxtModule<OptionsT extends ModuleOptions> (input: NuxtModule<OptionsT> | ((nuxt: Nuxt) => NuxtModule<OptionsT>)): LegacyNuxtModule {
+  let mod: NuxtModule<OptionsT>
+
   function wrappedModule (inlineOptions: OptionsT) {
     // Get nuxt context
     const nuxt: Nuxt = this.nuxt || useNuxt()
 
     // Resolve function
-    if (typeof m === 'function') {
-      m = m(nuxt)
+    if (typeof input === 'function') {
+      const fn = input
+      mod = nuxtCtx.call(nuxt, () => fn(nuxt))
+    } else {
+      mod = input
     }
 
     // Install hooks
-    if (m.hooks) {
-      nuxt.hooks.addHooks(m.hooks)
+    if (mod.hooks) {
+      nuxt.hooks.addHooks(mod.hooks)
     }
 
     // Stop if no install provided
-    if (typeof m.install !== 'function') {
+    if (typeof mod.install !== 'function') {
       return
     }
 
     // Resolve options
-    const configKey = m.configKey || m.name
+    const configKey = mod.configKey || mod.name
     const userOptions = defu(inlineOptions, nuxt.options[configKey]) as OptionsT
-    const resolvedOptions = applyDefaults(m.defaults as any, userOptions) as OptionsT
+    const resolvedOptions = applyDefaults(mod.defaults as any, userOptions) as OptionsT
 
     // Call install
-    return nuxtCtx.call(nuxt, () => m.install.call(null, resolvedOptions, nuxt))
+    return nuxtCtx.call(nuxt, () => mod.install.call(null, resolvedOptions, nuxt))
   }
 
-  wrappedModule.meta = m
+  wrappedModule.meta = mod
 
   return wrappedModule
 }
