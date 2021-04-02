3a0562c2d34a5ecf045120f8b3c112057e8c2e34
Sébastien Chopin <seb@nuxtjs.com>
2021-04-03 01:13:53 +0200

    chore(playground): add series async data page

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -5,11 +5,24 @@
     </h2>
     <strong>Playground pages</strong>
     <ul>
-      <li>
-        <nuxt-link to="/composables/asyncdata">
-          asyncdata
+      <li v-for="link of links" :key="link">
+        <nuxt-link :to="link">
+          {{ link }}
         </nuxt-link>
       </li>
     </ul>
   </div>
 </template>
+
+<script>
+import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
+
+export default defineComponent({
+  setup () {
+    const links = useRouter().getRoutes().filter(route => ['index', '404'].includes(route.name) === false).map(route => route.path)
+
+    return { links }
+  }
+})
+</script>
