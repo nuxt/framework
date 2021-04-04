953baf82d7e35514fa13b8e92448f728c17e020e
Daniel Roe <daniel@roe.dev>
2021-04-04 23:08:56 +0100

    fix: avoid double calling set payload

--- packages/app/src/composables/hydrate.ts
+++ packages/app/src/composables/hydrate.ts
@@ -15,12 +15,7 @@ export const useHydration = <T>(key: string, get: () => T, set: (value: T) => vo
     })
   }
 
-  if (process.client) {
-    if (nuxt.isHydrating) {
-      set(nuxt.payload[key])
-    }
-    nuxt.hooks.hook('app:created', () => {
-      set(nuxt.payload[key])
-    })
+  if (process.client && nuxt.isHydrating) {
+    set(nuxt.payload[key])
   }
 }
