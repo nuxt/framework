abc2ff134ae1c04bda84a498879b954bf63b8286
Pooya Parsa <pyapar@gmail.com>
2021-04-06 00:04:58 +0200

    wip

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,37 +1,79 @@
-import { resolve } from 'path'
+import 'v8-compile-cache'
+import * as c from 'colorette'
+import mri from 'mri'
+import { listen } from 'listhen'
+import { version } from '../package.json'
+
+const commands = {
+  dev: {},
+  build: {}
+}
 
 async function _main () {
-  const args = process.argv.splice(2)
-  const cmd = args[0]
+  const _argv = process.argv.slice(2)
+  const args = mri(_argv)
+  const [command, rootDir] = args._
 
-  if (!['dev', 'build'].includes(cmd)) {
-    // eslint-disable-next-line no-console
-    console.error('Usage nuxt dev|build [rootDir]')
+  if (!(command in commands)) {
+    usage()
     process.exit(1)
   }
 
-  const isDev = cmd === 'dev'
-  const rootDir = resolve(process.cwd(), args[1] || '.')
+  const isDev = command === 'dev'
+
+  let handler
+  let listenerP
+  if (isDev) {
+    handler = (_req, res) => {
+      res.setHeader('Content-Type', 'text/html; charset=UTF-8')
+      res.end('<!DOCTYPE html><html><head><meta http-equiv="refresh" content="1"><head><body>...')
+    }
+    // https://github.com/unjs/listhen
+    listenerP = listen((req, res) => { handler(req, res) }, { clipboard: true })
+  }
 
   const { loadNuxt, build } = await import('nuxt3')
-
   const nuxt = await loadNuxt({
     for: isDev ? 'dev' : 'build',
     rootDir
   })
 
   if (isDev) {
-    // https://github.com/nuxt-contrib/listhen
-    await nuxt.server.listen(3000, { name: 'Nuxt' })
+    handler = nuxt.server.app
   }
 
+  printTime('Starting build')
   await build(nuxt)
+
+  const listener = await listenerP
+  printTime('Ready')
+  listener.open().catch(() => {})
+
+  return Promise.resolve()
+}
+
+function usage () {
+  console.log(`Usage: ${c.cyan(`nu ${Object.keys(commands).join('|')} [<rootDir>] [args]`)}`)
+}
+
+function measureTime () {
+  if (!('_startTime' in process)) {
+    return 0
+  }
+  // @ts-ignore
+  return (Date.now() - process._startTime as number)
+}
+
+function printTime (pointName: string) {
+  console.log(c.gray((`⧗ ${pointName} after ${c.bold(measureTime() + 'ms')}`)))
+}
+
+function onFatalError (err) {
+  console.error(err)
+  process.exit(1)
 }
 
 export function main () {
-  _main()
-    .catch((error) => {
-      require('consola').fatal(error)
-      require('exit')(2)
-    })
+  console.log(c.green(`Nuxt CLI v${version}\n`))
+  _main().catch(onFatalError)
 }
