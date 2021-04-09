7b2d4994276cfe418bbc6c19bbcb48924dcb8d17
Daniel Roe <daniel@roe.dev>
2021-04-09 15:23:01 +0100

    fix: instantiate all elements within plugin

--- examples/vue-apollo/plugins/apollo.js
+++ examples/vue-apollo/plugins/apollo.js
@@ -2,18 +2,15 @@ import { DefaultApolloClient } from '@vue/apollo-composable'
 
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
 
-const httpLink = createHttpLink({
-  uri: 'http://localhost:3020/graphql'
-})
-
-const cache = new InMemoryCache()
-
-const apolloClient = new ApolloClient({
-  link: httpLink,
-  cache
-})
-
 export default ({ app }) => {
+  const httpLink = createHttpLink({
+    uri: '/graphql'
+  })
+  const cache = new InMemoryCache()
+  const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache
+  })
   // https://v3.vuejs.org/api/application-api.html#provide
   app.provide(DefaultApolloClient, apolloClient)
 }
