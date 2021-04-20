7ff9f25c9cc7dd3dffea8bb416c57ff55e1d6231
Alexander Lichter <github@lichter.io>
2021-04-20 12:18:03 +0200

    fix(nitro): replace optional chaining due to CF preset

--- packages/nitro/src/runtime/app/vue2.ts
+++ packages/nitro/src/runtime/app/vue2.ts
@@ -8,7 +8,8 @@ const __VUE_SSR_CONTEXT__ = global.__VUE_SSR_CONTEXT__ = {}
 export function renderToString (component, context) {
   return new Promise((resolve, reject) => {
     _renderer.renderToString(component, context, (err, result) => {
-      const styles = [__VUE_SSR_CONTEXT__, context].map(c => c?._styles?.default).filter(Boolean)
+      // No optional chaining because it breaks CF workers.
+      const styles = [__VUE_SSR_CONTEXT__, context].map(c => c && c._styles && c._styles.default).filter(Boolean)
       if (!context._styles) { context._styles = {} }
       context._styles.default = {
         ids: [...styles.map(s => s.ids)],
