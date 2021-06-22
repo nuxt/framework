310074766a5de0a70f9ce4eb7115d582736bafec
Pooya Parsa <pyapar@gmail.com>
2021-06-22 14:27:36 +0200

    test: use esbuild for jest transforms

---
+++ scripts/jest-transform.mjs
@@ -0,0 +1,15 @@
+import { transformSync } from 'esbuild'
+
+// https://jestjs.io/docs/next/code-transformation
+export default {
+  process (src, path, _opts) {
+    const r = transformSync(src, {
+      target: 'node14',
+      format: 'cjs',
+      sourcefile: path,
+      loader: path.endsWith('.ts') ? 'ts' : 'default'
+    })
+    r.code = r.code.replace(/import\(/g, 'require(')
+    return r
+  }
+}
