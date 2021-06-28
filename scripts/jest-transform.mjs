f839ff556507d913e72a8759f79e6b84f5c1f784
Daniel Roe <daniel@roe.dev>
2021-06-28 17:54:09 +0100

    chore: fix jest transform parser to keep track of brackets

--- scripts/jest-transform.mjs
+++ scripts/jest-transform.mjs
@@ -2,6 +2,9 @@ import { transformSync } from 'esbuild'
 
 // https://jestjs.io/docs/next/code-transformation
 export default {
+  getCacheKey () {
+    return String(Math.random())
+  },
   process (src, path, _opts) {
     const r = transformSync(src, {
       target: 'node14',
@@ -9,7 +12,25 @@ export default {
       sourcefile: path,
       loader: path.endsWith('.ts') ? 'ts' : 'default'
     })
-    r.code = r.code.replace(/import\((.*)\)/g, (_, id) => `Promise.resolve(require(${id}))`)
+    r.code = r.code.replace(/import(\(.*\))/g, (_, id) => {
+      let openBrackets = 0
+
+      for (let pos = 0; pos < id.length; pos++) {
+        const char = id[pos]
+        switch (char) {
+          case '(':
+            openBrackets++
+            break
+          case ')':
+            openBrackets--
+            if (!openBrackets) {
+              return 'Promise.resolve(require' + id.slice(0, pos) + ')' + id.slice(pos)
+            }
+            break
+        }
+      }
+      return 'Promise.resolve(require' + id + ')'
+    })
     return r
   }
 }
