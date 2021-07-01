880e3223cf93e0ed4f663d1c8e0856545699a3a8
Pooya Parsa <pyapar@gmail.com>
2021-07-01 12:25:50 +0200

    fix script

--- scripts/release-edge.sh
+++ scripts/release-edge.sh
@@ -13,11 +13,12 @@ yarn
 # yarn build
 
 # Update token
-if [[ ! -z NODE_AUTH_TOKEN}" ]] ; then
-echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" >> ~/.npmrc
-echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
-echo "always-auth=true" >> ~/.npmrc
-npm whoami
+if [[ ! -z ${NODE_AUTH_TOKEN} ]] ; then
+  echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" >> ~/.npmrc
+  echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
+  echo "always-auth=true" >> ~/.npmrc
+  npm whoami
+fi
 
 # Release packages
 for p in packages/* ; do
