4b3af0eb4abb0a18efef1475f757a0b8ad05f87f
Sébastien Chopin <seb@nuxtjs.com>
2021-07-07 01:43:20 +0200

    docs: add GH auth for private beta
    WIP

--- docs/src/docus/nuxt.config.ts
+++ docs/src/docus/nuxt.config.ts
@@ -1,5 +1,26 @@
 import { withDocus } from 'docus'
+import { createAuthMiddleware } from 'ezpass'
 
 export default withDocus({
-  rootDir: __dirname
+  rootDir: __dirname,
+  serverMiddleware: [
+    createAuthMiddleware({
+      // enabled: process.env.NODE_ENV === 'production',
+      enabled: true,
+      provider: 'github',
+      providerOptions: {
+        clientId: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET
+      },
+      sessionSecret: process.env.SESSION_SECRET
+    }),
+    function (req, _res, next) {
+      if (!req.auth) {
+        return next()
+      }
+      // TODO: check if the user is listed as a collaborator
+      console.log('req.auth.session.user', req.auth.session.user)
+      next()
+    }
+  ]
 })
