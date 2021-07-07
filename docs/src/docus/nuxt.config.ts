b99f1b833e6ac6cb5c6d3bfc7255ed1025802fa8
Sébastien Chopin <seb@nuxtjs.com>
2021-07-07 13:51:46 +0200

    chore: add GitHub collaborators check

--- docs/src/docus/nuxt.config.ts
+++ docs/src/docus/nuxt.config.ts
@@ -1,26 +1,48 @@
+import { readFileSync } from 'fs'
 import { withDocus } from 'docus'
 import { createAuthMiddleware } from 'ezpass'
+import { Octokit } from '@octokit/rest'
+
+const authEnabled = process.env.NODE_ENV === 'production'
+if (authEnabled) {
+  if (!process.env.GITHUB_CLIENT_ID) { throw new Error('Please define a GITHUB_CLIENT_ID in `.env` to add GitHub auth') }
+  if (!process.env.GITHUB_CLIENT_SECRET) { throw new Error('Please define a GITHUB_CLIENT_SECRET in `.env` to add GitHub auth') }
+  if (!process.env.GITHUB_TOKEN) { throw new Error('Please define a GITHUB_TOKEN in `.env` to add GitHub auth') }
+  if (!process.env.SESSION_SECRET) { throw new Error('Please define a SESSION_SECRET in `.env` to add GitHub auth') }
+}
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN
+})
+const forbiddenHtml = readFileSync('./forbidden.html', 'utf8')
 
 export default withDocus({
   rootDir: __dirname,
   serverMiddleware: [
     createAuthMiddleware({
-      // enabled: process.env.NODE_ENV === 'production',
-      enabled: true,
+      bypass: !authEnabled,
       provider: 'github',
       providerOptions: {
         clientId: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET
       },
-      sessionSecret: process.env.SESSION_SECRET
-    }),
-    function (req, _res, next) {
-      if (!req.auth) {
-        return next()
+      sessionSecret: process.env.SESSION_SECRET,
+      onAuthorize: async (authRes) => {
+        if (!authRes.authorized || !authRes.session) { return }
+        await octokit.rest.repos.checkCollaborator({
+          owner: 'nuxt',
+          repo: 'beta',
+          username: authRes.session.user
+        }).then(() => {
+          // console.log(authRes.session.user, 'has access to Nuxt 3 private beta')
+        }).catch(() => {
+          // console.warn(authRes.session.user, 'does not have access to Nuxt 3 private beta')
+          authRes.authorized = false
+          authRes.message = forbiddenHtml
+          delete authRes.redirect
+          delete authRes.session
+        })
       }
-      // TODO: check if the user is listed as a collaborator
-      console.log('req.auth.session.user', req.auth.session.user)
-      next()
-    }
+    })
   ]
 })
