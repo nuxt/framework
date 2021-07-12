efd2bd84cac55f413c66eb7329c39126432b96fe
Daniel Roe <daniel@roe.dev>
2021-07-12 10:46:46 +0100

    docs(examples): add `vue-content-loader` to async-data example

--- examples/async-data/app.vue
+++ examples/async-data/app.vue
@@ -3,11 +3,22 @@
     <div>
       <nuxt-page />
     </div>
-    <nuxt-link to="/">
-      Home
-    </nuxt-link>
-    <nuxt-link to="/options">
-      Options
-    </nuxt-link>
+    <ul>
+      <li>
+        <nuxt-link to="/">
+          Home
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/options">
+          Options
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/defer">
+          Defer
+        </nuxt-link>
+      </li>
+    </ul>
   </div>
 </template>
