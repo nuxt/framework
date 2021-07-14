ef01e103a774000d556ba3baf7907dd3ce4c4abc
Anthony Fu <anthonyfu117@hotmail.com>
2021-07-14 19:22:45 +0800

    chore: update

--- playground/modules/unplugin.ts
+++ playground/modules/unplugin.ts
@@ -23,6 +23,7 @@ function addUnplugin<UserOptions = {}> (factory: UnpluginFactory<UserOptions>) {
 export default defineNuxtModule({
   setup () {
     let i = 0
+    const date = new Date().toString()
 
     addUnplugin(() => {
       return {
@@ -32,6 +33,18 @@ export default defineNuxtModule({
         },
         transform (code) {
           return code.replace(/<template>/, `<template><div>Injected ${i++}</div>`)
+        },
+        resolveId (id) {
+          if (id === 'virtual-build-time') {
+            return '/node_modules/' + id
+          }
+          return null
+        },
+        load (id) {
+          console.log('load', id)
+          if (id === '/node_modules/virtual-build-time') {
+            return 'export default ' + JSON.stringify(date)
+          }
         }
       }
     })
--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    Hello World
+    Hello World {{ date }}
   </div>
 </template>
 
 <script>
+import date from 'virtual-build-time'
 import { defineNuxtComponent } from '@nuxt/app'
 
 export default defineNuxtComponent({
+  setup () {
+    return {
+      date
+    }
+  }
 })
 </script>
