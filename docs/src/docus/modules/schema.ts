a8cc351461d7ef4db5eea542e38ee0b70429d429
Daniel Roe <daniel@roe.dev>
2021-07-22 20:29:19 +0100

    fix: don't indent default

--- docs/src/docus/modules/schema.ts
+++ docs/src/docus/modules/schema.ts
@@ -32,8 +32,8 @@ function generateMarkdown (schema: Schema, title: string, level: string) {
       // TODO: https://github.com/unjs/untyped/issues/8
       const defaultValue = JSON.stringify(schema.default, null, 2).replace(rootDirPattern, '')
       const defaultInfo =
-        typeof schema.default === 'object' && !Array.isArray(schema.default)
-          ? ['', '```json', ...defaultValue.split('\n'), '```'].map(line => `   ${line}`).join('\n')
+        typeof schema.default === 'object' && !Array.isArray(schema.default) && defaultValue !== '{}'
+          ? ['', '```json', ...defaultValue.split('\n'), '```'].join('\n')
           : `\`${defaultValue}\``
       lines.push(`- **Default**: ${defaultInfo}`)
     }
