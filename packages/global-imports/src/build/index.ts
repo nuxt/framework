77706791c54a4601e6116dc77c4f5cdcfb6e10e5
Anthony Fu <anthonyfu117@hotmail.com>
2021-07-27 01:57:46 +0800

    chore: wip

--- packages/global-imports/src/build/index.ts
+++ packages/global-imports/src/build/index.ts
@@ -1,12 +1,31 @@
 import { createUnplugin } from 'unplugin'
+import { parseQuery, parseURL } from 'ufo'
 import { Identifier } from '../types'
 
 export const BuildPlugin = createUnplugin((identifers: Identifier[]) => ({
   name: 'nuxt-global-imports-build',
-  transformInclude (code, id) {
-    return false
+  enforce: 'post',
+  transformInclude (id) {
+    const { pathname, search } = parseURL(id)
+    const query = parseQuery(search)
+
+    // vue files
+    if (pathname.endsWith('.vue') && (query.type === 'template' || !search)) {
+      return true
+    }
+
+    // js files
+    if (pathname.match(/\.((c|m)?j|t)sx?/g)) {
+      return true
+    }
   },
   transform (code, id) {
+    const ignored = Array.from(code.matchAll(/\bimport\s*\{(.*?)\}\s*from\b/g))
+      .flatMap(i => i[1]?.split(',') || [])
+      .map(i => i.trim())
+
+    console.log(identifers)
+
     return code
   }
 }))
