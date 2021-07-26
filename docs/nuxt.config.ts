80141dd983534705cc76faa6a99d294fb46c07d8
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-07-26 21:01:11 +0200

    :bug: (docus) fix docus config / import modules from nuxt config

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,5 +1,24 @@
-import { withDocus } from 'docus'
+import { withDocus } from '@docus/app'
 
 export default withDocus({
-  rootDir: __dirname
+  // TODO: Remove that temporary fix
+  vite: {
+    server: {
+      fs: {
+        strict: false
+      }
+    }
+  },
+  /**
+   * Has to specify rootDir as we use nuxt-extend
+   */
+  rootDir: __dirname,
+  /**
+   * Modules
+   */
+  buildModules: [
+    '@nuxt/typescript-build',
+    '@docus/github',
+    '@docus/twitter'
+  ]
 })
