667e1530cbb821bd61754765169a78d4c3eaaac5
Daniel Roe <daniel@roe.dev>
2021-07-26 20:58:12 +0100

    fix: add template within hook

--- packages/meta/src/module.ts
+++ packages/meta/src/module.ts
@@ -13,12 +13,11 @@ export default defineNuxtModule({
     nuxt.options.build.transpile.push('@nuxt/meta', runtimeDir)
     nuxt.options.alias['@nuxt/meta'] = resolve(runtimeDir, 'index')
 
-    const { dst: implementation } = addTemplate({
-      src: resolve(runtimeDir, 'vueuse-head.mjs'),
-      options
-    })
-
     nuxt.hook('app:resolve', (app) => {
+      const { dst: implementation } = addTemplate({
+        src: resolve(runtimeDir, 'vueuse-head.mjs'),
+        options
+      })
       app.plugins.push({ src: resolve(nuxt.options.buildDir, implementation) })
       app.plugins.push({ src: resolve(runtimeDir, 'meta') })
     })
