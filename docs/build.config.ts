a7950bf5155123dd45cd84942c9d450cf2d0b8ab
Pooya Parsa <pyapar@gmail.com>
2021-07-28 21:37:01 +0200

    use unbuild to generated schema

---
+++ docs/build.config.ts
@@ -0,0 +1,16 @@
+import { resolve } from 'path'
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  entries: [
+    {
+      input: resolve(__dirname, '../packages/kit/src/config/schema/index'),
+      outDir: 'schema',
+      name: 'config',
+      builder: 'untyped',
+      defaults: {
+        rootDir: '/<rootDir>'
+      }
+    }
+  ]
+})
