9a3e1533585ba23598ecc9e420bd924e6a6f9609
Daniel Roe <daniel@roe.dev>
2021-07-30 14:47:12 +0100

    fix: resolve original id for externals tracking

--- packages/nitro/src/rollup/plugins/externals.ts
+++ packages/nitro/src/rollup/plugins/externals.ts
@@ -23,6 +23,8 @@ export function externals (opts: NodeExternalsOptions): Plugin {
         return null
       }
 
+      const originalId = id
+
       // Normalize path on windows
       if (process.platform === 'win32') {
         if (id.startsWith('/')) {
@@ -50,7 +52,7 @@ export function externals (opts: NodeExternalsOptions): Plugin {
 
       // Track externals
       if (opts.trace !== false) {
-        const resolved = await this.resolve(id, importer, { ...options, skipSelf: true }).then(r => r.id)
+        const resolved = await this.resolve(originalId, importer, { ...options, skipSelf: true }).then(r => r.id)
         trackedExternals.add(resolved)
       }
 
