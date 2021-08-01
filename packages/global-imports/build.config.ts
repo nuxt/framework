44dde798f7141b8c78dc59fd5bf6f1a44e1f8ade
Anthony Fu <anthonyfu117@hotmail.com>
2021-08-01 13:42:34 +0800

    build: fix

--- packages/global-imports/build.config.ts
+++ packages/global-imports/build.config.ts
@@ -3,7 +3,6 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   declaration: true,
   entries: [
-    'src/module',
-    { input: 'src/runtime/', outDir: 'dist/runtime', format: 'esm' }
+    'src/module'
   ]
 })
