2b9ea4954de21b7ea3fc0c03387e9e271124d979
Pooya Parsa <pyapar@gmail.com>
2021-08-11 20:38:34 +0200

    useMeta identifier

--- packages/nuxt3/src/global-imports/identifiers.ts
+++ packages/nuxt3/src/global-imports/identifiers.ts
@@ -1,57 +1,62 @@
-const VueAPIs = [
-  // lifecycle
-  'onActivated',
-  'onBeforeMount',
-  'onBeforeUnmount',
-  'onBeforeUpdate',
-  'onDeactivated',
-  'onErrorCaptured',
-  'onMounted',
-  'onServerPrefetch',
-  'onUnmounted',
-  'onUpdated',
+const identifiers = {
+  '#app': [
+    'useAsyncData',
+    'asyncData',
+    'defineNuxtComponent',
+    'useNuxt',
+    'defineNuxtPlugin'
+  ],
+  '#meta': [
+    'useMeta'
+  ],
+  vue: [
+    // lifecycle
+    'onActivated',
+    'onBeforeMount',
+    'onBeforeUnmount',
+    'onBeforeUpdate',
+    'onDeactivated',
+    'onErrorCaptured',
+    'onMounted',
+    'onServerPrefetch',
+    'onUnmounted',
+    'onUpdated',
 
-  // reactivity,
-  'computed',
-  'customRef',
-  'isReadonly',
-  'isRef',
-  'markRaw',
-  'reactive',
-  'readonly',
-  'ref',
-  'shallowReactive',
-  'shallowReadonly',
-  'shallowRef',
-  'toRaw',
-  'toRef',
-  'toRefs',
-  'triggerRef',
-  'unref',
-  'watch',
-  'watchEffect',
+    // reactivity,
+    'computed',
+    'customRef',
+    'isReadonly',
+    'isRef',
+    'markRaw',
+    'reactive',
+    'readonly',
+    'ref',
+    'shallowReactive',
+    'shallowReadonly',
+    'shallowRef',
+    'toRaw',
+    'toRef',
+    'toRefs',
+    'triggerRef',
+    'unref',
+    'watch',
+    'watchEffect',
 
-  // component
-  'defineComponent',
-  'defineAsyncComponent',
-  'getCurrentInstance',
-  'h',
-  'inject',
-  'nextTick',
-  'provide',
-  'useCssModule'
-]
+    // component
+    'defineComponent',
+    'defineAsyncComponent',
+    'getCurrentInstance',
+    'h',
+    'inject',
+    'nextTick',
+    'provide',
+    'useCssModule'
+  ]
+}
 
-const nuxtComposition = [
-  'useAsyncData',
-  'asyncData',
-  'defineNuxtComponent',
-  'useNuxt',
-  'defineNuxtPlugin'
-  // 'useMeta'
-]
-
-export const defaultIdentifiers = Object.fromEntries([
-  ...VueAPIs.map(name => [name, 'vue']),
-  ...nuxtComposition.map(name => [name, '#app'])
-])
+export const defaultIdentifiers = {}
+for (const pkg in identifiers) {
+  for (const id of identifiers[pkg]) {
+    defaultIdentifiers[id] = pkg
+  }
+}
