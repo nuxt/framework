4b5c19a5c2befd91884ac29cf98cf40c8a1b5481
pooya parsa <pyapar@gmail.com>
2021-09-20 14:12:14 +0200

    small improvements

--- packages/nitro/src/runtime/server/utils.ts
+++ packages/nitro/src/runtime/server/utils.ts
@@ -1,24 +1,26 @@
-export function requestHasBody (request: Request) : boolean {
-  return /post|put|patch/i.test(request.method)
+const METHOD_WITH_BODY_RE = /post|put|patch/i
+const TEXT_MIME_RE = /application\/text|text\/html/
+const JSON_MIME_RE = /application\/json/
+
+export function requestHasBody (request: globalThis.Request) : boolean {
+  return METHOD_WITH_BODY_RE.test(request.method)
 }
 
-export async function useRequestBody (request: Request): Promise<any> {
+export async function useRequestBody (request: globalThis.Request): Promise<any> {
   const contentType = request.headers.get('content-type') || ''
-
   if (contentType.includes('form')) {
     const formData = await request.formData()
-    const body = {}
+    const body = Object.create(null)
     for (const entry of formData.entries()) {
       body[entry[0]] = entry[1]
     }
     return body
-  } else if (contentType.includes('application/json')) {
+  } else if (JSON_MIME_RE.test(contentType)) {
     return request.json()
-  } else if (/application\/text|text\/html/.test(contentType)) {
+  } else if (TEXT_MIME_RE.test(contentType)) {
     return request.text()
   } else {
-    const myBlob = await request.blob()
-    const objectURL = URL.createObjectURL(myBlob)
-    return objectURL
+    const blob = await request.blob()
+    return URL.createObjectURL(blob)
   }
 }
