401dd1718888f3f38123a7ec4b49bfeefbe727ba
Pooya Parsa <pyapar@gmail.com>
2021-09-21 20:44:02 +0200

    fix: workarouind for cjs in esm

--- packages/nuxi/bin/nuxi.mjs
+++ packages/nuxi/bin/nuxi.mjs
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
+import { createRequire } from 'module'
+
+// https://github.com/unjs/unbuild/issues/7
 process._startTime = Date.now()
+globalThis.require = createRequire(import.meta.url)
+globalThis.__filename = '.'
+globalThis.__dirname = './'
+
 import('../dist/index.mjs').then(r => (r.default || r).main())
