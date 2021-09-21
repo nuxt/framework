8f46405148b36a3e38abbe966e924efdd3ecddc9
Pooya Parsa <pyapar@gmail.com>
2021-09-21 21:55:25 +0200

    fix eslint issue

--- packages/nuxt3/src/core/nitro.ts
+++ packages/nuxt3/src/core/nitro.ts
@@ -1,4 +1,4 @@
-import { normalize, resolve } from 'path'
+import { resolve } from 'path'
 import { wpfs, getNitroContext, createDevServer, resolveMiddleware, build, prepare, generate } from '@nuxt/nitro'
 import type { Nuxt } from '@nuxt/kit'
 
