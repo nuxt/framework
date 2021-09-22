6eac8ac2d4ec05af7d911fd1394c0b955a2b317c
Pooya Parsa <pyapar@gmail.com>
2021-09-22 17:26:30 +0200

    add missing transpile

--- packages/bridge/src/app.ts
+++ packages/bridge/src/app.ts
@@ -9,6 +9,9 @@ export function setupAppBridge (_options: any) {
   nuxt.options.alias['#app'] = resolve(distDir, 'runtime/index.mjs')
   nuxt.options.alias['#build'] = nuxt.options.buildDir
 
+  // Transpile runtime/
+  nuxt.options.build.transpile.push(resolve(distDir, 'runtime'))
+
   // Resolve to same vue2 path
   nuxt.options.alias.vue = nuxt.options.alias.vue ||
     resolveModule('vue/dist/vue.runtime.esm.js', { paths: nuxt.options.modulesDir })
