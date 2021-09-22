c4b8b35cba014acfd8762bcc962569daba1257d3
Pooya Parsa <pyapar@gmail.com>
2021-09-22 17:08:50 +0200

    chore: throw if nuxtjs capi installed on same project

--- packages/bridge/src/capi.ts
+++ packages/bridge/src/capi.ts
@@ -5,6 +5,11 @@ import { distDir } from './dirs'
 export function setupCAPIBridge (_options: any) {
   const nuxt = useNuxt()
 
+  // Error if `@nuxtjs/composition-api` is added
+  if (nuxt.options.buildModules.find(m => m === '@nuxtjs/composition-api' || m === '@nuxtjs/composition-api/module')) {
+    throw new Error('Please remove `@nuxtjs/composition-api` from `buildModules` to avoid conflict with bridge.')
+  }
+
   // Add composition-api support
   nuxt.options.alias['@vue/composition-api'] = require.resolve('@vue/composition-api/dist/vue-composition-api.mjs')
   const capiPluginPath = resolve(distDir, 'runtime/capi.plugin.mjs')
