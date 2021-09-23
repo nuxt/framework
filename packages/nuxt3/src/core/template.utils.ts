e37d4089f38f6eec33713175809d5540480cb2f7
Xin Du (Clark) <clark.duxin@gmail.com>
2021-09-23 15:21:57 +0100

    refactor(app): replace runtime templates

--- packages/nuxt3/src/core/template.utils.ts
+++ packages/nuxt3/src/core/template.utils.ts
@@ -2,6 +2,21 @@ import { basename, extname } from 'upath'
 import hash from 'hash-sum'
 import { camelCase } from 'scule'
 
+export type ImportMagicCommentsOptions = {
+  chunkName:string
+  prefetch?: boolean | number
+  preload?: boolean | number
+}
+
+export const createImportMagicComments = (options: ImportMagicCommentsOptions) => {
+  const { chunkName, prefetch, preload } = options
+  return [
+    `webpackChunkName: "${chunkName}"`,
+    prefetch === true || typeof prefetch === 'number' ? `webpackPrefetch: ${prefetch}` : false,
+    preload === true || typeof preload === 'number' ? `webpackPreload: ${preload}` : false
+  ].filter(Boolean).join(', ')
+}
+
 export const serialize = data => JSON.stringify(data, null, 2).replace(/"{(.+)}"/g, '$1')
 
 export const importName = (src: string) => `${camelCase(basename(src, extname(src))).replace(/[^a-zA-Z?\d\s:]/g, '')}_${hash(src)}`
