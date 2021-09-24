b4eae5ea1534573c7f626c61a33b605e84f4c278
Daniel Roe <daniel@roe.dev>
2021-09-24 12:38:16 +0100

    chore: use vue exports of compiler-sfc and server-renderer

--- packages/nitro/src/runtime/app/vue3.ts
+++ packages/nitro/src/runtime/app/vue3.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import { renderToString as render } from '@vue/server-renderer'
+import { renderToString as render } from 'vue/server-renderer/index.mjs'
 
 export const renderToString: typeof render = (...args) => {
   return render(...args).then(result => `<div id="__nuxt">${result}</div>`)
