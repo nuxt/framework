cc9795b28833f536d4c2ee02f17816637a119498
Pooya Parsa <pyapar@gmail.com>
2021-09-29 19:29:51 +0200

    fix typo

--- packages/nitro/src/runtime/entries/firebase.ts
+++ packages/nitro/src/runtime/entries/firebase.ts
@@ -5,5 +5,3 @@ import functions from 'firebase-functions'
 import { handle } from '../server'
 
 export const server = functions.https.onRequest(handle)
-
-const _require = createRequire(import.meta.url)
