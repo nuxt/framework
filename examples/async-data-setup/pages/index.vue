454946713bf43f4ce94452a94fb0832524818a52
Pooya Parsa <pyapar@gmail.com>
2021-09-30 19:58:31 +0200

    refactor: only async-data example with script setup

--- examples/async-data-setup/pages/index.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-const { data } = asyncData('time', () => $fetch('/api/count'))
-</script>
-
-<template>
-  <div>
-    Page visits: {{ data.count }}
-  </div>
-</template>
---
+++ examples/async-data/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    Page visits: {{ data.count }}
+  </div>
+</template>
+
+<script setup>
+const { data } = await asyncData('time', () => $fetch('/api/count'))
+</script>
