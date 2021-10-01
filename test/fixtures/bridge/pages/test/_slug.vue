66adc370693d7eb0a2b637f95463a194cf6ce930
Daniel Roe <daniel@roe.dev>
2021-10-01 23:10:25 +0100

    test: add example page to show implementation

---
+++ test/fixtures/bridge/pages/test/_slug.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nuxt-link :to="`/test/${Math.random() * 100}`">
+      Random path
+    </nuxt-link>
+    {{ route.path }}
+  </div>
+</template>
+
+<script>
+import { useRoute } from '#app'
+export default {
+  setup () {
+    const route = useRoute()
+    console.log(route.path)
+    return { route }
+  }
+}
+</script>
