c87a43965d1e0eacca917d88ab2f723b35367dcf
Daniel Roe <daniel@roe.dev>
2021-10-01 22:55:21 +0100

    feat(bridge, nuxt3): expose equivalent `useRoute` and `useRouter` composables

--- packages/bridge/src/runtime/composables.ts
+++ packages/bridge/src/runtime/composables.ts
@@ -1,3 +1,4 @@
+import { getCurrentInstance } from '@vue/composition-api'
 export * from '@vue/composition-api'
 
 const mock = () => () => { throw new Error('not implemented') }
@@ -8,3 +9,19 @@ export const useSSRRef = mock()
 export const useData = mock()
 export const useGlobalData = mock()
 export const useHydration = mock()
+
+// Auto-import equivalents for `vue-router`
+export const useRouter = () => {
+  return getCurrentInstance()?.proxy.$router
+}
+
+// This provides an equivalent interface to `vue-router` (unlike legacy implementation)
+export const useRoute = () => {
+  const vm = getCurrentInstance()?.root.proxy
+  if (!vm) { throw new Error('useRoute must be called from within a Vue component') }
+  if (!vm._route) {
+    vm._route = reactive(vm.$route)
+    watch(() => vm.$route, route => Object.assign(vm._route, route))
+  }
+  return vm._route
+}
