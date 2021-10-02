3528736cbde38369ee23ace7bfe27af348dd21f8
Pooya Parsa <pyapar@gmail.com>
2021-10-02 19:44:34 +0200

    feat: checkNuxtCompatibilityIssues, ensureNuxtCompatibility and hasNuxtCompatibility utils

--- packages/kit/src/module/utils.ts
+++ packages/kit/src/module/utils.ts
@@ -5,6 +5,9 @@ import hash from 'hash-sum'
 import type { WebpackPluginInstance, Configuration as WebpackConfig } from 'webpack'
 import type { Plugin as VitePlugin, UserConfig as ViteConfig } from 'vite'
 import { camelCase } from 'scule'
+import semver from 'semver'
+import { NuxtCompatibilityConstraints, NuxtCompatibilityIssue } from '../types/module'
+import { Nuxt } from '../types/nuxt'
 import { useNuxt } from '../nuxt'
 import type { NuxtTemplate, NuxtPlugin, NuxtPluginTemplate } from '../types/nuxt'
 
@@ -297,26 +300,59 @@ export const templateUtils = {
 /**
  * Check if current nuxt instance is version 2 legacy
  */
-export function isNuxt2 (nuxt?: any) {
+export function isNuxt2 (nuxt: Nuxt = useNuxt()) {
   return getNuxtVersion(nuxt).startsWith('2.')
 }
 
 /**
  * Check if current nuxt instance is version 2 legacy
  */
-export function isNuxt3 (nuxt?: any) {
-  nuxt = nuxt || useNuxt()
+export function isNuxt3 (nuxt: Nuxt = useNuxt()) {
   return getNuxtVersion(nuxt).startsWith('3.')
 }
 
 /**
  * Get nuxt version
  */
-export function getNuxtVersion (nuxt?: any) {
-  nuxt = nuxt || useNuxt()
+export function getNuxtVersion (nuxt: Nuxt | any = useNuxt() /* TODO: LegacyNuxt */) {
   const version = (nuxt?._version || nuxt?.version || nuxt?.constructor?.version || '').replace(/^v/g, '')
   if (!version) {
     throw new Error('Cannot determine nuxt version! Is currect instance passed?')
   }
   return version
+}
+
+/**
+ * Check version constraints and return incompatibility issues as an array
+ */
+export function checkNuxtCompatibilityIssues (constraints: NuxtCompatibilityConstraints, nuxt: Nuxt = useNuxt()): NuxtCompatibilityIssue[] {
+  const issues: NuxtCompatibilityIssue[] = []
+  if (constraints.nuxt) {
+    const nuxtVersion = getNuxtVersion(nuxt)
+    if (!semver.satisfies(nuxtVersion, constraints.nuxt)) {
+      issues.push({
+        name: 'nuxt',
+        message: `Nuxt version "${constraints.nuxt}" is required but currently using "${nuxtVersion}"`
+      })
+    }
+  }
+  return issues
+}
+
+/**
+ * Check version constraints and throw a detailed error if has any, otherwise returns true
+ */
+export function ensureNuxtCompatibility (constraints: NuxtCompatibilityConstraints, nuxt: Nuxt = useNuxt()): true {
+  const issues = checkNuxtCompatibilityIssues(constraints, nuxt)
+  if (issues.length) {
+    throw new Error('Nuxt compatibility issues found:\n' + issues.map(issue => ` - [${issue.name}] ${issue.message}`).join('\n'))
+  }
+  return true
+}
+
+/**
+ * Check version constraints and return true if passed, otherwise returns false
+ */
+export function hasNuxtCompatibility (constraints: NuxtCompatibilityConstraints, nuxt: Nuxt = useNuxt()) {
+  return !checkNuxtCompatibilityIssues(constraints, nuxt).length
 }
