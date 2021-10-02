ce95da184c54cc1da8db6af8fe7e3e976d7292e0
Daniel Roe <daniel@roe.dev>
2021-10-02 13:10:00 +0100

    test: update fixture to show reactivity issue

--- test/fixtures/bridge/pages/test/_slug.vue
+++ test/fixtures/bridge/pages/test/_slug.vue
@@ -3,17 +3,5 @@
     <nuxt-link :to="`/test/${Math.random() * 100}`">
       Random path
     </nuxt-link>
-    {{ route.path }}
   </div>
 </template>
-
-<script>
-import { useRoute } from '#app'
-export default {
-  setup () {
-    const route = useRoute()
-    console.log(route.path)
-    return { route }
-  }
-}
-</script>
