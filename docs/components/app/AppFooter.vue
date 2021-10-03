ec21610d83474bff3fb90bc2798aea504e312196
Sébastien Chopin <seb@nuxtjs.com>
2021-10-04 01:26:11 +0200

    chore: wip

---
+++ docs/components/app/AppFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="bg-cloud-surface py-2 text-center">
+    Looking for Nuxt 2? Checkout <a href="https://nuxtjs.org" class="font-bold underline">nuxtjs.org</a>
+  </footer>
+</template>
---
+++ docs/i18n/en-US.js
@@ -0,0 +1,8 @@
+import en from '~docus-i18n/en-US'
+
+export default {
+  ...en,
+  common: {
+    currently_version: 'Currently in public beta'
+  }
+}
