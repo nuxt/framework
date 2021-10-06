471e0eee80781b57ff172213445888607ef57a44
Daniel Roe <daniel@roe.dev>
2021-10-06 15:57:06 +0200

    fix(bridge): inject babel typescript plugin only once

--- packages/bridge/src/typescript.ts
+++ packages/bridge/src/typescript.ts
@@ -13,7 +13,16 @@ export function setupTypescript () {
   const _require = createRequire(import.meta.url)
   const babelPlugin = _require.resolve('@babel/plugin-transform-typescript')
   nuxt.options.build.babel.plugins = nuxt.options.build.babel.plugins || []
-  nuxt.options.build.babel.plugins.unshift(babelPlugin)
+
+  const hasBabelPlugin = nuxt.options.build.babel.plugins.some((r) => {
+    if (typeof r === 'string' && r.includes('@babel/plugin-transform-typescript')) { return true }
+    if (Array.isArray(r) && r.some(i => i.includes('@babel/plugin-transform-typescript'))) { return true }
+    return false
+  })
+
+  if (!hasBabelPlugin) {
+    nuxt.options.build.babel.plugins.unshift(babelPlugin)
+  }
 
   extendWebpackConfig((config) => {
     config.resolve.extensions!.push(...extensions.map(e => `.${e}`))
