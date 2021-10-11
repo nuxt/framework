8cfc1568275f0ea31e5c2a36bbad50571913d6d3
Pooya Parsa <pyapar@gmail.com>
2021-10-11 21:07:35 +0200

    feat: transform and pick

--- packages/nuxt3/src/app/composables/fetch.ts
+++ packages/nuxt3/src/app/composables/fetch.ts
@@ -5,6 +5,8 @@ import { useAsyncData } from './asyncData'
 
 type UseFetchOptions<ResT> = AsyncDataOptions<ResT> & FetchOptions & {
   fetchKey?: string
+  transform?: (res: any) => ResT
+  pick?: string[]
 }
 
 export function useFetch<ReqT extends string = string, ResT = ReturnType<$Fetch<unknown, ReqT>>> (url: ReqT, opts: UseFetchOptions<ResT> = {}): AsyncData<ResT> {
@@ -23,11 +25,27 @@ export function useFetch<ReqT extends string = string, ResT = ReturnType<$Fetch<
   }
 
   return useAsyncData<ResT>('$f' + opts.fetchKey, () => {
-    return $fetch(url, opts) as Promise<ResT>
+    return $fetch(url, opts).then((res) => {
+      if (opts.transform) {
+        res = opts.transform(res)
+      }
+      if (opts.pick) {
+        res = pick(res, opts.pick)
+      }
+      return res
+    }) as Promise<ResT>
   })
 }
 
 // TODO: More predictable universal hash
 function generateKey (keys) {
   return JSON.stringify(keys).replace(/[{":}=/,]|https?:\/\//g, '_').replace(/_+/g, '_')
+}
+
+function pick (obj: Record<string, any>, keys: string[]) {
+  const newObj = {}
+  for (const key of keys) {
+    newObj[key] = obj[key]
+  }
+  return newObj
 }
