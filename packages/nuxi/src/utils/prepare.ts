9fa298d42d79dae0da4c3cec49f5cf42f838d667
Daniel Roe <daniel@roe.dev>
2021-10-13 16:07:15 +0100

    fix: remove test

--- packages/nuxi/src/utils/prepare.ts
+++ packages/nuxi/src/utils/prepare.ts
@@ -75,9 +75,7 @@ export const writeTSConfig = async (nuxt: Nuxt) => {
   await nuxt.callHook('prepare:tsconfig', tsConfig)
 
   const tsConfigPath = resolve(`${nuxt.options.buildDir}/tsconfig.json`)
-  if (!await exists(nuxt.options.buildDir)) {
-    await fsp.mkdir(nuxt.options.buildDir, { recursive: true })
-  }
+  await fsp.mkdir(nuxt.options.buildDir, { recursive: true })
   await fsp.writeFile(tsConfigPath, JSON.stringify(tsConfig, null, 2))
 }
 
