149eedef962d7b8c61a715bfff6cef47f43a6241
Anthony Fu <anthonyfu117@hotmail.com>
2021-10-16 03:38:26 +0800

    fix(vite): dev ssr fouc with preprocessors

---
+++ examples/with-scss/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app">
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  padding: 1rem 1.5rem;
+
+  h1 {
+    font-size: 4em;
+    font-weight: 400;
+  }
+}
+</style>
