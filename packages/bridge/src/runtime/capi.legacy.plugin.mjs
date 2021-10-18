b4cd7fc9abee5d0f5c4660d9973487f7cd4e2685
Pooya Parsa <pyapar@gmail.com>
2021-10-18 18:31:04 +0200

    update capi plugin

--- packages/bridge/src/runtime/capi.legacy.plugin.mjs
+++ packages/bridge/src/runtime/capi.legacy.plugin.mjs
@@ -3,9 +3,9 @@ import { defineNuxtPlugin } from '#app'
 export default defineNuxtPlugin((nuxt) => {
   nuxt._setupFns = []
 
-  const _originalSetup = nuxt.nuxt2App.setup
+  const _originalSetup = nuxt.nuxt2Context.app.setup
 
-  nuxt.nuxt2App.setup = function (...args) {
+  nuxt.nuxt2Context.app.setup = function (...args) {
     const result = _originalSetup instanceof Function ? _originalSetup(...args) : {}
     for (const fn of nuxt._setupFns) {
       Object.assign(result, fn.call(this, ...args))
