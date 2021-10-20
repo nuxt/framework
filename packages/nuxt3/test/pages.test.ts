8d0f99cd198bd8f194cbccda74b5b8e016ae526c
Pooya Parsa <pyapar@gmail.com>
2021-10-20 19:57:47 +0200

    refactor: use generator pattern for tests

--- packages/nuxt3/test/pages.test.ts
+++ packages/nuxt3/test/pages.test.ts
@@ -1,171 +1,193 @@
 import { expect } from 'chai'
 import { generateRoutesFromFiles } from '../src/pages/utils'
 
-describe('pages:utils', () => {
-  describe('generateRoutesFromFiles', () => {
-    const pagesDir = 'pages'
-
-    it('should generate correct route for 404', () => {
-      const files = [`${pagesDir}/404.vue`]
-      const output = generateRoutesFromFiles(files, pagesDir)
-      expect(output[0]).to.deep.equal({
-        name: '404',
-        path: '/:catchAll(.*)*',
-        file: `${pagesDir}/404.vue`,
-        children: []
-      })
-    })
-
-    it('should generate correct routes for index pages', () => {
-      const files = [
-        `${pagesDir}/index.vue`,
-        `${pagesDir}/parent/index.vue`,
-        `${pagesDir}/parent/child/index.vue`
+describe('pages:generateRoutesFromFiles', () => {
+  const pagesDir = 'pages'
+  const tests = [
+    {
+      description: 'should generate correct route for 404',
+      files: [`${pagesDir}/404.vue`],
+      output: [
+        {
+          name: '404',
+          path: '/:catchAll(.*)*',
+          file: `${pagesDir}/404.vue`,
+          children: []
+        }
       ]
-      const output = generateRoutesFromFiles(files, pagesDir)
-      expect(output[0]).to.deep.equal({
-        name: 'index',
-        path: '/',
-        file: `${pagesDir}/index.vue`,
-        children: []
-      })
-      expect(output[1]).to.deep.equal({
-        name: 'parent',
-        path: '/parent',
-        file: `${pagesDir}/parent/index.vue`,
-        children: []
-      })
-      expect(output[2]).to.deep.equal({
-        name: 'parent-child',
-        path: '/parent/child',
-        file: `${pagesDir}/parent/child/index.vue`,
-        children: []
-      })
-    })
-
-    it('should generate correct routes for parent/child', () => {
-      const files = [
-        `${pagesDir}/parent.vue`,
-        `${pagesDir}/parent/child.vue`
+    },
+    {
+      description: 'should generate correct routes for index pages',
+      files: [
+          `${pagesDir}/index.vue`,
+          `${pagesDir}/parent/index.vue`,
+          `${pagesDir}/parent/child/index.vue`
+      ],
+      output: [
+        {
+          name: 'index',
+          path: '/',
+          file: `${pagesDir}/index.vue`,
+          children: []
+        },
+        {
+          name: 'parent',
+          path: '/parent',
+          file: `${pagesDir}/parent/index.vue`,
+          children: []
+        },
+        {
+          name: 'parent-child',
+          path: '/parent/child',
+          file: `${pagesDir}/parent/child/index.vue`,
+          children: []
+        }
       ]
-      const output = generateRoutesFromFiles(files, pagesDir)
-      expect(output[0]).to.deep.equal({
-        name: 'parent',
-        path: '/parent',
-        file: `${pagesDir}/parent.vue`,
-        children: [
-          {
-            name: 'parent-child',
-            path: 'child',
-            file: `${pagesDir}/parent/child.vue`,
-            children: []
-          }
-        ]
-      })
-    })
-
-    it('should generate correct route for snake_case file', () => {
-      const files = [`${pagesDir}/snake_case.vue`]
-      const output = generateRoutesFromFiles(files, pagesDir)
-      expect(output[0]).to.deep.equal({
-        name: 'snake_case',
-        path: '/snake_case',
-        file: `${pagesDir}/snake_case.vue`,
-        children: []
-      })
-    })
-
-    it('should generate correct route for kebab-case file', () => {
-      const files = [`${pagesDir}/kebab-case.vue`]
-      const output = generateRoutesFromFiles(files, pagesDir)
-      expect(output[0]).to.deep.equal({
-        name: 'kebab-case',
-        path: '/kebab-case',
-        file: `${pagesDir}/kebab-case.vue`,
-        children: []
-      })
-    })
-
-    it('should generate correct dynamic routes', () => {
-      const files = [
-        `${pagesDir}/[slug].vue`,
-        `${pagesDir}/sub/[slug].vue`,
-        `${pagesDir}/[sub]/route-[slug].vue`
+    },
+    {
+      description: 'should generate correct routes for parent/child',
+      files: [
+          `${pagesDir}/parent.vue`,
+          `${pagesDir}/parent/child.vue`
+      ],
+      output: [
+        {
+          name: 'parent',
+          path: '/parent',
+          file: `${pagesDir}/parent.vue`,
+          children: [
+            {
+              name: 'parent-child',
+              path: 'child',
+              file: `${pagesDir}/parent/child.vue`,
+              children: []
+            }
+          ]
+        }
       ]
-      const output = generateRoutesFromFiles(files, pagesDir)
-      expect(output[0]).to.deep.equal({
-        name: 'slug',
-        path: '/:slug?',
-        file: `${pagesDir}/[slug].vue`,
-        children: []
-      })
-      expect(output[1]).to.deep.equal({
-        name: 'sub-slug',
-        path: '/sub/:slug?',
-        file: `${pagesDir}/sub/[slug].vue`,
-        children: []
-      })
-      expect(output[2]).to.deep.equal({
-        name: 'sub-route-slug',
-        path: '/:sub/route-:slug',
-        file: `${pagesDir}/[sub]/route-[slug].vue`,
-        children: []
-      })
-    })
-
-    it('should generate correct catch-all route', () => {
-      const files = [`${pagesDir}/[...slug].vue`]
-      const output = generateRoutesFromFiles(files, pagesDir)
-      expect(output[0]).to.deep.equal({
-        name: 'slug',
-        path: '/:slug(.*)*',
-        file: `${pagesDir}/[...slug].vue`,
-        children: []
-      })
-    })
-
-    it('should throw unfinished param error for dynamic route', () => {
-      const files = [`${pagesDir}/[slug.vue`]
-      expect(() => generateRoutesFromFiles(files, pagesDir)).to.throw('Unfinished param "slug"')
-    })
-
-    it('should throw empty param error for dynamic route', () => {
-      const files = [`${pagesDir}/[].vue`]
-      expect(() => generateRoutesFromFiles(files, pagesDir)).to.throw('Empty param')
-    })
-
-    it('should only allow "_" & "." as special character for dynamic route', () => {
-      const files = [
-        `${pagesDir}/[a1_1a].vue`,
-        `${pagesDir}/[b2.2b].vue`,
-        `${pagesDir}/[c3@3c].vue`,
-        `${pagesDir}/[d4-4d].vue`
+    },
+    {
+      description: 'should generate correct route for snake_case file',
+      files: [
+          `${pagesDir}/snake_case.vue`
+      ],
+      output: [
+        {
+          name: 'snake_case',
+          path: '/snake_case',
+          file: `${pagesDir}/snake_case.vue`,
+          children: []
+        }
       ]
-      const output = generateRoutesFromFiles(files, pagesDir)
-      expect(output[0]).to.deep.equal({
-        name: 'a1_1a',
-        path: '/:a1_1a?',
-        file: `${pagesDir}/[a1_1a].vue`,
-        children: []
-      })
-      expect(output[1]).to.deep.equal({
-        name: 'b2.2b',
-        path: '/:b2.2b?',
-        file: `${pagesDir}/[b2.2b].vue`,
-        children: []
-      })
-      expect(output[2]).to.deep.equal({
-        name: 'c33c',
-        path: '/:c33c?',
-        file: `${pagesDir}/[c3@3c].vue`,
-        children: []
-      })
-      expect(output[3]).to.deep.equal({
-        name: 'd44d',
-        path: '/:d44d?',
-        file: `${pagesDir}/[d4-4d].vue`,
-        children: []
-      })
+    },
+    {
+      description: 'should generate correct route for kebab-case file',
+      files: [`${pagesDir}/kebab-case.vue`],
+      output: [
+        {
+          name: 'kebab-case',
+          path: '/kebab-case',
+          file: `${pagesDir}/kebab-case.vue`,
+          children: []
+        }
+      ]
+    },
+    {
+      description: 'should generate correct dynamic routes',
+      files: [
+          `${pagesDir}/[slug].vue`,
+          `${pagesDir}/sub/[slug].vue`,
+          `${pagesDir}/[sub]/route-[slug].vue`
+      ],
+      output: [
+        {
+          name: 'slug',
+          path: '/:slug?',
+          file: `${pagesDir}/[slug].vue`,
+          children: []
+        },
+        {
+          name: 'sub-slug',
+          path: '/sub/:slug?',
+          file: `${pagesDir}/sub/[slug].vue`,
+          children: []
+        },
+        {
+          name: 'sub-route-slug',
+          path: '/:sub/route-:slug',
+          file: `${pagesDir}/[sub]/route-[slug].vue`,
+          children: []
+        }
+      ]
+    },
+    {
+      description: 'should generate correct catch-all route',
+      files: [`${pagesDir}/[...slug].vue`],
+      output: [
+        {
+          name: 'slug',
+          path: '/:slug(.*)*',
+          file: `${pagesDir}/[...slug].vue`,
+          children: []
+        }
+      ]
+    },
+    {
+      description: 'should throw unfinished param error for dynamic route',
+      files: [`${pagesDir}/[slug.vue`],
+      error: 'Unfinished param "slug"'
+    },
+    {
+      description: 'should throw empty param error for dynamic route',
+      files: [
+          `${pagesDir}/[].vue`
+      ],
+      error: 'Empty param'
+    },
+    {
+      description: 'should only allow "_" & "." as special character for dynamic route',
+      files: [
+          `${pagesDir}/[a1_1a].vue`,
+          `${pagesDir}/[b2.2b].vue`,
+          `${pagesDir}/[c3@3c].vue`,
+          `${pagesDir}/[d4-4d].vue`
+      ],
+      output: [
+        {
+          name: 'a1_1a',
+          path: '/:a1_1a?',
+          file: `${pagesDir}/[a1_1a].vue`,
+          children: []
+        },
+        {
+          name: 'b2.2b',
+          path: '/:b2.2b?',
+          file: `${pagesDir}/[b2.2b].vue`,
+          children: []
+        },
+        {
+          name: 'c33c',
+          path: '/:c33c?',
+          file: `${pagesDir}/[c3@3c].vue`,
+          children: []
+        },
+        {
+          name: 'd44d',
+          path: '/:d44d?',
+          file: `${pagesDir}/[d4-4d].vue`,
+          children: []
+        }
+      ]
+    }
+  ]
+
+  for (const test of tests) {
+    it(test.description, async () => {
+      if (test.error) {
+        expect(() => generateRoutesFromFiles(test.files, pagesDir)).to.throws(test.error)
+      } else {
+        expect(await generateRoutesFromFiles(test.files, pagesDir)).to.deep.equal(test.output)
+      }
     })
-  })
+  }
 })
