36d3f0ec71375849c5046fea31f35e6a63f620cd
Pooya Parsa <pyapar@gmail.com>
2021-10-20 21:51:57 +0200

    feat(nitro): improve dev worker stability

--- packages/nitro/src/runtime/entries/dev.ts
+++ packages/nitro/src/runtime/entries/dev.ts
@@ -1,14 +1,17 @@
 import '#polyfill'
 import { Server } from 'http'
-import { parentPort } from 'worker_threads'
-import type { AddressInfo } from 'net'
+import { tmpdir } from 'os'
+import { join } from 'path'
+import { mkdirSync } from 'fs'
+import { threadId, parentPort } from 'worker_threads'
+// import type { AddressInfo } from 'net'
 import { handle } from '../server'
 
 const server = new Server(handle)
 
-const netServer = server.listen(0, () => {
-  parentPort.postMessage({
-    event: 'listen',
-    port: (netServer.address() as AddressInfo).port
-  })
+const socketDir = join(tmpdir(), 'nitro')
+mkdirSync(socketDir, { recursive: true })
+const socketPath = join(socketDir, `worker-${process.pid}-${threadId}.sock`)
+server.listen(socketPath, () => {
+  parentPort.postMessage({ event: 'listen', address: { socketPath } })
 })
