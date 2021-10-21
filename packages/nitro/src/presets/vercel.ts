70d751501ab522d89613abb8d8a2f3cc3ef05839
Andy Bitz <artzbitz@gmail.com>
2021-10-21 21:36:27 +0200

    [nitro] Update output for Vercel

--- packages/nitro/src/presets/vercel.ts
+++ packages/nitro/src/presets/vercel.ts
@@ -1,13 +1,15 @@
-import { resolve } from 'pathe'
+import path from 'pathe'
 import { extendPreset, writeFile } from '../utils'
 import { NitroPreset, NitroContext } from '../context'
 import { node } from './node'
+import globby from 'globby'
+import fs from 'fs-extra'
 
 export const vercel: NitroPreset = extendPreset(node, {
   entry: '{{ _internal.runtimeDir }}/entries/vercel',
   output: {
-    dir: '{{ _nuxt.rootDir }}/.vercel_build_output',
-    serverDir: '{{ output.dir }}/functions/node/server',
+    dir: '{{ _nuxt.rootDir }}/.output',
+    serverDir: '{{ output.dir }}/nuxt-server',
     publicDir: '{{ output.dir }}/static'
   },
   ignore: [
@@ -15,35 +17,73 @@ export const vercel: NitroPreset = extendPreset(node, {
   ],
   hooks: {
     async 'nitro:compiled' (ctx: NitroContext) {
+      await createServerlessFunction(ctx);
       await writeRoutes(ctx)
     }
   }
 })
 
-async function writeRoutes ({ output }: NitroContext) {
-  const routes = [
-    {
-      src: '/sw.js',
-      headers: {
-        'cache-control': 'public, max-age=0, must-revalidate'
-      },
-      continue: true
-    },
-    {
-      src: '/_nuxt/(.*)',
-      headers: {
-        'cache-control': 'public,max-age=31536000,immutable'
-      },
-      continue: true
-    },
-    {
-      handle: 'filesystem'
-    },
-    {
-      src: '(.*)',
-      dest: '/.vercel/functions/server/index'
-    }
-  ]
+async function createServerlessFunction (ctx: NitroContext) {
+  const { output, _nuxt: { rootDir } } = ctx;
 
-  await writeFile(resolve(output.dir, 'config/routes.json'), JSON.stringify(routes, null, 2))
+  const pageEntryPoint = path.join(output.dir, 'server', 'pages', 'index.mjs')
+  const nftFile = `${pageEntryPoint}.nft.json`;
+
+  const mountDir = path.join('.output', 'server', 'pages')
+
+  await fs.ensureDir(path.join(rootDir, mountDir))
+  await fs.promises.rename(path.join(output.serverDir, 'index.mjs'), pageEntryPoint)
+
+  const includedFiles = await globby('**', { cwd: output.serverDir })
+
+  const nftContent = {
+    version: 1,
+    files: includedFiles.map(relPath => {
+      return {
+        input: path.relative(
+          path.join(rootDir, mountDir),
+          path.join(output.serverDir, relPath)
+        ),
+        output: relPath
+      }
+    })
+  }
+  await writeFile(nftFile, JSON.stringify(nftContent, null, 2));
+}
+
+async function writeRoutes ({ output }: NitroContext) {
+  const routesManifest = {
+    version: 3,
+    headers: [
+      {
+        source: '/sw.js',
+        headers: [
+          {
+            key: 'cache-control',
+            value: 'public, max-age=0, must-revalidate'
+          }
+        ],
+        regex: '^/sw.js'
+      },
+      {
+        source: '/_nuxt/(.*)',
+        headers: [
+          {
+            key: 'cache-control',
+            value: 'public,max-age=31536000,immutable'
+          }
+        ],
+        regex: '^/_nuxt/(.*)',
+      }
+    ],
+    rewrites: [
+      {
+        source: '(.*)',
+        destination: '/server/index',
+        regex: '^(.*)$'
+      }
+    ]
+  };
+
+  await writeFile(path.resolve(output.dir, 'routes-manifest.json'), JSON.stringify(routesManifest, null, 2))
 }
