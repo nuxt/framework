8b0c913962c5c22c44ce181db108f1d3802a83df
huynl-96 <huy.nguyen.luong96@gmail.com>
2021-10-21 23:49:59 +0700

    fix: nuxt-child

---
+++ packages/nuxt3/src/pages/runtime/child.vue
@@ -0,0 +1,24 @@
+<template>
+  <RouterView v-slot="{ Component }">
+    <NuxtLayout v-if="Component" :name="layout || Component.type.layout">
+      <transition name="page" mode="out-in">
+        <component :is="Component" :key="$route.path" />
+      </transition>
+    </NuxtLayout>
+  </RouterView>
+</template>
+
+<script>
+import NuxtLayout from './layout'
+
+export default {
+  name: 'NuxtChild',
+  components: { NuxtLayout },
+  props: {
+    layout: {
+      type: String,
+      default: null
+    },
+  }
+}
+</script>
