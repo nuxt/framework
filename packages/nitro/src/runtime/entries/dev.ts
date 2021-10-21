248484db3a0028ced51267382fe30ba27621e292
pooya parsa <pyapar@gmail.com>
2021-10-21 13:39:23 +0200

    use \\pipe ns for windows

--- packages/nitro/src/runtime/entries/dev.ts
+++ packages/nitro/src/runtime/entries/dev.ts
@@ -4,14 +4,23 @@ import { tmpdir } from 'os'
 import { join } from 'path'
 import { mkdirSync } from 'fs'
 import { threadId, parentPort } from 'worker_threads'
-// import type { AddressInfo } from 'net'
 import { handle } from '../server'
 
 const server = new Server(handle)
 
-const socketDir = join(tmpdir(), 'nitro')
-mkdirSync(socketDir, { recursive: true })
-const socketPath = join(socketDir, `worker-${process.pid}-${threadId}.sock`)
+function createSocket () {
+  const isWin = process.platform === 'win32'
+  const socketName = `worker-${process.pid}-${threadId}.sock`
+  if (isWin) {
+    return join('\\\\.\\pipe\\nitro', socketName)
+  } else {
+    const socketDir = join(tmpdir(), 'nitro')
+    mkdirSync(socketDir, { recursive: true })
+    return join(socketDir, socketName)
+  }
+}
+
+const socketPath = createSocket()
 server.listen(socketPath, () => {
   parentPort.postMessage({ event: 'listen', address: { socketPath } })
 })
