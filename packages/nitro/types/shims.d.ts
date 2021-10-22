4949e123af4dd3da51b630e9f057ac592474cbfa
Daniel Roe <daniel@roe.dev>
2021-10-22 22:58:20 +0100

    docs: remove warnings

--- packages/nitro/types/shims.d.ts
+++ packages/nitro/types/shims.d.ts
@@ -1,31 +1,11 @@
 declare module '#storage' {
   import type { Storage } from 'unstorage'
-  /**
-   * @warning This is only available within server API routes or server middleware,
-   * not within your Vue app.
-   */
   export const storage: Storage
 }
 
 declare module '#assets' {
-  /**
-   * @warning This is only available within server API routes or server middleware,
-   * not within your Vue app.
-   */
   export interface AssetMeta { type?: string, etag?: string, mtime?: string }
-  /**
-   * @warning This is only available within server API routes or server middleware,
-   * not within your Vue app.
-   */
-  export function readAsset<T=any>(id: string): Promise<T>
-  /**
-   * @warning This is only available within server API routes or server middleware,
-   * not within your Vue app.
-   */
-  export function statAsset(id: string): Promise<AssetMeta>
-  /**
-   * @warning This is only available within server API routes or server middleware,
-   * not within your Vue app.
-   */
+  export function readAsset<T = any> (id: string): Promise<T>
+  export function statAsset (id: string): Promise<AssetMeta>
   export function getKeys() : Promise<string[]>
 }
