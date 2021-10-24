c506093a1417c10e38a4553dc8816868d467cc45
Pooya Parsa <pyapar@gmail.com>
2021-10-24 18:48:52 +0200

    improved impl for dirs.ts

--- packages/bridge/src/dirs.ts
+++ packages/bridge/src/dirs.ts
@@ -1,7 +1,11 @@
 import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'pathe'
+import { dirname, } from 'pathe'
+import { existsSync } from 'fs'
+import { join } from 'path'
 
-// Assuming dirs goes to dist/chunks
-export const distDir = resolve(dirname(fileURLToPath(import.meta.url)), '..')
-
-export const pkgDir = resolve(distDir, '..')
+let dir = dirname(fileURLToPath(import.meta.url))
+while(dir !== '/' && !existsSync(join(dir, 'package.json'))) {
+  dir = dirname(dir)
+}
+export const pkgDir = dir
+export const distDir = join(pkgDir, 'dist')
