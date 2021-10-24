3bd1e330f1aa4449b2deedd4a896d93d103bba2f
Pooya Parsa <pyapar@gmail.com>
2021-10-24 16:28:55 +0200

    fix: use explicit reexport from vue capi

--- packages/bridge/src/runtime/vue2-bridge.mjs
+++ packages/bridge/src/runtime/vue2-bridge.mjs
@@ -1,6 +1,6 @@
 import Vue from 'vue2'
 
-export * from '@vue/composition-api'
+export { EffectScope, computed, createApp, createRef, customRef, defineAsyncComponent, defineComponent, del, effectScope, getCurrentInstance, getCurrentScope, h, inject, isRaw, isReactive, isReadonly, isRef, markRaw, nextTick, onActivated, onBeforeMount, onBeforeUnmount, onBeforeUpdate, onDeactivated, onErrorCaptured, onMounted, onScopeDispose, onServerPrefetch, onUnmounted, onUpdated, provide, proxyRefs, reactive, readonly, ref, set, shallowReactive, shallowReadonly, shallowRef, toRaw, toRef, toRefs, triggerRef, unref, useAttrs, useCSSModule, useCssModule, useSlots, version, warn, watch, watchEffect, watchPostEffect, watchSyncEffect } from '@vue/composition-api'
 
 export const isFunction = fn => fn instanceof Function
 
