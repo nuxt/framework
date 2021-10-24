79f39de4b67bc869fb09fe08ffe118b975824f6c
Pooya Parsa <pyapar@gmail.com>
2021-10-24 18:39:17 +0200

    temp fix for dirs

--- packages/bridge/src/dirs.ts
+++ packages/bridge/src/dirs.ts
@@ -1,5 +1,7 @@
 import { fileURLToPath } from 'url'
 import { dirname, resolve } from 'pathe'
 
-export const distDir = dirname(fileURLToPath(import.meta.url))
+// Assuming dirs goes to dist/chunks
+export const distDir = resolve(dirname(fileURLToPath(import.meta.url)), '..')
+
 export const pkgDir = resolve(distDir, '..')
