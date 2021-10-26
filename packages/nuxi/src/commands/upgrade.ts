52dc0867cdbe84a09c0de9a2750c8e12886763ab
Mgs. M. Rizqi Fadhlurrahman <rizqirizqi23@gmail.com>
2021-10-26 22:07:40 +0700

    fix review

--- packages/nuxi/src/commands/upgrade.ts
+++ packages/nuxi/src/commands/upgrade.ts
@@ -1,49 +1,59 @@
 import { execSync } from 'child_process'
+import { rmdir } from 'fs/promises'
+import { existsSync } from 'fs'
+import consola from 'consola'
 import { resolve } from 'pathe'
-import { pathExists, remove } from 'fs-extra'
+import { getNearestPackage } from '../utils/cjs'
 import { defineNuxtCommand } from './index'
+
+function getNuxtVersion () {
+  return getNearestPackage('nuxt3', './node_modules')?.version
+}
 
 export default defineNuxtCommand({
   meta: {
     name: 'upgrade',
-    usage: 'npx nuxi upgrade [--force|-f] [--verbose|-v]',
+    usage: 'npx nuxi upgrade [--force|-f]',
     description: 'Upgrade nuxt3'
   },
   async invoke (args) {
     const rootDir = resolve(args._[0] || '.')
-    const verbose = args.verbose || args.v
 
     const yarnLock = 'yarn.lock'
     const npmLock = 'package-lock.json'
 
-    if (verbose) { console.log('Detecting package manager...') }
-    const [isYarn, isNpm] = await Promise.all([
-      pathExists(resolve(rootDir, yarnLock)),
-      pathExists(resolve(rootDir, npmLock))
-    ])
+    consola.info('Detecting package manager...')
+    const isYarn = existsSync(resolve(rootDir, yarnLock))
+    const isNpm = existsSync(resolve(rootDir, npmLock))
     const packageManager = isYarn ? 'yarn' : isNpm ? 'npm' : null
     if (!packageManager) {
       throw new Error('Cannot detect Package Manager.')
     }
     const packageManagerVersion = execSync(`${packageManager} --version`).toString('utf8').trim()
-    if (verbose) { console.log('Package Manager:', packageManager, packageManagerVersion) }
+    consola.info('Package Manager:', packageManager, packageManagerVersion)
 
     if (args.force || args.f) {
-      if (verbose) { console.log('Removing lock file and node_modules...') }
+      consola.info('Removing lock file and node_modules...')
       await Promise.all([
-        remove(yarnLock),
-        remove(npmLock),
-        remove('node_modules')
+        rmdir(yarnLock, { recursive: true }),
+        rmdir(npmLock, { recursive: true }),
+        rmdir('node_modules', { recursive: true })
       ])
     }
 
-    if (verbose) { console.log('Upgrading nuxt3...') }
+    consola.info('Upgrading nuxt3...')
+    const prevVersion = getNuxtVersion()
     if (isYarn && Number(packageManagerVersion.split('.')[0]) > 1) {
       execSync(`${packageManager} up nuxt3 -i`, { stdio: 'inherit' })
     } else {
       execSync(`${packageManager} upgrade nuxt3`, { stdio: 'inherit' })
     }
+    const currentVersion = getNuxtVersion()
 
-    console.log('Upgrade nuxt3 success.')
+    if (prevVersion === currentVersion) {
+      consola.success('You\'re already using the latest nuxt3.')
+    } else {
+      consola.success('Upgrade nuxt3 success. Upgraded from', prevVersion, 'to', currentVersion)
+    }
   }
 })
