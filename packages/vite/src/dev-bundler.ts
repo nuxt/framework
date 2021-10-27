6be7c90d54818ef50c148378736d26967308dd93
Anthony Fu <anthonyfu117@hotmail.com>
2021-10-27 17:33:21 +0800

    fix(vite): dev-bundler externality

--- packages/vite/src/dev-bundler.ts
+++ packages/vite/src/dev-bundler.ts
@@ -25,12 +25,17 @@ function isExternal (opts: TransformOptions, id: string) {
   // Externals
   const ssrConfig = (opts.viteServer.config as any).ssr
 
+  const alias = opts.viteServer.config.resolve.alias || {}
+  const aliasKeys = Array.isArray(alias)
+    ? alias.map(i => i.find).filter(Boolean)
+    : Object.keys(alias)
+
   const externalOpts: ExternalsOptions = {
     inline: [
       /virtual:/,
       /\.ts$/,
       // Things like '~', '@', etc.
-      ...Object.keys(opts.viteServer.config.resolve.alias),
+      ...aliasKeys,
       ...ExternalsDefaults.inline,
       ...ssrConfig.noExternal
     ],
