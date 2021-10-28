27f1add8617d89a2a1beceb34f0f3c7bc1c06552
Daniel Roe <daniel@roe.dev>
2021-10-28 17:52:36 +0100

    chore: use `externality` to detect invalid node imports

--- packages/vite/src/dev-bundler.ts
+++ packages/vite/src/dev-bundler.ts
@@ -1,7 +1,6 @@
 import { pathToFileURL } from 'url'
 import * as vite from 'vite'
 import { ExternalsOptions, isExternal as _isExternal, ExternalsDefaults } from 'externality'
-import { isValidNodeImport } from 'mlly'
 import { hashId, uniq } from './utils'
 
 export interface TransformChunk {
@@ -40,12 +39,11 @@ function isExternal (opts: TransformOptions, id: string) {
     resolve: {
       type: 'module',
       extensions: ['.ts', '.js', '.json', '.vue', '.mjs', '.jsx', '.tsx', '.wasm']
-    }
+    },
+    detectInvalidNodeImports: true
   }
   return _isExternal(id, opts.viteServer.config.root, externalOpts)
 }
-
-const inlinedPackages = new Set<string>()
 
 async function transformRequest (opts: TransformOptions, id: string) {
   // Virtual modules start with `\0`
@@ -64,18 +62,10 @@ async function transformRequest (opts: TransformOptions, id: string) {
   }
 
   if (await isExternal(opts, id)) {
-    if (await isValidNodeImport(id)) {
-      return {
-        code: `(global, exports, importMeta, ssrImport, ssrDynamicImport, ssrExportAll) => import('${(pathToFileURL(id))}').then(r => { exports.default = r.default; ssrExportAll(r) })`,
-        deps: [],
-        dynamicDeps: []
-      }
-    } else {
-      const packageName = id.replace(/^.*node_modules\/([^/]*)\/(.*)/g, '$1')
-      if (!inlinedPackages.has(packageName)) {
-        console.warn(`Automatically inlining \`${packageName}\` as it does not provide a valid Node ESM import. Please raise an issue with that package's maintainers directly. If you believe this to be incorrect, let us know so we can improve our detection.`)
-        inlinedPackages.add(packageName)
-      }
+    return {
+      code: `(global, exports, importMeta, ssrImport, ssrDynamicImport, ssrExportAll) => import('${(pathToFileURL(id))}').then(r => { exports.default = r.default; ssrExportAll(r) })`,
+      deps: [],
+      dynamicDeps: []
     }
   }
 
