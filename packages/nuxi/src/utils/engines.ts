e8c3a737d084976a1a84a00830f81c984d1299ba
Daniel Roe <daniel@roe.dev>
2021-10-30 07:16:31 +0100

    fix(nuxi, kit): import individual semver function

--- packages/nuxi/src/utils/engines.ts
+++ packages/nuxi/src/utils/engines.ts
@@ -1,11 +1,11 @@
 import { engines } from '../../package.json'
 
 export async function checkEngines () {
-  const semver = await import('semver').then(r => r.default || r)
+  const satisfies = await import('semver/functions/satisfies').then(r => r.default)
   const currentNode = process.versions.node
   const nodeRange = engines.node
 
-  if (!semver.satisfies(process.versions.node, engines.node)) {
+  if (!satisfies(process.versions.node, engines.node)) {
     console.warn(`Current version of Node.js (\`${currentNode}\`) is unsupported and might cause issues.\n       Please upgrade to a compatible version (${nodeRange}).`)
   }
 }
