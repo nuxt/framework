904927c767377d7c2b8fd778341dbf24c19f25ed
Daniel Roe <daniel@roe.dev>
2021-10-30 07:21:02 +0100

    refactor: use consistent variable naming

--- packages/nuxi/src/utils/engines.ts
+++ packages/nuxi/src/utils/engines.ts
@@ -5,7 +5,7 @@ export async function checkEngines () {
   const currentNode = process.versions.node
   const nodeRange = engines.node
 
-  if (!satisfies(process.versions.node, engines.node)) {
+  if (!satisfies(currentNode, nodeRange)) {
     console.warn(`Current version of Node.js (\`${currentNode}\`) is unsupported and might cause issues.\n       Please upgrade to a compatible version (${nodeRange}).`)
   }
 }
