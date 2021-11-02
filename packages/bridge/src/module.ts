acb0b90671c9fdacb8a358cc74e4ce2bb8980696
Anthony Fu <anthonyfu117@hotmail.com>
2021-11-02 18:08:20 +0800

    Update module.ts

--- packages/bridge/src/module.ts
+++ packages/bridge/src/module.ts
@@ -17,8 +17,8 @@ export default defineNuxtModule({
   defaults: {
     nitro: true,
     vite: false,
-    app: true,
-    capi: true,
+    app: {},
+    capi: {},
     transpile: true,
     scriptSetup: true,
     autoImports: true,
