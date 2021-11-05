f82bd12c49d8f0e891226eef64a7eb6dd23e4da4
Anthony Fu <anthonyfu117@hotmail.com>
2021-11-05 11:10:34 +0800

    fix(vite): swap `<link>` style on dev ssr

---
+++ packages/vite/src/plugins/dev-ssr-css.ts
@@ -0,0 +1,23 @@
+import { Plugin } from 'vite'
+import { isCSS } from '../utils'
+
+export function devStyleSSRPlugin (rootDir: string): Plugin {
+  return {
+    name: 'nuxt:dev-style-ssr',
+    apply: 'serve',
+    enforce: 'post',
+    transform (code, id) {
+      if (!isCSS(id) || !code.includes('import.meta.hot')) {
+        return
+      }
+
+      let moduleId = id
+      if (moduleId.startsWith(rootDir)) {
+        moduleId = moduleId.slice(rootDir.length)
+      }
+
+      // When dev `<style>` is injected, remove the `<link>` styles from manifest
+      return code + `\ndocument.querySelector(\`link[href="/_nuxt${moduleId}"]\`).forEach(i=>i.remove())`
+    }
+  }
+}
