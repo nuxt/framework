f6914435c2b090a9c884fe010275e4b998407321
Daniel Roe <daniel@roe.dev>
2021-11-10 16:23:39 +0000

    feat: add example renderings of meta tags

--- packages/kit/src/config/schema/_app.ts
+++ packages/kit/src/config/schema/_app.ts
@@ -115,16 +115,20 @@ export default {
    * ```js
    * meta: {
    *  meta: [
+   *    // <meta name="viewport" content="width=device-width, initial-scale=1">
    *    { name: 'viewport', content: 'test content' }
    *  ],
    *  script: [
+   *    // <script src="https://myawesome-lib.js"></script>
    *    { src: 'https://awesome-lib.js' }
    *  ],
    *  link: [
+   *    // <link rel="stylesheet" href="https://myawesome-lib.css">
    *    { rel: 'stylesheet', href: 'https://awesome-lib.css' }
    *  ],
    *  // please note that this is an area that is likely to change
    *  style: [
+   *    // <style type="text/css">:root { color: red }</style>
    *    { children: ':root { color: red }', type: 'text/css' }
    *  ]
    * }
