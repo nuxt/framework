26095106ae1911d3fd68a502935b1d5b7a0d157b
Pooya Parsa <pyapar@gmail.com>
2021-11-15 12:06:36 +0100

    revert test code

--- playground/app.vue
+++ playground/app.vue
@@ -4,9 +4,6 @@
     <h1 class="greeting">
       {{ hello }}, <br>Nuxt 3!
     </h1>
-    <ClientOnly fallback="...">
-      Hello {{ l() }}
-    </ClientOnly>
   </div>
 </template>
 
@@ -19,7 +16,6 @@ const greetings = [
   '你好'
 ]
 const hello = useState('hello', () => greetings[Math.random() * greetings.length | 0])
-const l = () => window.location.pathname
 </script>
 
 <style scoped>
