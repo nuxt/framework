9e74f8a057f26b60b28f5735cb0363c9d86cc1fe
Pooya Parsa <pyapar@gmail.com>
2021-11-16 15:10:25 +0100

    update import

--- packages/kit/src/module/container.ts
+++ packages/kit/src/module/container.ts
@@ -1,9 +1,8 @@
 import { parse, relative } from 'pathe'
 import consola from 'consola'
-import { isNuxt2 } from '@nuxt/kit'
 import type { Nuxt, NuxtPluginTemplate, NuxtTemplate } from '../types/nuxt'
 import { chainFn } from '../utils/task'
-import { addTemplate, addPluginTemplate, addServerMiddleware } from './utils'
+import { isNuxt2, addTemplate, addPluginTemplate, addServerMiddleware } from './utils'
 import { installModule } from './install'
 
 /** Legacy ModuleContainer for backwards compatibility with existing Nuxt 2 modules. */
