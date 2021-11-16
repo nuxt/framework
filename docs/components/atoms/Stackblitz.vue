2a67819fe17b7e6ea5b319bf976a2345b8884c09
Sébastien Chopin <seb@nuxtjs.com>
2021-11-16 13:04:22 +0100

    docs: add stackblitz component
    Will be used later on for examples section

---
+++ docs/components/atoms/Stackblitz.vue
@@ -0,0 +1,25 @@
+<template>
+  <div ref="embed">
+    Loading example...
+  </div>
+</template>
+
+<script>
+const loadSdk = () => import('@stackblitz/sdk' /* webpackChunkName: "stackblitz" */).then(m => m.default || m)
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    options: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  async mounted () {
+    const sdk = await loadSdk()
+    sdk.embedProjectId(this.$refs.embed, this.id, this.options)
+  }
+}
+</script>
