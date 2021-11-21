7002b1933d2efe65163e258f997a97a07131899c
Pooya Parsa <pyapar@gmail.com>
2021-11-21 15:46:08 +0100

    refactor: split schema to `@nuxt/schema` and simplify kit

--- packages/kit/build.config.ts
+++ packages/kit/build.config.ts
@@ -4,27 +4,10 @@ export default defineBuildConfig({
   declaration: true,
   emitCJS: false,
   entries: [
-    {
-      input: 'src/config/schema/index',
-      outDir: 'schema',
-      name: 'config',
-      builder: 'untyped',
-      defaults: {
-        rootDir: '/project/'
-      }
-    },
     'src/index'
   ],
   externals: [
     'webpack',
-    'vite',
-    'nuxt',
-    'nuxt3',
-    // type imports
-    '@vue/compiler-core',
-    'vue-meta',
-    'rollup-plugin-visualizer',
-    'webpack-bundle-analyzer',
-    'vue'
+    'vite'
   ]
 })
