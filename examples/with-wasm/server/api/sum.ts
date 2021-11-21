c9d4713e029e3be851c369e72bd3229602c71ef9
Pooya Parsa <pyapar@gmail.com>
2021-11-21 16:55:27 +0100

    update

--- examples/with-wasm/server/api/sum.ts
+++ examples/with-wasm/server/api/sum.ts
@@ -2,7 +2,7 @@ import { useQuery, lazyHandle } from 'h3'
 
 export default lazyHandle(async () => {
   // @ts-ignore
-  const { exports: { sum } } = await loadWasmInstance(import('~/server/wasm/sum.wasm'))
+  const { exports: { sum } } = await loadWasmInstance(() => import('~/server/wasm/sum.wasm'))
 
   return (req) => {
     const { a = 0, b = 0 } = useQuery(req)
@@ -10,8 +10,8 @@ export default lazyHandle(async () => {
   }
 })
 
-async function loadWasmInstance (importedModule, imports = {}) {
-  const init = await Promise.resolve(importedModule).then(m => m.default || m)
+async function loadWasmInstance (importFn, imports = {}) {
+  const init = await importFn().then(m => m.default || m)
   const { instance } = await init(imports)
   return instance
 }
