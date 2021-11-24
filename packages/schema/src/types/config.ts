25d701cdd521e18ebacec3fd191873af8ecaad3a
pooya parsa <pyapar@gmail.com>
2021-11-24 16:48:20 +0100

    Apply suggestions from code review

--- packages/schema/src/types/config.ts
+++ packages/schema/src/types/config.ts
@@ -1,11 +1,11 @@
 import { ConfigSchema } from '../../schema/config'
 
-/** Normalized Nuxt options */
+/** Normalized Nuxt options available as `nuxt.options.*` */
 export interface NuxtOptions extends ConfigSchema { }
 
 type DeepPartial<T> = T extends Record<string, any> ? { [P in keyof T]?: DeepPartial<T[P]> | T[P] } : T
 
-/** User configuration (may be different from normalized NuxtOptions) */
+/** User configuration in `nuxt.config` file */
 export interface NuxtConfig extends DeepPartial<ConfigSchema> { }
 
 
