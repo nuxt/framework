f9ed010faa9e22af4b87e9bea24ad6a929bf7e6d
Yasser Lahbibi <yasser.lahbibi@apenhet.com>
2021-11-27 13:24:27 +0100

    fix(script-setup): use user-defined scriptSetup options

--- packages/bridge/src/setup.ts
+++ packages/bridge/src/setup.ts
@@ -3,9 +3,10 @@ import scriptSetupPlugin from 'unplugin-vue2-script-setup'
 
 export const setupScriptSetup = () => {
   const nuxt = useNuxt()
-
-  addVitePlugin(scriptSetupPlugin.vite())
-  addWebpackPlugin(scriptSetupPlugin.webpack())
+  const options = nuxt.options.scriptSetup
+  
+  addVitePlugin(scriptSetupPlugin.vite(options))
+  addWebpackPlugin(scriptSetupPlugin.webpack(options))
 
   nuxt.hook('prepare:types', ({ references }) => {
     references.push({
