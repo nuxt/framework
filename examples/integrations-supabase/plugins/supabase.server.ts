ac77c698ca288156ede2af8966359d02c86bfed1
Sébastien Chopin <seb@nuxtjs.com>
2021-11-29 17:04:29 +0100

    fix: support RLS protected tables

--- examples/integrations-supabase/plugins/supabase.server.ts
+++ examples/integrations-supabase/plugins/supabase.server.ts
@@ -8,4 +8,12 @@ export default defineNuxtPlugin(async (nuxtApp) => {
   req.cookies = useCookies(req)
   // Check authenticated user during SSR
   user.value = (await auth.api.getUserByCookie(req)).user
+  // Fix SSR called to RLS protected tables
+  if (user.value) {
+    // https://github.com/supabase/supabase/issues/1735#issuecomment-922284089
+    // @ts-ignore
+    auth.session = () => ({
+      access_token: req.cookies['sb:token']
+    })
+  }
 })
