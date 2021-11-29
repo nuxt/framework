0fbd65cfb3f0f7ef23536e7096dc37444032f9e2
Daniel Roe <daniel@roe.dev>
2021-11-29 10:26:55 +0000

    feat: extract `filterByKey` function and improve type helps

--- packages/nuxt3/src/app/composables/ssr.ts
+++ packages/nuxt3/src/app/composables/ssr.ts
@@ -1,28 +1,35 @@
 import { useNuxtApp } from '#app'
 
-export const useReqHeaders = (opts?: {
+function filterByKey<T extends Record<string, any>, IncludeKey extends keyof T = keyof T, ExcludeKey extends keyof T = never> (source: T, opts?: {
+  exclude?: ExcludeKey[]
+  include?: IncludeKey[]
+}) {
+  return Object.fromEntries(Object.entries(source).filter(([key]) => {
+    key = key.toLowerCase()
+    if (opts.exclude && opts.exclude.includes(key as any)) {
+      return false
+    }
+    if (opts.include && !opts.include.includes(key as any)) {
+      return false
+    }
+    return true
+  })) as Pick<T, Exclude<IncludeKey, ExcludeKey>>
+}
+
+export const useReqHeaders = <K extends string = string>(opts?: {
   exclude?: string[]
-  include?: string[]
-}): Readonly<Record<string, string>> => {
-  if (process.client) { return {} }
+  include?: K[]
+}): Readonly<Partial<Record<K, string>>> => {
+  if (process.client) { return {} as Readonly<Partial<Record<K, string>>> }
 
   const nuxtApp = useNuxtApp()
   const headers: Record<string, string> = nuxtApp.ssrContext?.req.headers ?? {}
   if (!opts) {
-    return headers
+    return headers as Readonly<Partial<Record<K, string>>>
   }
 
-  opts.exclude = opts.exclude?.map(key => key.toLowerCase())
-  opts.include = opts.include?.map(key => key.toLowerCase())
-
-  return Object.fromEntries(Object.entries(headers).filter(([key]) => {
-    key = key.toLowerCase()
-    if (opts.exclude && opts.exclude.includes(key)) {
-      return false
-    }
-    if (opts.include && !opts.include.includes(key)) {
-      return false
-    }
-    return true
-  }))
+  return filterByKey(headers, {
+    exclude: opts.exclude?.map(key => key.toLowerCase()),
+    include: opts.include?.map(key => key.toLowerCase())
+  }) as Readonly<Partial<Record<K, string>>>
 }
