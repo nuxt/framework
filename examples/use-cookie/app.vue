768de24e655300f9ce95a10d2ca213ddc8ab5999
Anthony Fu <anthonyfu117@hotmail.com>
2021-12-07 16:32:52 +0800

    chore: update example

--- examples/use-cookie/app.vue
+++ examples/use-cookie/app.vue
@@ -1,10 +1,12 @@
-<script setup>
-const user = useCookie('user')
-const logins = useCookie('logins')
+<script setup lang="ts">
+const user = useCookie<{ name: string }>('user')
+const logins = useCookie<number>('logins')
+
+const name = ref('')
 
 const login = () => {
   logins.value = (logins.value || 0) + 1
-  user.value = { name: 'CookieLover.' + new Date().getMilliseconds() }
+  user.value = { name: name.value }
 }
 
 const logout = () => {
@@ -13,18 +15,44 @@ const logout = () => {
 </script>
 
 <template>
-  <div v-if="user">
-    <h1>Welcome {{ user.name }}</h1>
-    <button @click="logout">
-      Logout
-    </button>
-    <hr>
-    You have logged in {{ logins }} times!
-  </div>
-  <div v-else>
-    Click here to login:
-    <button @click="login">
-      Login
-    </button>
+  <div class="relative font-sans">
+    <div class="container mx-auto py-8">
+      <div class="flex items-bottom">
+        <img src="https://raw.githubusercontent.com/nuxt/framework/main/playground/assets/logo.svg" h="12">
+        <div class="mt-auto text-2xl mx-3 m-0.5 flex">
+          <div class="op-50">
+            examples/
+          </div><a href="https://v3.nuxtjs.org/docs/usage/cookies" class="hover:text-green5 hover:underline">useCookie</a>
+        </div>
+      </div>
+      <NCard class="mt-2 p-5 flex flex-col gap-2">
+        <template v-if="user">
+          <h1 class="text-3xl">
+            Welcome, {{ user.name }}! 👋
+          </h1>
+          <div>
+            <NTip n="green6" icon="carbon:idea" class="inline-flex">
+              You have logged in <b>{{ logins }} times</b>!
+            </NTip>
+          </div>
+          <div class="mt-3">
+            <NButton n="red" icon="carbon:logout" @click="logout">
+              Log out
+            </NButton>
+          </div>
+        </template>
+        <template v-else>
+          <h1 class="text-3xl">
+            Login
+          </h1>
+          <NTextInput v-model="name" placeholder="Enter your name..." @keypress.enter="login()" />
+          <div class="mt-3">
+            <NButton n="green6" icon="carbon:user" :disabled="!name" @click="login">
+              Log in
+            </NButton>
+          </div>
+        </template>
+      </NCard>
+    </div>
   </div>
 </template>
