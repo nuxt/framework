a90775459604cb3f93b2a0258e23a8c7deba932d
Anthony Fu <anthonyfu117@hotmail.com>
2021-12-09 23:06:58 +0800

    chore: update

--- examples/locale/app.vue
+++ examples/locale/app.vue
@@ -1,17 +1,55 @@
-<script setup>
+<script setup lang="ts">
 const locales = useLocales()
 const locale = useLocale()
 const date = useLocaleDate(new Date('2016-10-26') /* NUXT_BIRTHDAY */)
+
+const interacted = ref(false)
+
+watch(locale, () => {
+  interacted.value = true
+})
+
+const openInEditor = () => {
+  fetch('/__open-in-editor?file=app.vue')
+}
 </script>
 
 <template>
-  <div>
-    <h2>Nuxt birthday: {{ date }}</h2>
-    <label for="locale-chooser">Locale: </label>
-    <select id="locale-chooser" v-model="locale">
-      <option v-for="l of locales" :key="l" :value="l">
-        {{ l }}
-      </option>
-    </select>
+  <div class="relative font-sans" n="green6">
+    <div class="container max-w-200 mx-auto py-10 px-4">
+      <div class="flex flex-col gap-2 items-center">
+        <img src="https://raw.githubusercontent.com/nuxt/framework/main/playground/assets/logo.svg" h="12">
+        <div class="text-xl flex">
+          <div class="op-50">
+            examples/
+          </div><a href="https://github.com/nuxt/framework/tree/main/examples/locale" class="hover:text-green5 hover:underline">locale</a>
+        </div>
+      </div>
+      <NCard class="mt-8 p-6 flex flex-col h-50 gap-2 text-center">
+        <h1 class="text-xl opacity-50">
+          Nuxt birthday
+        </h1>
+        <p class="text-4xl">
+          {{ date }}
+        </p>
+        <label for="locale-chooser">Locale</label>
+        <select id="locale-chooser" v-model="locale" class="m-auto w-50 border n-border-base rounded p-1">
+          <option v-for="l of locales" :key="l" :value="l">
+            {{ l }}
+          </option>
+        </select>
+      </NCard>
+
+      <!-- Tips -->
+      <div :class="interacted ? 'opacity-100' : 'opacity-0'" class="transition py-5 flex items-center gap-2 text-gray-400">
+        <NIcon icon="carbon-idea" class="text-xl flex-none" />
+        <div>
+          You can right click to "View Page Source" and see that Nuxt renders the correct date in SSR based on visitor's locale.
+        </div>
+        <NButton icon="carbon-edit" class="flex-none" @click="openInEditor">
+          Open in Editor
+        </NButton>
+      </div>
+    </div>
   </div>
 </template>
