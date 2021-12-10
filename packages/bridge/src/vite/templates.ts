ef1b467eda36383470a334fc857c08de22513d0a
Daniel Roe <daniel@roe.dev>
2021-12-10 14:00:17 +0000

    fix: don't filter

--- packages/bridge/src/vite/templates.ts
+++ packages/bridge/src/vite/templates.ts
@@ -24,7 +24,7 @@ export const middlewareTemplate = {
         filePath: resolve(srcDir, dir.middleware, m.src),
         id: m.name || m.src.replace(/[\\/]/g, '/').replace(/\.(js|ts)$/, '')
       }
-    }).filter(Boolean) as Array<{ filePath: string, id: string }>
+    })
     return `${_middleware.map(m => `import $${hash(m.id)} from '${m.filePath}'`).join('\n')}
 const middleware = {
 ${_middleware.map(m => `  ['${m.id}']: $${hash(m.id)}`).join(',\n')}
