6a6fca6175d5e1678b2faa511ade3b991ca2096d
Daniel Roe <daniel@roe.dev>
2021-12-15 14:28:58 +0000

    fix(nitro): correct `#assets` type declaration

--- packages/nitro/types/shims.d.ts
+++ packages/nitro/types/shims.d.ts
@@ -5,9 +5,12 @@ declare module '#storage' {
 
 declare module '#assets' {
   export interface AssetMeta { type?: string, etag?: string, mtime?: string }
-  export function readAsset<T = any> (id: string): Promise<T>
-  export function statAsset (id: string): Promise<AssetMeta>
-  export function getKeys() : Promise<string[]>
+
+  export const assets: {
+    readAsset<T = any> (id: string): Promise<T>
+    statAsset (id: string): Promise<AssetMeta>
+    getKeys() : Promise<string[]>
+  }
 }
 
 declare module '#config' {
