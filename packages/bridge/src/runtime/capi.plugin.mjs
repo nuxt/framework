ac27e4ce76594cac7aac033252a4b185723dd69d
Daniel Roe <daniel@roe.dev>
2021-12-20 15:19:58 +0000

    refactor: use hookable's `callHookWith`

--- packages/bridge/src/runtime/capi.plugin.mjs
+++ packages/bridge/src/runtime/capi.plugin.mjs
@@ -10,12 +10,9 @@ export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.nuxt2Context.app.setup = function (...args) {
     const result = _originalSetup instanceof Function ? _originalSetup(...args) : {}
 
-    const hooks = nuxtApp.hooks._hooks['vue:setup']
-    if (hooks) {
-      const hookResult = hooks.map(hook => hook())
-      if (process.dev && hookResult && hookResult.some(i => i && 'then' in i)) {
-        console.error('[nuxt] Error in `vue:setup`. Callbacks must be synchronous.')
-      }
+    const hookResult = useNuxtApp().hooks.callHookWith(hooks => hooks.map(hook => hook()), 'vue:setup')
+    if (process.dev && hookResult && hookResult.some(i => i && 'then' in i)) {
+      console.error('[nuxt] Error in `vue:setup`. Callbacks must be synchronous.')
     }
 
     return result
--- packages/nuxt3/src/app/components/nuxt-root.vue
+++ packages/nuxt3/src/app/components/nuxt-root.vue
@@ -9,9 +9,7 @@ import { useNuxtApp } from '#app'
 
 export default {
   setup () {
-    const hooks = useNuxtApp().hooks._hooks['vue:setup']
-    if (!hooks) { return }
-    const results = hooks.map(hook => hook())
+    const results = useNuxtApp().hooks.callHookWith(hooks => hooks.map(hook => hook()), 'vue:setup')
     if (process.dev && results && results.some(i => i && 'then' in i)) {
       console.error('[nuxt] Error in `vue:setup`. Callbacks must be synchronous.')
     }
