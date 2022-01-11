c438a5ad00f177b70e5d2208420dd56063d6087a
Daniel Roe <daniel@roe.dev>
2022-01-11 16:38:27 +0000

    feat(nuxt3): add support for `definePageMeta` macro

---
+++ packages/nuxt3/src/pages/macros.ts
@@ -0,0 +1,94 @@
+import { createUnplugin } from 'unplugin'
+import { parseQuery, parseURL, withQuery } from 'ufo'
+import { findStaticImports, findExports } from 'mlly'
+
+export interface TransformMacroPluginOptions {
+  macros: Record<string, string>
+}
+
+export const TransformMacroPlugin = createUnplugin((options: TransformMacroPluginOptions) => {
+  return {
+    name: 'nuxt-pages-macros-transform',
+    enforce: 'post',
+    transformInclude (id) {
+      // We only process SFC files for macros
+      return parseURL(id).pathname.endsWith('.vue')
+    },
+    transform (code, id) {
+      const { search } = parseURL(id)
+
+      // Tree-shake out any runtime references to the macro.
+      // We do this first as it applies to all files, not just those with the query
+      for (const macro in options.macros) {
+        const match = code.match(new RegExp(`\\b${macro}\\s*\\(\\s*`))?.[0]
+        if (match) {
+          code = code.replace(match, `/*#__PURE__*/ false && ${match}`)
+        }
+      }
+
+      if (!parseQuery(search).macro) { return code }
+
+      // [webpack] Re-export any imports from script blocks in the components
+      // with workaround for vue-loader bug: https://github.com/vuejs/vue-loader/pull/1911
+      const scriptImport = findStaticImports(code).find(i => parseQuery(i.specifier.replace('?macro=true', '')).type === 'script')
+      if (scriptImport) {
+        const specifier = withQuery(scriptImport.specifier.replace('?macro=true', ''), { macro: 'true' })
+        return `export { meta } from "${specifier}"`
+      }
+
+      // [webpack] Export named exports rather than the default (component)
+      const defaultExport = code.match(/export \{ default \} from ['"]([^'"]+)['"]/)
+      if (defaultExport) {
+        return defaultExport[0].replace('{ default }', '*')
+      }
+
+      // ensure we tree-shake any _other_ exports out of the macro script
+      const currentExports = findExports(code)
+      const macroExports = Object.values(options.macros)
+      for (const match of currentExports) {
+        if (!macroExports.includes(match.name)) {
+          code = code.replace(match.code, '/*#__PURE__*/ false &&')
+        }
+      }
+
+      for (const macro in options.macros) {
+        // Skip already-processed macros
+        if (currentExports.some(e => e.name === options.macros[macro])) { continue }
+
+        const { 0: match, index = 0 } = code.match(new RegExp(`\\b${macro}\\s*\\(\\s*`)) || {} as RegExpMatchArray
+        const macroContent = match ? extractObject(code.slice(index + match.length)) : 'undefined'
+
+        code += `\nexport const ${options.macros[macro]} = ${macroContent}`
+      }
+
+      return code
+    }
+  }
+})
+
+const starts = {
+  '{': '}',
+  '[': ']',
+  '(': ')',
+  '<': '>',
+  '"': '"',
+  "'": "'"
+}
+
+function extractObject (code: string) {
+  // Strip comments
+  code = code.replace(/\/\/.*$/gm, '')
+
+  const stack = []
+  let result = ''
+  do {
+    if (stack[0] === code[0] && result.slice(-1) !== '\\') {
+      stack.shift()
+    } else if (code[0] in starts) {
+      stack.unshift(starts[code[0]])
+    }
+    result += code[0]
+    code = code.slice(1)
+  } while (stack.length && code.length)
+  return result
+}
--- packages/nuxt3/src/pages/runtime/composables.ts
+++ packages/nuxt3/src/pages/runtime/composables.ts
@@ -8,3 +8,21 @@ export const useRouter = () => {
 export const useRoute = () => {
   return useNuxtApp()._route as RouteLocationNormalizedLoaded
 }
+
+export interface PageMeta {
+  [key: string]: any
+}
+
+const warnRuntimeUsage = (method: string) =>
+  console.warn(
+    `${method}() is a compiler-hint helper that is only usable inside ` +
+      '<script setup> of a single file component. Its arguments should be ' +
+      'compiled away and passing it at runtime has no effect.'
+  )
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const definePageMeta = (meta: PageMeta): void => {
+  if (!process.dev) {
+    warnRuntimeUsage('definePageMeta')
+  }
+}
