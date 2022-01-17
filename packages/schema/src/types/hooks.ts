a0288ebc59ade6b6ce2e6804497dd1520b378be8
Daniel Roe <daniel@roe.dev>
2022-01-17 19:35:30 +0000

    fix: restore `nitro:generate` hook type

--- packages/schema/src/types/hooks.ts
+++ packages/schema/src/types/hooks.ts
@@ -78,6 +78,7 @@ export interface NuxtHooks {
   // @nuxt/nitro
   'nitro:document': (template: { src: string, contents: string }) => HookResult
   'nitro:context': (context: any) => HookResult
+  'nitro:generate': (context: any) => HookResult
 
   // @nuxt/cli
   'generate:cache:ignore': (ignore: string[]) => HookResult
