8ba245fc32cdedbad90054914c45e8eb73d3d59e
Pooya Parsa <pyapar@gmail.com>
2022-01-17 19:54:27 +0100

    refactor: use joinURL for adding basePath

--- packages/nitro/src/presets/browser.ts
+++ packages/nitro/src/presets/browser.ts
@@ -1,17 +1,19 @@
 import { existsSync, promises as fsp } from 'fs'
 import { resolve } from 'pathe'
 import consola from 'consola'
+import { joinURL } from 'ufo'
 import { extendPreset, prettyPath } from '../utils'
 import { NitroPreset, NitroContext, NitroInput } from '../context'
 import { worker } from './worker'
 
 export const browser: NitroPreset = extendPreset(worker, (input: NitroInput) => {
+  // TODO: Join base at runtime
   const basePath = input._nuxt.basePath
 
   const script = `<script>
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
-    navigator.serviceWorker.register('${basePath}sw.js');
+    navigator.serviceWorker.register('${joinURL(basePath, 'sw.js')}');
   });
 }
 </script>`
@@ -21,11 +23,11 @@ if ('serviceWorker' in navigator) {
 <html>
 <head>
   <meta charset="utf-8">
-  <link rel="prefetch" href="${basePath}sw.js">
-  <link rel="prefetch" href="${basePath}_server/index.mjs">
+  <link rel="prefetch" href="${joinURL(basePath, 'sw.js')}">
+  <link rel="prefetch" href="${joinURL(basePath, '_server/index.mjs')}">
   <script>
   async function register () {
-    const registration = await navigator.serviceWorker.register('${basePath}sw.js')
+    const registration = await navigator.serviceWorker.register('${joinURL(basePath, 'sw.js')}')
     await navigator.serviceWorker.ready
     registration.active.addEventListener('statechange', (event) => {
       if (event.target.state === 'activated') {
@@ -62,7 +64,7 @@ if ('serviceWorker' in navigator) {
         tmpl.contents = tmpl.contents.replace('</body>', script + '</body>')
       },
       async 'nitro:compiled' ({ output }: NitroContext) {
-        await fsp.writeFile(resolve(output.publicDir, 'sw.js'), `self.importScripts('${input._nuxt.basePath}_server/index.mjs');`, 'utf8')
+        await fsp.writeFile(resolve(output.publicDir, 'sw.js'), `self.importScripts('${joinURL(basePath, '_server/index.mjs')}');`, 'utf8')
 
         // Temp fix
         if (!existsSync(resolve(output.publicDir, 'index.html'))) {
