a1f57e7f0ab7bda3105e3e31a583bf250603ec8a
Harlan Wilton <harlan@harlanzw.com>
2022-01-18 22:22:58 +1100

    Revert "fix: extract the needed types from http modules"
    This reverts commit 9229d803f99533b7cf97b20c226f19dcb92a5a10.

--- examples/hello-world/nuxt.config.ts
+++ examples/hello-world/nuxt.config.ts
@@ -1,11 +1,4 @@
 import { defineNuxtConfig } from 'nuxt3'
-import {useNuxt} from "@nuxt/kit";
 
 export default defineNuxtConfig({
-  hooks: {
-    listen (server, listener) {
-      const nuxt = useNuxt()
-      console.log(nuxt.server.app, server.address(), listener)
-    },
-  }
 })
