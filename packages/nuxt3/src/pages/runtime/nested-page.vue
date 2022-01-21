e26f8ae6d8244277f0c2b11304e67cd13b234881
Daniel Roe <daniel@roe.dev>
2022-01-21 16:47:52 +0000

    fix(nuxt3): allow customising page keys

--- packages/nuxt3/src/pages/runtime/nested-page.vue
+++ packages/nuxt3/src/pages/runtime/nested-page.vue
@@ -1,11 +1,26 @@
 <template>
-  <RouterView v-slot="{ Component }">
-    <component :is="Component" :key="$route.path" />
+  <RouterView v-slot="{ Component, route }">
+    <component :is="Component" :key="getKeyFor(childKey || route.meta.key, route)" />
   </RouterView>
 </template>
 
-<script>
+<script lang="ts">
+import type { RouteLocationNormalizedLoaded } from 'vue-router'
+
+const getKeyFor = (key: string | ((route: RouteLocationNormalizedLoaded) => string), route?: RouteLocationNormalizedLoaded) => {
+  return key && typeof key === 'function' ? key(route) : key
+}
+
 export default {
-  name: 'NuxtNestedPage'
+  name: 'NuxtNestedPage',
+  props: {
+    childKey: {
+      type: [Function, String] as unknown as () => string | ((route: RouteLocationNormalizedLoaded) => string),
+      default: null
+    }
+  },
+  setup () {
+    return { getKeyFor }
+  }
 }
 </script>
