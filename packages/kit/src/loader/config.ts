93d7aae536a31e7c451a057ee0c23e3c141277bd
Pooya Parsa <pyapar@gmail.com>
2022-01-31 13:45:21 +0100

    refactor: use c12 for loading configuration

--- packages/kit/src/loader/config.ts
+++ packages/kit/src/loader/config.ts
@@ -1,16 +1,14 @@
-import { existsSync } from 'fs'
 import { resolve } from 'pathe'
-import defu from 'defu'
 import { applyDefaults } from 'untyped'
-import * as rc from 'rc9'
+import { loadConfig, DotenvOptions } from 'c12'
 import type { NuxtOptions } from '@nuxt/schema'
 import { NuxtConfigSchema } from '@nuxt/schema'
-import { tryResolveModule, requireModule, scanRequireTree } from '../internal/cjs'
-import { setupDotenv, DotenvOptions } from '../internal/dotenv'
+// TODO
+// import { tryResolveModule, requireModule, scanRequireTree } from '../internal/cjs'
 
 export interface LoadNuxtConfigOptions {
   /** Your project root directory (either absolute or relative to the current working directory). */
-  rootDir?: string
+  cwd?: string
 
   /** The path to your `nuxt.config` file (either absolute or relative to your project `rootDir`). */
   configFile?: string
@@ -23,41 +21,17 @@ export interface LoadNuxtConfigOptions {
 }
 
 export async function loadNuxtConfig (opts: LoadNuxtConfigOptions): Promise<NuxtOptions> {
-  const rootDir = resolve(process.cwd(), opts.rootDir || '.')
+  const rootDir = resolve(process.cwd(), opts.cwd || '.')
 
-  if (opts.dotenv !== false) {
-    await setupDotenv({ rootDir, ...opts.dotenv })
-  }
-
-  const nuxtConfigFile = tryResolveModule(resolve(rootDir, opts.configFile || 'nuxt.config'))
-
-  let nuxtConfig: any = {}
-
-  if (nuxtConfigFile && existsSync(nuxtConfigFile)) {
-    nuxtConfig = requireModule(nuxtConfigFile, { clearCache: true })
-
-    if (typeof nuxtConfig === 'function') {
-      nuxtConfig = await nuxtConfig(opts)
-    }
-
-    nuxtConfig = { ...nuxtConfig }
-    nuxtConfig._nuxtConfigFile = nuxtConfigFile
-    nuxtConfig._nuxtConfigFiles = Array.from(scanRequireTree(nuxtConfigFile))
-  }
-
-  // Combine configs
-  // Priority: configOverrides > nuxtConfig > .nuxtrc > .nuxtrc (global)
-  nuxtConfig = defu(
-    opts.config,
-    nuxtConfig,
-    rc.read({ name: '.nuxtrc', dir: rootDir }),
-    rc.readUser('.nuxtrc')
-  )
-
-  // Set rootDir
-  if (!nuxtConfig.rootDir) {
-    nuxtConfig.rootDir = rootDir
-  }
+  const { config: nuxtConfig } = await loadConfig({
+    cwd: rootDir,
+    name: 'nuxt',
+    configFile: 'nuxt.config',
+    rcFile: '.nuxtrc',
+    dotenv: opts.dotenv,
+    globalRc: true,
+    overrides: opts.config
+  })
 
   // Resolve and apply defaults
   return applyDefaults(NuxtConfigSchema, nuxtConfig) as NuxtOptions
