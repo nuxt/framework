2c7bfc960632d5178f141f7efd5560531c464e27
Daniel Roe <daniel@roe.dev>
2022-02-05 13:05:18 +0000

    test: update lambda test and add v2 test

--- test/presets/lambda.test.ts
+++ test/presets/lambda.test.ts
@@ -1,6 +1,30 @@
 import { resolve } from 'pathe'
 import { describe } from 'vitest'
+import type { APIGatewayProxyEvent, APIGatewayProxyEventV2 } from 'aws-lambda'
 import { setupTest, testNitroBehavior, importModule } from './_tests'
+
+describe('nitro:preset:lambda_v1', () => {
+  const ctx = setupTest('lambda_v1')
+  testNitroBehavior(ctx, async () => {
+    const { handler } = await importModule(resolve(ctx.outDir, 'server/index.mjs'))
+    return async ({ url: rawRelativeUrl, headers, method, body }) => {
+      // creating new URL object to parse query easier
+      const url = new URL(`https://example.com${rawRelativeUrl}`)
+      const queryStringParameters = Object.fromEntries(url.searchParams.entries())
+      const event: Partial<APIGatewayProxyEvent> = {
+        path: url.pathname,
+        headers: headers || {},
+        httpMethod: method || 'GET',
+        queryStringParameters,
+        body: body || ''
+      }
+      const res = await handler(event)
+      return {
+        data: res.body
+      }
+    }
+  })
+})
 
 describe('nitro:preset:lambda', () => {
   const ctx = setupTest('lambda')
@@ -10,10 +34,18 @@ describe('nitro:preset:lambda', () => {
       // creating new URL object to parse query easier
       const url = new URL(`https://example.com${rawRelativeUrl}`)
       const queryStringParameters = Object.fromEntries(url.searchParams.entries())
-      const event = {
-        path: url.pathname,
+      const event: Partial<APIGatewayProxyEventV2> = {
+        rawPath: url.pathname,
         headers: headers || {},
-        method: method || 'GET',
+        requestContext: {
+          ...Object.fromEntries([['accountId'], ['apiId'], ['domainName'], ['domainPrefix']]),
+          http: {
+            path: url.pathname,
+            protocol: 'http',
+            ...Object.fromEntries([['userAgent'], ['sourceIp']]),
+            method: method || 'GET'
+          }
+        },
         queryStringParameters,
         body: body || ''
       }
