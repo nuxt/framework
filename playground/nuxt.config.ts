4c91ff8c782b72e26ba1278112340f20b76f7222
Pooya Parsa <pyapar@gmail.com>
2022-02-07 22:27:29 +0100

    feat(kit): `createResolver` utility

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,7 @@
 import { defineNuxtConfig } from 'nuxt3'
+import { createResolver } from '@nuxt/kit'
 
+setTimeout(() => { createResolver(import.meta.url).resolvePath('ufo').then(console.log) }, 2000)
 export default defineNuxtConfig({
   extends: './base',
   modules: [
