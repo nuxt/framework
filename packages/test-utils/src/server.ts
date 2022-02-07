cfd9ee7be8cebfdbe16194b45f898342eeb16933
Anthony Fu <anthonyfu117@hotmail.com>
2022-02-07 18:56:15 +0800

    fix: improve test listening

--- packages/test-utils/src/server.ts
+++ packages/test-utils/src/server.ts
@@ -1,25 +1,49 @@
 import { resolve } from 'path'
+import { createServer, AddressInfo } from 'net'
 import { execa } from 'execa'
 import { getPort } from 'get-port-please'
 import { fetch as _fetch, $fetch as _$fetch, FetchOptions } from 'ohmyfetch'
 import { useTestContext } from './context'
 
+// TODO: use the export from `get-port-please`
+function checkPort (port: number, host: string): Promise<number|false> {
+  return new Promise((resolve) => {
+    const server = createServer()
+    server.unref()
+    server.on('error', () => { resolve(false) })
+    server.listen(port, host, () => {
+      const { port } = server.address() as AddressInfo
+      server.close(() => { resolve(port) })
+    })
+  })
+}
+
 export async function listen () {
   const ctx = useTestContext()
-  const port = await getPort({})
+  const host = process.env.HOST || '0.0.0.0'
+  const port = await getPort({ host })
+
   ctx.url = 'http://localhost:' + port
   execa('node', [
     // @ts-expect-error TODO: TS2339: Property 'nitro' does not exist on type 'NuxtOptions'
     resolve(ctx.nuxt.options.nitro.output.dir, 'server/index.mjs')
   ], {
     env: {
-      PORT: String(port)
+      PORT: String(port),
+      NODE_ENV: 'test'
     }
   })
-  // TODO: Wait for listen event
-  await new Promise(resolve => setTimeout(resolve, 1000))
-  console.log(ctx.url)
-  await new Promise(resolve => setTimeout(resolve, 10000))
+
+  const TRIES = 50
+  const DELAY = 100
+
+  for (let i = TRIES; i; i--) {
+    await new Promise(resolve => setTimeout(resolve, DELAY))
+    // wait until port is in used
+    if (await checkPort(port, host) === false) {
+      return
+    }
+  }
 }
 
 export function fetch (path: string, options?: any) {
