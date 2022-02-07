d425a76b279cd2e02eb9e95d9022a04c074e4bdb
Pooya Parsa <pyapar@gmail.com>
2022-02-07 22:28:06 +0100

    revert test changes

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,7 +1,5 @@
 import { defineNuxtConfig } from 'nuxt3'
-import { createResolver } from '@nuxt/kit'
 
-setTimeout(() => { createResolver(import.meta.url).resolvePath('ufo').then(console.log) }, 2000)
 export default defineNuxtConfig({
   extends: './base',
   modules: [
