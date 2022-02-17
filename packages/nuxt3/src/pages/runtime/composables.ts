e1794c9b232f01f09e6682d6ab99bde33f2312c6
Daniel Roe <daniel@roe.dev>
2022-02-17 09:30:00 +0000

    fix: relocate bridge access pattern

--- packages/nuxt3/src/pages/runtime/composables.ts
+++ packages/nuxt3/src/pages/runtime/composables.ts
@@ -71,7 +71,7 @@ export const navigateTo = (to: RouteLocationRaw) => {
   if (isProcessingMiddleware()) {
     return to
   }
-  const router: Router = process.server ? useRouter() : (window as any).$nuxt.config?.globalProperties.$router || (window as any).$nuxt.context?.app.router
+  const router: Router = process.server ? useRouter() : (window as any).$nuxt.config?.globalProperties.$router
   return router.push(to)
 }
 
