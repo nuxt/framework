87596c39b0c495e656e63dc3177a0318055d72f4
Pooya Parsa <pyapar@gmail.com>
2022-02-18 13:20:35 +0100

    webpack test

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -2,7 +2,7 @@ import { fileURLToPath } from 'url'
 import { describe, expect, it } from 'vitest'
 import { setup, $fetch } from '@nuxt/test-utils'
 
-describe('Basic tests', async () => {
+describe('fixtures:basic', async () => {
   await setup({
     rootDir: fileURLToPath(new URL('./fixtures/basic', import.meta.url)),
     server: true
--- test/bridge.test.ts
+++ test/bridge.test.ts
@@ -2,7 +2,7 @@ import { fileURLToPath } from 'url'
 import { describe, expect, it } from 'vitest'
 import { setup, $fetch } from '@nuxt/test-utils'
 
-describe('Bridge tests', async () => {
+describe('fixtures:bridge', async () => {
   await setup({
     rootDir: fileURLToPath(new URL('./fixtures/bridge', import.meta.url)),
     server: true
