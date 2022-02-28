29f689f2fc2efc4b2d4da6bb42017146af90f663
Anthony Fu <anthonyfu117@hotmail.com>
2022-02-28 22:19:13 +0800

    fix: support sourcemap for `nuxt:pages-macros-transform`

--- packages/nuxt3/src/pages/macros.ts
+++ packages/nuxt3/src/pages/macros.ts
@@ -1,6 +1,7 @@
 import { createUnplugin } from 'unplugin'
 import { parseQuery, parseURL, withQuery } from 'ufo'
 import { findStaticImports, findExports } from 'mlly'
+import MagicString from 'magic-string'
 
 export interface TransformMacroPluginOptions {
   macros: Record<string, string>
@@ -16,20 +17,28 @@ export const TransformMacroPlugin = createUnplugin((options: TransformMacroPlugi
       return pathname.endsWith('.vue') || !!parseQuery(search).macro
     },
     transform (code, id) {
-      const originalCode = code
+      let changed = false
+      const s = new MagicString(code)
       const { search } = parseURL(id)
 
       // Tree-shake out any runtime references to the macro.
       // We do this first as it applies to all files, not just those with the query
       for (const macro in options.macros) {
-        const match = code.match(new RegExp(`\\b${macro}\\s*\\(\\s*`))?.[0]
-        if (match) {
-          code = code.replace(match, `/*#__PURE__*/ false && ${match}`)
+        const match = code.match(new RegExp(`\\b${macro}\\s*\\(\\s*`))
+        if (match?.[0]) {
+          changed = true
+          s.overwrite(match.index, match.index + match[0].length, `/*#__PURE__*/ false && ${match[0]}`)
         }
       }
 
       if (!parseQuery(search).macro) {
-        return originalCode === code ? undefined : code
+        if (changed) {
+          return {
+            code: s.toString(),
+            map: s.generateMap({ hires: true })
+          }
+        }
+        return null
       }
 
       // [webpack] Re-export any imports from script blocks in the components
@@ -37,33 +46,52 @@ export const TransformMacroPlugin = createUnplugin((options: TransformMacroPlugi
       const scriptImport = findStaticImports(code).find(i => parseQuery(i.specifier.replace('?macro=true', '')).type === 'script')
       if (scriptImport) {
         const specifier = withQuery(scriptImport.specifier.replace('?macro=true', ''), { macro: 'true' })
-        return `export { meta } from "${specifier}"`
+        s.overwrite(0, code.length, `export { meta } from "${specifier}"`)
+        return {
+          code: s.toString(),
+          map: s.generateMap()
+        }
       }
 
       const currentExports = findExports(code)
       for (const match of currentExports) {
-        if (match.type !== 'default') { continue }
+        if (match.type !== 'default') {
+          continue
+        }
         if (match.specifier && match._type === 'named') {
           // [webpack] Export named exports rather than the default (component)
-          return code.replace(match.code, `export {${Object.values(options.macros).join(', ')}} from "${match.specifier}"`)
+          s.overwrite(match.start, match.end, `export {${Object.values(options.macros).join(', ')}} from "${match.specifier}"`)
+          return {
+            code: s.toString(),
+            map: s.generateMap()
+          }
         } else if (!options.dev) {
+          changed = true
           // ensure we tree-shake any _other_ default exports out of the macro script
-          code = code.replace(match.code, '/*#__PURE__*/ false &&')
-          code += '\nexport default {}'
+          s.overwrite(match.start, match.end, '/*#__PURE__*/ false &&')
+          s.append('\nexport default {}')
         }
       }
 
       for (const macro in options.macros) {
         // Skip already-processed macros
-        if (currentExports.some(e => e.name === options.macros[macro])) { continue }
+        if (currentExports.some(e => e.name === options.macros[macro])) {
+          continue
+        }
 
         const { 0: match, index = 0 } = code.match(new RegExp(`\\b${macro}\\s*\\(\\s*`)) || {} as RegExpMatchArray
         const macroContent = match ? extractObject(code.slice(index + match.length)) : 'undefined'
 
-        code += `\nexport const ${options.macros[macro]} = ${macroContent}`
+        changed = true
+        s.append(`\nexport const ${options.macros[macro]} = ${macroContent}`)
       }
 
-      return code
+      if (changed) {
+        return {
+          code: s.toString(),
+          map: s.generateMap()
+        }
+      }
     }
   }
 })
