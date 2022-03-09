335a6cf1cc497f7a0811722b011cd2e02c73a820
Daniel Roe <daniel@roe.dev>
2022-03-09 19:03:32 +0000

    Update packages/nuxt3/src/app/components/nuxt-root.vue
    Co-authored-by: pooya parsa <pyapar@gmail.com>

--- packages/nuxt3/src/app/components/nuxt-root.vue
+++ packages/nuxt3/src/app/components/nuxt-root.vue
@@ -23,7 +23,7 @@ if (process.dev && results && results.some(i => i && 'then' in i)) {
 // error handling
 const error = useError()
 onErrorCaptured((err, target, info) => {
-  nuxtApp.hooks.callHook('vue:error', err, target, info).catch(e => console.error('[nuxt] Error in `vue:error` hook', e))
+  nuxtApp.hooks.callHook('vue:error', err, target, info).catch(hookError => console.error('[nuxt] Error in `vue:error` hook', hookError))
   if (process.server) {
     callWithNuxt(nuxtApp, throwError, [err])
   }
