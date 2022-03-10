674bb437f21c6c8ca4b682d876e520248ab75612
Daniel Roe <daniel@roe.dev>
2022-03-10 10:21:45 +0000

    fix: improve default error template

--- packages/nuxt3/src/app/components/nuxt-error-page.vue
+++ packages/nuxt3/src/app/components/nuxt-error-page.vue
@@ -11,9 +11,11 @@ const props = defineProps({
 })
 
 const err = props.error
-const handler = err.statusCode === 404 ? error404 : process.dev ? errorDev : error500
+const is404 = Number(err.statusCode) === 404
+const handler = is404 ? error404 : process.dev ? errorDev : error500
 const html = handler({
-  description: err.message,
-  statusCode: err.statusCode || 500
+  statusMessage: err.statusMessage,
+  description: err.description || (is404 ? 'Sorry, the page you are looking for could not be found.' : undefined),
+  statusCode: err.statusCode
 })
 </script>
