f71b0506f564f1132f054bc9042594b6639c5f38
François Risoud <francois.risoud@gmail.com>
2022-03-16 00:08:12 +0100

    feat(test): scope and reword brigde test

--- test/bridge.test.ts
+++ test/bridge.test.ts
@@ -8,8 +8,12 @@ describe('fixtures:bridge', async () => {
     server: true
   })
 
-  it('Render hello world', async () => {
-    expect(await $fetch('/')).to.contain('Hello Vue 2!')
+  describe('pages', () => {
+    it('render hello world', async () => {
+      expect(await $fetch('/')).to.contain('Hello Vue 2!')
+    })
+  })
+
   describe('navigate', () => {
     it('should redirect to index with navigateTo', async () => {
       const html = await $fetch('/navigate-to/')
