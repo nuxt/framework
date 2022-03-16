59440a2bbd57eef0030f216127b832bc12739e08
Pooya Parsa <pyapar@gmail.com>
2022-03-16 16:34:40 +0100

    fix(schema): pick json serializable VueRouter options

--- packages/schema/src/config/router.ts
+++ packages/schema/src/config/router.ts
@@ -9,7 +9,7 @@ export default {
    * For more control, you can use `app/router.optionts.ts` file.
    *
    * @see [documentation](https://router.vuejs.org/api/#routeroptions)
-   * @type {import('vue-router').RouterOptions}
+   * @type {Pick<import('vue-router').RouterOptions, 'linkActiveClass' | 'linkExactActiveClass' | 'end' | 'sensitive' | 'strict'>}
    *
    * @version 3
    */
