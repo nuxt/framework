424142a14c023916c64d2e1b18b8bf6d397cf7f0
Daniel Roe <daniel@roe.dev>
2022-03-16 11:54:32 +0000

    feat: add `@error` event handling

--- packages/nuxt3/src/app/components/nuxt-error-boundary.ts
+++ packages/nuxt3/src/app/components/nuxt-error-boundary.ts
@@ -2,12 +2,13 @@ import { defineComponent, ref, onErrorCaptured, onBeforeUnmount } from 'vue'
 import { useNuxtApp, useRouter } from '#app'
 
 export default defineComponent({
-  setup (_props, { slots }) {
+  setup (_props, { slots, emit }) {
     const error = ref(null)
     const nuxtApp = useNuxtApp()
 
     onErrorCaptured((err) => {
       if (process.client && !nuxtApp.isHydrating) {
+        emit('error', err)
         error.value = err
         return false
       }
