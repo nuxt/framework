5f08f906375323b4b2d86627b08c155d111c78e1
Pooya Parsa <pyapar@gmail.com>
2022-03-16 19:55:38 +0100

    filter layers with exisitng config file only

--- packages/kit/src/loader/config.ts
+++ packages/kit/src/loader/config.ts
@@ -44,7 +44,7 @@ export async function loadNuxtConfig (opts: LoadNuxtConfigOptions): Promise<Nuxt
     layer.config.srcDir = resolve(layer.config.rootDir, layer.config.srcDir)
   }
 
-  nuxtConfig._layers = layers
+  nuxtConfig._layers = layers.filter(layer => layer.configFile && !layer.configFile.endsWith('.nuxtrc'))
 
   // Resolve and apply defaults
   return applyDefaults(NuxtConfigSchema, nuxtConfig) as NuxtOptions
