fca15ad371a711ff061cdaf9adeee2126a7930fd
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-18 03:18:37 +0800

    chore: update

--- test/bridge.test.ts
+++ test/bridge.test.ts
@@ -1,12 +1,12 @@
 import { fileURLToPath } from 'url'
-import { describe, expect, it, runOnce } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { setup, $fetch } from '@nuxt/test-utils'
 
 describe('fixtures:bridge', async () => {
-  await runOnce(() => setup({
+  await setup({
     rootDir: fileURLToPath(new URL('./fixtures/bridge', import.meta.url)),
     server: true
-  }))
+  })
 
   describe('pages', () => {
     it('render hello world', async () => {
