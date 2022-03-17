1a2d16afd29a2b873362081f361aedcf3efe70b5
pooya parsa <pyapar@gmail.com>
2022-03-17 18:52:46 +0100

    Update packages/schema/src/config/experimental.ts

--- packages/schema/src/config/experimental.ts
+++ packages/schema/src/config/experimental.ts
@@ -12,7 +12,8 @@ export default {
   viteNode: process.env.EXPERIMENTAL_VITE_NODE ? true : false,
 
   /**
-   * Enable Vue's reactivity transfrom: https://vuejs.org/guide/extras/reactivity-transform.html
+   * Enable Vue's reactivity transform
+   * @see https://vuejs.org/guide/extras/reactivity-transform.html
    * @version 3
    */
   reactivityTransform: false
