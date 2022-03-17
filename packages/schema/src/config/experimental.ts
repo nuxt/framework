e8029dbce64ff33b96037d950d76a54a5210f58e
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-17 21:17:19 +0800

    feat: support reactivity transform

--- packages/schema/src/config/experimental.ts
+++ packages/schema/src/config/experimental.ts
@@ -9,5 +9,11 @@ export default {
    * Use vite-node for on-demand server chunk loading
    * @version 3
    */
-  viteNode: process.env.EXPERIMENTAL_VITE_NODE ? true : false
+  viteNode: process.env.EXPERIMENTAL_VITE_NODE ? true : false,
+
+  /**
+   * Enable Vue's reactivity transfrom: https://vuejs.org/guide/extras/reactivity-transform.html
+   * @version 3
+   */
+  reactivityTransform: false
 }
