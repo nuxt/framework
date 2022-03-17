52caf4527334ffe8a7198dde13567b2d40f68c71
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-17 08:29:51 +0800

    chore: await for dev server ready

--- packages/test-utils/src/dev.ts
+++ packages/test-utils/src/dev.ts
@@ -1,3 +1,4 @@
+import { Writable } from 'stream'
 import { execa } from 'execa'
 import { getRandomPort, waitForPort } from 'get-port-please'
 import { useTestContext } from './context'
@@ -6,13 +7,34 @@ export async function startDevServer () {
   const ctx = useTestContext()
   const port = await getRandomPort()
   ctx.url = 'http://localhost:' + port
+
+  let serverReadyResolve = () => {}
+  const serverReadyPromise = new Promise<void>((resolve) => {
+    serverReadyResolve = resolve
+  })
+
+  const stdout = new Writable({
+    write (data, encoding, callback) {
+      console.log({ out: data.toString() })
+      if (data.toString().includes('Nitro built')) {
+        serverReadyResolve()
+      }
+      callback()
+    }
+  })
+
   ctx.serverProcess = execa('npx', ['nuxi', 'dev'], {
     cwd: ctx.options.rootDir,
     env: {
       PORT: String(port),
       NODE_ENV: 'test'
-    }
+    },
+    stdio: 'pipe'
   })
 
+  ctx.serverProcess.stdout.pipe(stdout)
+
   await waitForPort(port, { retries: 8 })
+
+  await serverReadyPromise
 }
