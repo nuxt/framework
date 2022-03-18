e2e021cae68b1e62f0d7d8ecfb9ca6b4889ff9d5
Matteo Rigoni <rigonimat@gmail.com>
2022-03-18 12:44:29 +0100

    feat: add typings to kit server middleware

--- packages/kit/src/server.ts
+++ packages/kit/src/server.ts
@@ -1,6 +1,12 @@
 import { useNuxt } from './context'
+import { IncomingMessage, ServerResponse } from 'http'
+
+export interface ServerMiddleware {
+  path: string,
+  handler: (req: IncomingMessage, res: ServerResponse) => void
+}
 
 /** Adds a new server middleware to the end of the server middleware array. */
-export function addServerMiddleware (middleware) {
+export function addServerMiddleware (middleware: ServerMiddleware) {
   useNuxt().options.serverMiddleware.push(middleware)
 }
