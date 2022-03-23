46f53cdbd140d347cb48109816b9bd5a9d2e434c
Daniel Roe <daniel@roe.dev>
2022-03-23 09:06:36 +0000

    fix: mutate preset array rather than replacing it

--- packages/bridge/src/auto-imports.ts
+++ packages/bridge/src/auto-imports.ts
@@ -1,7 +1,6 @@
 import { installModule, useNuxt } from '@nuxt/kit'
 import * as CompositionApi from '@vue/composition-api'
 import autoImports from '../../nuxt3/src/auto-imports/module'
-import { vuePreset, commonPresets, appPreset } from '../../nuxt3/src/auto-imports/presets'
 
 const UnsupportedImports = new Set(['useAsyncData', 'useFetch', 'useError', 'throwError', 'clearError'])
 const CapiHelpers = new Set(Object.keys(CompositionApi))
@@ -9,21 +8,17 @@ const CapiHelpers = new Set(Object.keys(CompositionApi))
 export function setupAutoImports () {
   const nuxt = useNuxt()
   nuxt.hook('autoImports:sources', (presets) => {
-    presets.length = 0
-    presets.push(
-      ...commonPresets,
-      {
-        from: '#app',
-        imports: [
-          ...appPreset.imports.filter(i => !UnsupportedImports.has(i as string)),
-          'useNuxt2Meta'
-        ]
-      },
-      {
-        from: '@vue/composition-api',
-        imports: vuePreset.imports.filter(i => CapiHelpers.has(i as string))
+    for (const preset of presets) {
+      if (preset.from === '#app') {
+        preset.imports = preset.imports.filter(i => !UnsupportedImports.has(i as string))
+        if (!preset.imports.includes('useNuxt2Meta')) {
+          preset.imports.push('useNuxt2Meta')
+        }
+      } else if (preset.from === 'vue') {
+        preset.from = '@vue/composition-api'
+        preset.imports = preset.imports.filter(i => CapiHelpers.has(i as string))
       }
-    )
+    }
   })
 
   nuxt.hook('modules:done', () => installModule(autoImports))
