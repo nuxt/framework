4980358c8a818fd43041f7812fe7faba33f60c22
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-24 22:26:50 +0800

    feat: support async middleware

--- packages/nuxt3/src/core/plugins/unctx.ts
+++ packages/nuxt3/src/core/plugins/unctx.ts
@@ -1,22 +1,23 @@
-import { Nuxt, NuxtApp } from '@nuxt/schema'
+import { Nuxt, NuxtApp, NuxtMiddleware } from '@nuxt/schema'
 import { createTransformer } from 'unctx/transform'
 import { createUnplugin } from 'unplugin'
 
 export const UnctxTransformPlugin = createUnplugin((nuxt: Nuxt) => {
   const transformer = createTransformer({
-    asyncFunctions: ['defineNuxtPlugin', 'withAsyncContext', 'callAsync']
+    asyncFunctions: ['defineNuxtPlugin', 'defineNuxtRouteMiddleware', 'withAsyncContext', 'callAsync']
   })
 
   let app: NuxtApp | undefined
+  let middlewares: NuxtMiddleware[] = []
   nuxt.hook('app:resolve', (_app) => { app = _app })
-
+  nuxt.hook('pages:middleware:extend', (_middlewares) => { middlewares = _middlewares })
   return {
     name: 'unctx:transfrom',
     enforce: 'post',
     transformInclude (id) {
-      return !!app?.plugins.find(i => i.src === id)
+      return !!(app?.plugins.find(i => i.src === id) || middlewares.find(m => m.path === id))
     },
-    transform (code) {
+    transform (code, id) {
       const result = transformer.transform(code)
       if (result) {
         return {
