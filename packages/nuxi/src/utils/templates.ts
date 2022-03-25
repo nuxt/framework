00e37cc1d8b40df047fcb2a0c45b0d961d80814e
Pooya Parsa <pyapar@gmail.com>
2022-03-25 14:20:44 +0100

    refactor: update templates

--- packages/nuxi/src/utils/templates.ts
+++ packages/nuxi/src/utils/templates.ts
@@ -1,55 +1,92 @@
 import { upperFirst } from 'scule'
 
-export const templates = {
-  api: (name: string) => ({
-    path: `/server/api/${name}.ts`,
-    template: `export default (req, res) => 'Hello ${name}'`
-  }),
-  plugin: (name: string) => ({
-    path: `/plugins/${name}.ts`,
-    template: 'export default defineNuxtPlugin(nuxtApp => {})'
-  }),
-  component: (name: string) => ({
-    path: `/components/${name}.vue`,
-    template: `<script lang="ts" setup></script>
+interface Template {
+  (options: { name: string }): { path: string, template: string }
+}
+
+const api: Template = ({ name }) => ({
+  path: `/server/api/${name}.ts`,
+  template: `
+import { defineHandle } from 'h3'
+export default defineHandle((req, res) => {
+  return 'Hello ${name}'
+})
+`
+})
+
+const plugin: Template = ({ name }) => ({
+  path: `/plugins/${name}.ts`,
+  template: `
+export default defineNuxtPlugin(nuxtApp => {})
+  `
+})
+
+const component: Template = ({ name }) => ({
+  path: `/components/${name}.vue`,
+  template: `
+<script lang="ts" setup></script>
 
 <template>
-  <div>${name}</div>
+<div>${name}</div>
 </template>
 
-<style scoped></style>`
-  }),
-  composable: (name: string) => ({
-    path: `/composables/${!name.includes('use') ? `use${upperFirst(name)}` : name}.ts`,
-    template: `export const ${name} = () => {
+<style scoped></style>
+`
+})
+
+const composable: Template = ({ name }) => ({
+  path: `/composables/${!name.includes('use') ? `use${upperFirst(name)}` : name}.ts`,
+  template: `
+export const ${name} = () => {
 return useState(${name}, () => 'bar')
-}`
-  }),
-  middleware: (name: string) => ({
-    path: `/middleware/${name}.ts`,
-    template: 'export default defineNuxtRouteMiddleware((to, from) => {})'
-  }),
-  layout: (name: string) => ({
-    path: `/layouts/${name}.vue`,
-    template: `<script lang="ts" setup></script>
+}
+`
+})
+
+const middleware: Template = ({ name }) => ({
+  path: `/middleware/${name}.ts`,
+  template: `
+export default defineNuxtRouteMiddleware((to, from) => {})
+`
+})
+
+const layout: Template = ({ name }) => ({
+  path: `/layouts/${name}.vue`,
+  template: `
+<script lang="ts" setup></script>
 
 <template>
-  <div>
-    ${name}
-    <slot />
-  </div>
+<div>
+  Layout: ${name}
+  <slot />
+</div>
 </template>
 
-<style scoped></style>`
-  }),
-  page: (name: string) => ({
-    path: `/pages/${name}.vue`,
-    template: `<script lang="ts" setup></script>
+<style scoped></style>
+`
+})
+
+const page: Template = ({ name }) => ({
+  path: `/pages/${name}.vue`,
+  template: `
+<script lang="ts" setup></script>
 
 <template>
-  <div>${name}</div>
+<div>
+  Page: ${name}
+</div>
 </template>
 
-<style scoped></style>`
-  })
+<style scoped></style>
+`
+})
+
+export const templates = {
+  api,
+  plugin,
+  component,
+  composable,
+  middleware,
+  layout,
+  page
 }
