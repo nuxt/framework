5b6e8e7974b624222842964c74c44f89745a5cc4
Daniel Roe <daniel@roe.dev>
2022-03-28 11:01:08 +0100

    feat: add `<NuxtTeleport>` wrapper

---
+++ packages/nuxt3/src/app/components/nuxt-teleport.vue
@@ -0,0 +1,20 @@
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'NuxtTeleport',
+  props: {
+    //  string | RendererElement | null | undefined
+    to: [String, Object],
+    disabled: Boolean
+  }
+})
+</script>
+
+<template>
+  <ClientOnly>
+    <Teleport :to="to" :disabled="disabled">
+      <slot />
+    </Teleport>
+  </ClientOnly>
+</template>
