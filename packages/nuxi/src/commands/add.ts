7e668c92fe70d889f511dafad5a7b777c477800c
lazaroofarrill <lazaroofarrill@gmail.com>
2022-03-29 16:22:27 -0400

    fix: don't break on templates folder not found

--- packages/nuxi/src/commands/add.ts
+++ packages/nuxi/src/commands/add.ts
@@ -1,9 +1,9 @@
-import { existsSync, promises as fsp } from 'fs'
-import { dirname, resolve } from 'pathe'
+import {existsSync, promises as fsp} from 'fs'
+import {dirname, resolve} from 'pathe'
 import consola from 'consola'
-import { loadKit } from '../utils/kit'
-import { templates } from '../utils/templates'
-import { defineNuxtCommand } from './index'
+import {loadKit} from '../utils/kit'
+import {templates} from '../utils/templates'
+import {defineNuxtCommand} from './index'
 
 export default defineNuxtCommand({
   meta: {
@@ -11,18 +11,16 @@ export default defineNuxtCommand({
     usage: `npx nuxi add [--cwd] [--force] ${Object.keys(templates).join('|')} <name>`,
     description: 'Create a new template file.'
   },
-  async invoke (args) {
+  async invoke(args) {
     const cwd = resolve(args.cwd || '.')
 
     const template = args._[0]
     const name = args._[1]
 
-    consola.info('custom templates')
-    const files = await fsp.readdir(resolve('templates'))
+    const files = await fsp.readdir(resolve('templates')).catch(() => [])
 
     const customTemplateArray = (await Promise.all(files.map(async file => await (import(resolve('templates', file))))))
     const customTemplates = customTemplateArray.reduce((r, c) => Object.assign(r, c), {})
-    consola.info(customTemplates)
 
     const allTemplates = {
       ...templates,
@@ -43,10 +41,10 @@ export default defineNuxtCommand({
 
     // Load config in order to respect srcDir
     const kit = await loadKit(cwd)
-    const config = await kit.loadNuxtConfig({ cwd })
+    const config = await kit.loadNuxtConfig({cwd})
 
     // Resolve template
-    const res = allTemplates[template]({ name })
+    const res = allTemplates[template]({name})
 
     // Resolve full path to generated file
     const path = resolve(config.srcDir, res.path)
@@ -68,7 +66,7 @@ export default defineNuxtCommand({
         consola.error('This template does not support nested directories')
         process.exit(1)
       }
-      await fsp.mkdir(parentDir, { recursive: true })
+      await fsp.mkdir(parentDir, {recursive: true})
     }
 
     // Write file
