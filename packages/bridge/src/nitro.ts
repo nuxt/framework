1966b493dc52f40cccb7472abb52d12abb9e2627
Pooya Parsa <pyapar@gmail.com>
2022-04-08 01:33:39 +0200

    add fallback for appTemplatePath

--- packages/bridge/src/nitro.ts
+++ packages/bridge/src/nitro.ts
@@ -374,7 +374,7 @@ async function resolveHandlers (nuxt: Nuxt) {
 
 async function writeDocumentTemplate (nuxt: Nuxt) {
   // Compile html template
-  const src = nuxt.options.appTemplatePath
+  const src = nuxt.options.appTemplatePath || resolve(nuxt.options.buildDir, 'views/app.template.html')
   const dst = src.replace(/.html$/, '.mjs').replace('app.template.mjs', 'document.template.mjs')
   const contents = nuxt.vfs[src] || await fsp.readFile(src, 'utf-8').catch(() => '')
   if (contents) {
