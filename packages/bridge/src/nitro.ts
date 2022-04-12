6b4e50564d139d3cb5f039d6b4baf22de5ac53b5
Daniel Roe <daniel@roe.dev>
2022-04-12 13:51:35 +0100

    fix: remove nuxt3 line

--- packages/bridge/src/nitro.ts
+++ packages/bridge/src/nitro.ts
@@ -69,7 +69,7 @@ export async function setupNitroBridge () {
     buildDir: resolve(nuxt.options.buildDir),
     scanDirs: nuxt.options._layers.map(layer => join(layer.config.srcDir, 'server')),
     renderer: resolve(distDir, 'runtime/nitro/renderer'),
-    errorHandler: resolve(distDir, 'core/runtime/nitro/error'),
+    errorHandler: resolve(distDir, 'runtime/nitro/error'),
     nodeModulesDirs: nuxt.options.modulesDir,
     handlers,
     devHandlers: [],
