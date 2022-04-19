296a11d7df01050d6fda8bbc764a5b28de342be7
Daniel Roe <daniel@roe.dev>
2022-04-19 13:56:08 +0100

    fix(nuxt3): improve redirect lifecycle

--- packages/nuxt3/src/app/composables/cookie.ts
+++ packages/nuxt3/src/app/composables/cookie.ts
@@ -32,13 +32,10 @@ export function useCookie <T=string> (name: string, _opts?: CookieOptions<T>): C
   if (process.client) {
     watch(cookie, () => { writeClientCookie(name, cookie.value, opts as CookieSerializeOptions) })
   } else if (process.server) {
-    const initialValue = cookie.value
     const nuxtApp = useNuxtApp()
-    nuxtApp.hooks.hookOnce('app:rendered', () => {
-      if (cookie.value !== initialValue) {
-        writeServerCookie(useRequestEvent(nuxtApp), name, cookie.value, opts)
-      }
-    })
+    const writeFinalCookieValue = () => writeServerCookie(useRequestEvent(nuxtApp), name, cookie.value, opts)
+    nuxtApp.hooks.hookOnce('app:rendered', writeFinalCookieValue)
+    nuxtApp.hooks.hookOnce('app:redirected', writeFinalCookieValue)
   }
 
   return cookie as CookieRef<T>
