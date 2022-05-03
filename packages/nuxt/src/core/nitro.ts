5d92902b564949aad4c588c29aa978de1136a6ed
jhuang@hsk-partners.com <jhuang@hsk-partners.com>
2022-05-03 12:06:52 +0200

    fix(nuxt): set esmExternals and requireReturnsDefault on commonjs()
    - avoid possible side-effect due to nitro rollup commonjs plugin being overriden by setting back the default nitro data

--- packages/nuxt/src/core/nitro.ts
+++ packages/nuxt/src/core/nitro.ts
@@ -219,7 +219,9 @@ export async function initNitro (nuxt: Nuxt & { _nitro?: Nitro }) {
             './node_modules/@vue/compiler-ssr',
             './node_modules/@vue/devtools-api',
             './node_modules/vue/server-renderer'
-          ]
+          ],
+          esmExternals: id => !id.startsWith('unenv/'),
+          requireReturnsDefault: 'auto'
         }))
       }
     })
