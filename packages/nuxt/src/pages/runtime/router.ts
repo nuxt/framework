58a94b112057aacd0721b483add4697bc54051a3
Daniel Roe <daniel@roe.dev>
2022-05-06 11:18:07 +0100

    perf: check if error is set before clearing

--- packages/nuxt/src/pages/runtime/router.ts
+++ packages/nuxt/src/pages/runtime/router.ts
@@ -9,7 +9,7 @@ import {
 import { createError } from 'h3'
 import { withoutBase } from 'ufo'
 import NuxtPage from './page'
-import { callWithNuxt, defineNuxtPlugin, useRuntimeConfig, throwError, clearError, navigateTo } from '#app'
+import { callWithNuxt, defineNuxtPlugin, useRuntimeConfig, throwError, clearError, navigateTo, useError } from '#app'
 // @ts-ignore
 import routes from '#build/routes'
 // @ts-ignore
@@ -107,8 +107,9 @@ export default defineNuxtPlugin(async (nuxtApp) => {
     named: {}
   }
 
+  const error = useError()
   router.afterEach(async (to) => {
-    if (process.client && !nuxtApp.isHydrating) {
+    if (process.client && !nuxtApp.isHydrating && error.value) {
       // Clear any existing errors
       await callWithNuxt(nuxtApp, clearError)
     }
