4b20d10612b54397f83483054f5b8d9511d21247
Daniel Roe <daniel@roe.dev>
2022-05-13 14:03:01 +0100

    fix: remove changed files

--- playground/app.tsx
+++
@@ -1,8 +0,0 @@
-type A = string
-export default defineComponent({
-async setup() {
-  const { data } = await useAsyncData<A>(() => Promise.resolve({ foo: 'bar' }))
-
-return () => <div>{'value:' + JSON.stringify(data.value)}</div>
-}
-})
