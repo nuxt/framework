7c21ae7e89de1cba96f8a11226e0d56f4c027fb8
pooya parsa <pyapar@gmail.com>
2022-06-13 00:02:30 +0200

    Update packages/nuxi/src/utils/env.ts

--- packages/nuxi/src/utils/env.ts
+++ packages/nuxi/src/utils/env.ts
@@ -1,7 +1,7 @@
 export const overrideEnv = (targetEnv: string) => {
   const currentEnv = process.env.NODE_ENV
   if (currentEnv && currentEnv !== targetEnv) {
-    console.warn(`Defined \`NODE_ENV\` will be replaced from \`${currentEnv}\` to \`${targetEnv}\`, to avoid unintended behaviours from libaries in development mode.`)
+    console.warn(`Defined \`NODE_ENV\` will be replaced from \`${currentEnv}\` to \`${targetEnv}\`, to avoid unintended behaviors from libraries.`)
   }
 
   process.env.NODE_ENV = targetEnv
