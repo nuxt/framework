b409c1cdba33c8326084efce57e257d7b541a697
Daniel Roe <daniel@roe.dev>
2022-06-12 22:20:15 +0100

    fix: use warn

--- packages/nuxi/src/utils/env.ts
+++ packages/nuxi/src/utils/env.ts
@@ -1,7 +1,7 @@
 export const overrideEnv = (targetEnv: string) => {
   const currentEnv = process.env.NODE_ENV
   if (currentEnv && currentEnv !== targetEnv) {
-    console.info(`Changing NODE_ENV from \`${currentEnv}\` to \`${targetEnv}\`.`)
+    console.warn(`Changing NODE_ENV from \`${currentEnv}\` to \`${targetEnv}\`.`)
   }
 
   process.env.NODE_ENV = targetEnv
