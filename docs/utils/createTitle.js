89ebe83c6945b4af79785d9875c2adde3a7b61ae
Yaël Guilloux <yael.guilloux@gmail.com>
2022-06-16 01:30:57 +0200

    feat(theme): use @docus/nuxt3-docs theme

--- docs/utils/createTitle.js
+++
@@ -1,3 +0,0 @@
-import { splitByCase, upperFirst } from 'scule'
-
-export default (title, link) => (title || link.split('/').filter(Boolean).map(part => splitByCase(part).map(p => upperFirst(p)).join(' ')).join(' > ').replace('Api', 'API'))
