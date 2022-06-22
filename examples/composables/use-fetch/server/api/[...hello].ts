7dfab59e312e33bea1018dae1c551c458a28bcd4
Qin Guan <qinguan20040914@gmail.com>
2022-06-22 22:25:18 +0800

    docs(examples): lint

--- examples/composables/use-fetch/server/api/[...hello].ts
+++ examples/composables/use-fetch/server/api/[...hello].ts
@@ -1,6 +1,6 @@
-import { useQuery } from "h3";
+import { useQuery } from 'h3'
 
 export default event => ({
-  path: "/api/" + event.context.params.hello,
+  path: '/api/' + event.context.params.hello,
   query: useQuery(event)
-});
+})
