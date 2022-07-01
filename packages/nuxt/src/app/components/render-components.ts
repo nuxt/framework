109d48a6da09e3d34e7db0ca01abf3d44e72aea8
Daniel Roe <daniel@roe.dev>
2022-07-01 11:41:36 +0100

    fix: workaround upstream vue issue

--- packages/nuxt/src/app/components/render-components.ts
+++ packages/nuxt/src/app/components/render-components.ts
@@ -4,7 +4,12 @@ export default defineComponent({
   props: {
     components: Array as () => Array<{ name: string, props?: Record<string, any> }>
   },
-  setup (props) {
+  async setup (props) {
+    // TODO: https://github.com/vuejs/core/issues/6207
+    await Promise.all(props.components.map((c) => {
+      const component = resolveComponent(c.name)
+      return component && typeof component === 'object' && component.__asyncLoader?.()
+    }))
     return () => props.components.map(
       (c, index) => createBlock(Teleport as any, { to: `render-target-${index}` }, [
         h(resolveComponent(c.name), c.props)
