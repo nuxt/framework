811954acd71188eb3afbb4dfb4cba4c57366be4a
Cupid Valentine <53572196+valcosmos@users.noreply.github.com>
2022-07-03 07:18:19 +0800

    Update nuxt.config.ts

--- examples/other/pinia/nuxt.config.ts
+++ examples/other/pinia/nuxt.config.ts
@@ -2,7 +2,5 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  buildModules: [
-    '@pinia/nuxt'
-  ]
+  modules: ['@pinia/nuxt']
 })
