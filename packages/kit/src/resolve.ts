05444550a8f440436dbf4e5a47d0008b7204db97
Conrawl Rogers <me@diizzayy.com>
2022-07-29 15:14:34 +0000

    allowing overriding `followSymbolicLinks`

--- packages/kit/src/resolve.ts
+++ packages/kit/src/resolve.ts
@@ -151,7 +151,7 @@ async function isDirectory (path: string) {
   return (await fsp.lstat(path)).isDirectory()
 }
 
-export async function resolveFiles (path: string, pattern: string | string[]) {
-  const files = await globby(pattern, { cwd: path, followSymbolicLinks: true })
+export async function resolveFiles (path: string, pattern: string | string[], followSymbolicLinks: boolean = true) {
+  const files = await globby(pattern, { cwd: path, followSymbolicLinks })
   return files.map(p => resolve(path, p)).filter(p => !isIgnored(p)).sort()
 }
