294c07734d182c59ba3c82a538840bed7b577089
jhuang@hsk-partners.com <jhuang@hsk-partners.com>
2022-07-29 14:40:34 +0200

    fix(nuxt): fix inheritAttrs and handle template in createClientOnly

--- packages/nuxt/src/app/components/client-only.mjs
+++ packages/nuxt/src/app/components/client-only.mjs
@@ -19,17 +19,24 @@ export default defineComponent({
 })
 
 export function createClientOnly (component) {
-  const { setup, render: _render } = component
-  if (component.render) {
+  const { setup, render: _render, template: _template } = component
+  if (_render) {
     // override the component render (non <script setup> component)
     component.render = (ctx, cache, props, state, data, options) => {
       return state.mounted$
         ? h(Fragment, [_render(ctx, cache, props, state, data, options)])
         : h('div', ctx.$attrs ?? ctx._.attrs)
     }
+  } else if (_template) {
+    // handle runtime-compiler template
+    component.template = `
+      <template v-if="mounted$">${_template}</template>
+      <template v-else><div v-bind="$attrs"></div></template>
+    `
   }
   return defineComponent({
     ...component,
+    inheritAttrs: false,
     setup (props, ctx) {
       const mounted$ = ref(false)
       onMounted(() => { mounted$.value = true })
