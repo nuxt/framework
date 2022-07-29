a3b3e35ffd750dda95322e8a0f0bb29c9a500215
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2022-07-29 10:42:47 -0300

    feat(test-utils): add mocks

--- packages/test-utils/src/index.ts
+++ packages/test-utils/src/index.ts
@@ -1,5 +1,6 @@
 export * from './browser'
 export * from './context'
+export * from './mocks'
 export * from './nuxt'
 export * from './server'
 export * from './setup'
---
+++ packages/test-utils/src/mocks.ts
@@ -0,0 +1,30 @@
+import consola from 'consola'
+import { useTestContext } from './context'
+
+const mockFnVitest = async () => {
+  const vitest = await import('vitest')
+  return vitest.vi.fn()
+}
+
+const mockFnJest = async () =>{
+  const jest = await import('jest')
+  return jest.fn()
+}
+
+export const mockFnMaps = {
+  jest: mockFnJest,
+  vitest: mockFnVitest
+}
+
+export function mockLogger (): typeof consola {
+  const mock = {}
+  const mockFn = mockFnMaps[useTestContext().options.runner]
+
+  consola.mockTypes((type) => {
+    mock[type] = mock[type] || mockFn()
+    return mock[type]
+  })
+
+  // @ts-ignore
+  return mock
+}
