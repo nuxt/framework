cb867f0c23d1f638f9600bf27176a8df0d8ae66a
Sanjaiyan Parthipan <parthipankalayini@gmail.com>
2022-08-01 10:07:53 +0530

    Formatting and adding types for link tag.

--- packages/nuxt/src/head/runtime/components.ts
+++ packages/nuxt/src/head/runtime/components.ts
@@ -1,45 +1,55 @@
-import { defineComponent, PropType } from 'vue'
-import type { SetupContext } from 'vue'
-import { useHead } from './composables'
+import { defineComponent, PropType } from "vue";
+import type { SetupContext } from "vue";
+import { useHead } from "./composables";
 import type {
   Props,
   FetchPriority,
   CrossOrigin,
   HTTPEquiv,
-  ReferrerPolicy
-} from "./types.ts"
+  ReferrerPolicy,
+  LinkRelationship,
+} from "./types.ts";
 
 const removeUndefinedProps = (props: Props) =>
-  Object.fromEntries(Object.entries(props).filter(([, value]) => value !== undefined))
+  Object.fromEntries(
+    Object.entries(props).filter(([, value]) => value !== undefined)
+  );
 
-const setupForUseMeta = (metaFactory: (props: Props, ctx: SetupContext) => Record<string, any>, renderChild?: boolean) => (props: Props, ctx: SetupContext) => {
-  useHead(() => metaFactory({ ...removeUndefinedProps(props), ...ctx.attrs }, ctx))
-  return () => renderChild ? ctx.slots.default?.() : null
-}
+const setupForUseMeta =
+  (
+    metaFactory: (props: Props, ctx: SetupContext) => Record<string, any>,
+    renderChild?: boolean
+  ) =>
+  (props: Props, ctx: SetupContext) => {
+    useHead(() =>
+      metaFactory({ ...removeUndefinedProps(props), ...ctx.attrs }, ctx)
+    );
+    return () => (renderChild ? ctx.slots.default?.() : null);
+  };
 
 const globalProps = {
   accesskey: String,
   autocapitalize: String,
   autofocus: {
     type: Boolean,
-    default: undefined
+    default: undefined,
   },
   class: String,
   contenteditable: {
     type: Boolean,
-    default: undefined
+    default: undefined,
   },
   contextmenu: String,
   dir: String,
   draggable: {
     type: Boolean,
-    default: undefined
+    default: undefined,
   },
   enterkeyhint: String,
   exportparts: String,
   hidden: {
     type: Boolean,
-    default: undefined
+    default: undefined,
   },
   id: String,
   inputmode: String,
@@ -55,24 +65,24 @@ const globalProps = {
   slot: String,
   spellcheck: {
     type: Boolean,
-    default: undefined
+    default: undefined,
   },
   style: String,
   tabindex: String,
   title: String,
-  translate: String
-}
+  translate: String,
+};
 
 // <script>
 export const Script = defineComponent({
-  name: 'Script',
+  name: "Script",
   inheritAttrs: false,
   props: {
     ...globalProps,
     async: Boolean,
     crossorigin: {
       type: [Boolean, String as PropType<CrossOrigin>],
-      default: undefined
+      default: undefined,
     },
     defer: Boolean,
     fetchpriority: String as PropType<FetchPriority>,
@@ -85,39 +95,39 @@ export const Script = defineComponent({
     /** @deprecated **/
     charset: String,
     /** @deprecated **/
-    language: String
+    language: String,
   },
-  setup: setupForUseMeta(script => ({
-    script: [script]
-  }))
-})
+  setup: setupForUseMeta((script) => ({
+    script: [script],
+  })),
+});
 
 // <noscript>
 export const NoScript = defineComponent({
-  name: 'NoScript',
+  name: "NoScript",
   inheritAttrs: false,
   props: {
     ...globalProps,
-    title: String
+    title: String,
   },
   setup: setupForUseMeta((props, { slots }) => {
-    const noscript = { ...props }
+    const noscript = { ...props };
     const textContent = (slots.default?.() || [])
       .filter(({ children }) => children)
       .map(({ children }) => children)
-      .join('')
+      .join("");
     if (textContent) {
-      noscript.children = textContent
+      noscript.children = textContent;
     }
     return {
-      noscript: [noscript]
-    }
-  })
-})
+      noscript: [noscript],
+    };
+  }),
+});
 
 // <link>
 export const Link = defineComponent({
-  name: 'Link',
+  name: "Link",
   inheritAttrs: false,
   props: {
     ...globalProps,
@@ -133,71 +143,71 @@ export const Link = defineComponent({
     media: String,
     prefetch: {
       type: Boolean,
-      default: undefined
+      default: undefined,
     },
     referrerpolicy: String as PropType<ReferrerPolicy>,
-    rel: String,
+    rel: String as PropType<LinkRelationship>,
     sizes: String,
     title: String,
     type: String,
     /** @deprecated **/
     methods: String,
     /** @deprecated **/
-    target: String
+    target: String,
   },
-  setup: setupForUseMeta(link => ({
-    link: [link]
-  }))
-})
+  setup: setupForUseMeta((link) => ({
+    link: [link],
+  })),
+});
 
 // <base>
 export const Base = defineComponent({
-  name: 'Base',
+  name: "Base",
   inheritAttrs: false,
   props: {
     ...globalProps,
     href: String,
-    target: String
+    target: String,
   },
-  setup: setupForUseMeta(base => ({
-    base
-  }))
-})
+  setup: setupForUseMeta((base) => ({
+    base,
+  })),
+});
 
 // <title>
 export const Title = defineComponent({
-  name: 'Title',
+  name: "Title",
   inheritAttrs: false,
   setup: setupForUseMeta((_, { slots }) => {
-    const title = slots.default?.()?.[0]?.children || null
-    if (process.dev && title && typeof title !== 'string') {
-      console.error('<Title> can only take a string in its default slot.')
+    const title = slots.default?.()?.[0]?.children || null;
+    if (process.dev && title && typeof title !== "string") {
+      console.error("<Title> can only take a string in its default slot.");
     }
     return {
-      title
-    }
-  })
-})
+      title,
+    };
+  }),
+});
 
 // <meta>
 export const Meta = defineComponent({
-  name: 'Meta',
+  name: "Meta",
   inheritAttrs: false,
   props: {
     ...globalProps,
     charset: String,
     content: String,
     httpEquiv: String as PropType<HTTPEquiv>,
-    name: String
+    name: String,
   },
-  setup: setupForUseMeta(meta => ({
-    meta: [meta]
-  }))
-})
+  setup: setupForUseMeta((meta) => ({
+    meta: [meta],
+  })),
+});
 
 // <style>
 export const Style = defineComponent({
-  name: 'Style',
+  name: "Style",
   inheritAttrs: false,
   props: {
     ...globalProps,
@@ -208,48 +218,48 @@ export const Style = defineComponent({
     /** @deprecated **/
     scoped: {
       type: Boolean,
-      default: undefined
-    }
+      default: undefined,
+    },
   },
   setup: setupForUseMeta((props, { slots }) => {
-    const style = { ...props }
-    const textContent = slots.default?.()?.[0]?.children
+    const style = { ...props };
+    const textContent = slots.default?.()?.[0]?.children;
     if (textContent) {
-      if (process.dev && typeof textContent !== 'string') {
-        console.error('<Style> can only take a string in its default slot.')
+      if (process.dev && typeof textContent !== "string") {
+        console.error("<Style> can only take a string in its default slot.");
       }
-      style.children = textContent
+      style.children = textContent;
     }
     return {
-      style: [style]
-    }
-  })
-})
+      style: [style],
+    };
+  }),
+});
 
 // <head>
 export const Head = defineComponent({
-  name: 'Head',
+  name: "Head",
   inheritAttrs: false,
-  setup: (_props, ctx) => () => ctx.slots.default?.()
-})
+  setup: (_props, ctx) => () => ctx.slots.default?.(),
+});
 
 // <html>
 export const Html = defineComponent({
-  name: 'Html',
+  name: "Html",
   inheritAttrs: false,
   props: {
     ...globalProps,
     manifest: String,
     version: String,
-    xmlns: String
+    xmlns: String,
   },
-  setup: setupForUseMeta(htmlAttrs => ({ htmlAttrs }), true)
-})
+  setup: setupForUseMeta((htmlAttrs) => ({ htmlAttrs }), true),
+});
 
 // <body>
 export const Body = defineComponent({
-  name: 'Body',
+  name: "Body",
   inheritAttrs: false,
   props: globalProps,
-  setup: setupForUseMeta(bodyAttrs => ({ bodyAttrs }), true)
-})
+  setup: setupForUseMeta((bodyAttrs) => ({ bodyAttrs }), true),
+});
--- packages/nuxt/src/head/runtime/types.ts
+++ packages/nuxt/src/head/runtime/types.ts
@@ -1,14 +1,50 @@
-type Props = Readonly<Record<string, any>>
+type Props = Readonly<Record<string, any>>;
 
-type FetchPriority = 'high' | 'low' | 'auto'
-type CrossOrigin = '' | 'anonymous' | 'use-credentials'
-type HTTPEquiv = 'content-security-policy' | 'content-type' | 'default-style' | 'refresh' | 'x-ua-compatible'
-type ReferrerPolicy = '' | 'no-referrer' | 'no-referrer-when-downgrade' | 'same-origin' | 'origin' | 'strict-origin' | 'origin-when-cross-origin' | 'strict-origin-when-cross-origin' | 'unsafe-url'
+type FetchPriority = "high" | "low" | "auto";
+type CrossOrigin = "" | "anonymous" | "use-credentials";
+type HTTPEquiv =
+  | "content-security-policy"
+  | "content-type"
+  | "default-style"
+  | "refresh"
+  | "x-ua-compatible";
+type ReferrerPolicy =
+  | ""
+  | "no-referrer"
+  | "no-referrer-when-downgrade"
+  | "same-origin"
+  | "origin"
+  | "strict-origin"
+  | "origin-when-cross-origin"
+  | "strict-origin-when-cross-origin"
+  | "unsafe-url";
+type LinkRelationship =
+  | "alternate"
+  | "author"
+  | "canonical"
+  | "dns-prefetch"
+  | "help"
+  | "icon"
+  | "license"
+  | "manifest"
+  | "me"
+  | "modulepreload"
+  | "next"
+  | "pingback"
+  | "preconnect"
+  | "prefetch"
+  | "preload"
+  | "prerender"
+  | "prev"
+  | "search"
+  | "stylesheet"
+  | String;
 
 export {
-    Props,
-    FetchPriority,
-    CrossOrigin,
-    HTTPEquiv,
-    ReferrerPolicy
-}
+  Props,
+  FetchPriority,
+  CrossOrigin,
+  HTTPEquiv,
+  ReferrerPolicy,
+  LinkRelationship,
+};
