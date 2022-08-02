986bc42e5e71a9d9eea75ee2a8d9e842a010b044
Pooya Parsa <pooya@pi0.io>
2022-08-02 16:06:35 +0200

    refactor: rename state to __mounted__

--- packages/nuxt/src/app/components/client-only.mjs
+++ packages/nuxt/src/app/components/client-only.mjs
@@ -23,29 +23,29 @@ export function createClientOnly (component) {
   if (_render) {
     // override the component render (non <script setup> component)
     component.render = (ctx, ...args) => {
-      return ctx.mounted$
+      return ctx.__mounted__
         ? h(Fragment, null, [h(_render(ctx, ...args), ctx.$attrs ?? ctx._.attrs)])
         : h('div', ctx.$attrs ?? ctx._.attrs)
     }
   } else if (_template) {
     // handle runtime-compiler template
     component.template = `
-      <template v-if="mounted$">${_template}</template>
+      <template v-if="__mounted__">${_template}</template>
       <template v-else><div></div></template>
     `
   }
   return defineComponent({
     ...component,
     setup (props, ctx) {
-      const mounted$ = ref(false)
-      onMounted(() => { mounted$.value = true })
+      const __mounted__ = ref(false)
+      onMounted(() => { __mounted__.value = true })
 
       return Promise.resolve(setup?.(props, ctx) || {})
         .then((setupState) => {
           return typeof setupState !== 'function'
-            ? { ...setupState, mounted$ }
+            ? { ...setupState, __mounted__ }
             : () => {
-                return mounted$.value
+                return __mounted__.value
                 // use Fragment to avoid oldChildren is null issue
                   ? h(Fragment, null, [h(setupState(props, ctx), ctx.attrs)])
                   : h('div', ctx.attrs)
