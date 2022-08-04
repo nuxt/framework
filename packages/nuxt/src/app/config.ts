79ff3b11ee8ca982d58757d2848987b60ca7c875
Pooya Parsa <pooya@pi0.io>
2022-08-04 13:56:04 +0200

    webpack hmr support

--- packages/nuxt/src/app/config.ts
+++ packages/nuxt/src/app/config.ts
@@ -4,6 +4,7 @@ import { useNuxtApp } from './nuxt'
 // @ts-ignore
 import __appConfig from '#build/app.config.mjs'
 
+// Workaround for vite HMR with virtual modules
 export const _appConfig = __appConfig as AppConfig
 
 export function useRuntimeConfig (): RuntimeConfig {
@@ -22,15 +23,30 @@ export function useAppConfig (): AppConfig {
   return nuxtApp._appConfig
 }
 
-if (import.meta.hot) {
-  import.meta.hot.accept((newModule) => {
+// HMR Support
+if (process.dev) {
+  function applyHMR (newConfig) {
     const appConfig = useAppConfig()
-    const newConfig = newModule?._appConfig
     if (newConfig && appConfig) {
-      // TODO: Deep assign
       for (const key in newConfig) {
         appConfig[key] = newConfig[key]
       }
     }
-  })
+  }
+
+  // Vite
+  if (import.meta.hot) {
+    import.meta.hot.accept((newModule) => {
+      const newConfig = newModule?._appConfig
+      applyHMR(newConfig)
+    })
+  }
+
+  // Webpack
+  if (import.meta.webpackHot) {
+    console.log('Register webpackHot')
+    import.meta.webpackHot.accept('#build/app.config.mjs', () => {
+      applyHMR(__appConfig)
+    })
+  }
 }
