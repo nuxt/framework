827e07acaca20f616c1338967e1c95e17404a89f
Pooya Parsa <pooya@pi0.io>
2022-08-08 15:25:31 +0200

    revert playground

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,8 +1,4 @@
 import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
-  builder: 'webpack',
-  experimental: {
-    // serverDynamicImports: true
-  }
 })
--- playground/pages/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    Hello
-  </div>
-</template>
