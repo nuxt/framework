bf16a1a20ade58624fbf3c5814109d8b81b6f7be
Pooya Parsa <pooya@pi0.io>
2022-08-09 18:27:10 +0200

    add random fixture

---
+++ test/fixtures/basic/pages/random/[id].vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <NuxtLink to="/random/a">
+      Random (A)
+    </NuxtLink>
+    <NuxtLink to="/random/b">
+      Random (B)
+    </NuxtLink>
+    <NuxtLink to="/random/c">
+      Random (C)
+    </NuxtLink>
+    <br>
+
+    Random page: <b>{{ route.params.id }}</b><br>
+
+    Here are some random numbers for you:
+
+    <ul>
+      <li v-for="n in randomNumbers" :key="n">
+        {{ n }}
+      </li>
+    </ul>
+    <button @click="refresh">
+      Give me another set
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+// TODO
+prefetchPayload(route.path)
+
+const { data: randomNumbers, refresh } = await useFetch('/api/random')
+</script>
