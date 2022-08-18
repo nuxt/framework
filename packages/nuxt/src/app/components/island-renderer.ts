6f8a2d9a48ec283b180467723c759cd577415cbb
Pooya Parsa <pooya@pi0.io>
2022-08-18 13:20:51 +0200

    handle 404 components

--- packages/nuxt/src/app/components/island-renderer.ts
+++ packages/nuxt/src/app/components/island-renderer.ts
@@ -2,6 +2,7 @@ import { createBlock, defineComponent, h, Teleport } from 'vue'
 
 // @ts-ignore
 import * as islandComponents from '#build/components-islands.mjs'
+import { createError } from '#app'
 
 export default defineComponent({
   props: {
@@ -13,9 +14,18 @@ export default defineComponent({
   async setup (props) {
     // TODO: https://github.com/vuejs/core/issues/6207
     const component = islandComponents[props.context.name]
+
+    if (!component) {
+      throw createError({
+        statusCode: 404,
+        statusMessage: `Island component not found: ${JSON.stringify(component)}`
+      })
+    }
+
     if (typeof component === 'object') {
       await component.__asyncLoader?.()
     }
+
     return () => [
       createBlock(Teleport as any, { to: 'nuxt-island' }, [h(component || 'span', props.context.props)])
     ]
