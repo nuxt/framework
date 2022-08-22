d25f471a2a103433cd6491cd84a4a94643b08768
Sanjaiyan Parthipan <parthipankalayini@gmail.com>
2022-08-22 17:33:51 +0530

    Adding route annoncer

---
+++ packages/nuxt/src/app/components/nuxt-route-announcer.vue
@@ -0,0 +1,47 @@
+<template>
+  <p
+    class="nuxtjs-route-announcer"
+    aria-live="assertive"
+    id="__next-route-announcer__"
+    role="alert"
+  >
+    {{ routeAnnouncement }}
+  </p>
+</template>
+
+<script setup>
+import { shallowRef, watchEffect } from "vue";
+import { useRoute } from "#app";
+
+const previouslyLoadedPath = shallowRef(useRoute().fullPath);
+const routeAnnouncement = shallowRef("");
+
+watchEffect(() => {
+  if (previouslyLoadedPath.value === useRoute().fullPath) return;
+  previouslyLoadedPath.value === useRoute().fullPath;
+  if (document?.title) {
+    routeAnnouncement.value = document?.title;
+  } else {
+    const nuxtPageHeader = document?.querySelector?.("h1");
+    const nuxtContent =
+      nuxtPageHeader?.innerText ?? nuxtPageHeader?.textContent;
+
+    routeAnnouncement.value = nuxtContent || useRoute()?.fullPath;
+  }
+});
+</script>
+
+<style scoped>
+.nuxtjs-route-announcer{
+  border: 0;
+  clip: rect(0 0 0 0);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  width: 1px;
+  white-space: nowrap;
+  word-wrap: normal;
+}
+</style>
