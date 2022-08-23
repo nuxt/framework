16dd3547c814eede0b3dbba893b3afc989f03abf
Daniel Roe <daniel@roe.dev>
2022-08-23 11:35:15 +0100

    wip

--- packages/nuxt/src/app/composables/prefetch.ts
+++ packages/nuxt/src/app/composables/prefetch.ts
@@ -1,4 +1,11 @@
+import type { Component } from 'vue'
 import { useNuxtApp } from '#app'
+
+function prefetchAsyncComponent (component: Component) {
+  if ((component as any)?.__asyncLoader && !(component as any).__asyncResolved) {
+    return (component as any).__asyncLoader()
+  }
+}
 
 /**
  * Prefetch a component or components that have been globally registered
@@ -7,8 +14,11 @@ import { useNuxtApp } from '#app'
  * @note This does not work for components you have globally registered yourself.
  * @param components Pascal-cased name or names of components to prefetch
  */
-export const prefetchComponents = (components: string | string[]) => {
+export const prefetchComponents = async (components: string | string[]) => {
   if (process.server) { return }
 
-  return useNuxtApp().callHook('components:prefetch', components)
+  components = Array.isArray(components) ? components : [components]
+  await Promise.all(components.map((name) => {
+    return name in components && prefetchAsyncComponent(components[name])
+  }))
 }
