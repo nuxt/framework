0160e7fdc0c5838adf4aa1ed565325d1389973f0
Pooya Parsa <pooya@pi0.io>
2022-08-23 16:10:18 +0200

    refactor: preserve arg names

--- packages/kit/src/auto-import.ts
+++ packages/kit/src/auto-import.ts
@@ -5,17 +5,17 @@ import { assertNuxtCompatibility } from './compatibility'
 export function addAutoImport (imports: Import | Import[]) {
   assertNuxtCompatibility({ bridge: true })
 
-  useNuxt().hook('imports:extend', (extend) => {
-    extend.push(...(Array.isArray(imports) ? imports : [imports]))
+  useNuxt().hook('imports:extend', (_imports) => {
+    _imports.push(...(Array.isArray(imports) ? imports : [imports]))
   })
 }
 
 export function addAutoImportDir (dirs: string | string[]) {
   assertNuxtCompatibility({ bridge: true })
 
-  useNuxt().hook('imports:dirs', (extend: string[]) => {
+  useNuxt().hook('imports:dirs', (_dirs: string[]) => {
     for (const dir of (Array.isArray(dirs) ? dirs : [dirs])) {
-      extend.push(dir)
+      _dirs.push(dir)
     }
   })
 }
