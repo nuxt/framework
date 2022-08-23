1ef0be1539c29250b98db2c8604b7dee731ad358
Daniel Roe <daniel@roe.dev>
2022-08-23 11:55:07 +0100

    docs: update comment

--- packages/nuxt/src/app/composables/prefetch.ts
+++ packages/nuxt/src/app/composables/prefetch.ts
@@ -8,10 +8,8 @@ function prefetchAsyncComponent (component: Component) {
 }
 
 /**
- * Prefetch a component or components that have been globally registered
- * with the Nuxt component integration.
+ * Prefetch a component or components that have been globally registered.
  *
- * @note This does not work for components you have globally registered yourself.
  * @param components Pascal-cased name or names of components to prefetch
  */
 export const prefetchComponents = async (components: string | string[]) => {
