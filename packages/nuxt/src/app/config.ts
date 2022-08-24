bdd222a85341de5cccb18cd29002efe30629e33c
Yaël Guilloux <yael.guilloux@gmail.com>
2022-08-24 16:47:26 +0200

    feat(app.config): support setAppConfig/updateAppConfig functions

--- packages/nuxt/src/app/config.ts
+++ packages/nuxt/src/app/config.ts
@@ -7,12 +7,55 @@ import __appConfig from '#build/app.config.mjs'
 // Workaround for vite HMR with virtual modules
 export const _getAppConfig = () => __appConfig as AppConfig
 
+function deepDelete (obj: any, newObj: any) {
+  for (const key in obj) {
+    if (!(key in newObj)) {
+      delete (obj as any)[key]
+    }
+
+    if (typeof obj[key] === 'object' && newObj[key]) {
+      deepDelete(obj[key], newObj[key])
+    }
+  }
+}
+function deepAssign (obj: any, newObj: any) {
+  for (const key in newObj) {
+    const val = newObj[key]
+    if (val !== null && typeof val === 'object') {
+      deepAssign(obj[key], val)
+    } else {
+      obj[key] = val
+    }
+  }
+}
+
 export function useAppConfig (): AppConfig {
   const nuxtApp = useNuxtApp()
   if (!nuxtApp._appConfig) {
     nuxtApp._appConfig = reactive(__appConfig) as AppConfig
   }
   return nuxtApp._appConfig
+}
+
+/**
+ * Deep assign the current appConfig with the new one.
+ *
+ * Will preserve existing properties.
+ */
+export function updateAppConfig (appConfig: Partial<AppConfig>) {
+  const nuxtApp = useNuxtApp()
+  if (!nuxtApp._appConfig) {
+    nuxtApp._appConfig = reactive(__appConfig) as AppConfig
+  }
+  deepAssign(nuxtApp._appConfig, appConfig)
+}
+
+/**
+ * Overwrites the current app config with the new one.
+ */
+export function setAppConfig (appConfig: AppConfig) {
+  const nuxtApp = useNuxtApp()
+  nuxtApp._appConfig = appConfig
 }
 
 // HMR Support
@@ -22,29 +65,6 @@ if (process.dev) {
     if (newConfig && appConfig) {
       deepAssign(appConfig, newConfig)
       deepDelete(appConfig, newConfig)
-    }
-  }
-
-  function deepDelete (obj: any, newObj: any) {
-    for (const key in obj) {
-      if (!(key in newObj)) {
-        delete (obj as any)[key]
-      }
-
-      if (typeof obj[key] === 'object' && newObj[key]) {
-        deepDelete(obj[key], newObj[key])
-      }
-    }
-  }
-
-  function deepAssign (obj: any, newObj: any) {
-    for (const key in newObj) {
-      const val = newObj[key]
-      if (val !== null && typeof val === 'object') {
-        deepAssign(obj[key], val)
-      } else {
-        obj[key] = val
-      }
     }
   }
 
