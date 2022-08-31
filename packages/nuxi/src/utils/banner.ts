26be10f70b8734be18bd57ee47e70395bdcf5968
Pooya Parsa <pooya@pi0.io>
2022-08-31 21:58:50 +0200

    feat(nuxi): show versions in dev and build commands

--- packages/nuxi/src/utils/banner.ts
+++ packages/nuxi/src/utils/banner.ts
@@ -1,8 +1,23 @@
+import { createRequire } from 'node:module'
 import clear from 'clear'
-import { green } from 'colorette'
+import { gray, green } from 'colorette'
 import { version } from '../../package.json'
 
 export function showBanner (_clear?: boolean) {
   if (_clear) { clear() }
-  console.log(green(`Nuxt CLI v${version}`))
+  console.log(green(`Nuxi ${version}`))
+}
+
+export function showVersions (cwd: string) {
+  const _require = createRequire(cwd)
+  const versions = []
+  for (const pkg of ['nuxt', 'nitropack', 'vite', 'webpack']) {
+    try {
+      const { version } = _require(`${pkg}/package.json`)
+      versions.push(pkg + '@' + version)
+    } catch {
+      // Not found
+    }
+  }
+  console.log(gray(versions.join(' ')))
 }
