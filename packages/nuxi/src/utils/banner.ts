9b9aaff95d22095e2b924e5a93e55dad2ff9ef36
Pooya Parsa <pooya@pi0.io>
2022-08-31 22:20:57 +0200

    make casing consistent

--- packages/nuxi/src/utils/banner.ts
+++ packages/nuxi/src/utils/banner.ts
@@ -5,7 +5,7 @@ import { version } from '../../package.json'
 
 export function showBanner (_clear?: boolean) {
   if (_clear) { clear() }
-  console.log(green(`Nuxi ${version}`))
+  console.log(green(`nuxi ${version}`))
 }
 
 export function showVersions (cwd: string) {
