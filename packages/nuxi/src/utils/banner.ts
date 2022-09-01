f281bbc6a81a1254c20d630b7df42296eb631a61
Pooya Parsa <pooya@pi0.io>
2022-09-01 10:18:08 +0200

    make it more readable

--- packages/nuxi/src/utils/banner.ts
+++ packages/nuxi/src/utils/banner.ts
@@ -5,23 +5,23 @@ import { version } from '../../package.json'
 
 export function showBanner (_clear?: boolean) {
   if (_clear) { clear() }
-  console.log(gray(`nuxi@${version}`))
+  console.log(gray(`Nuxi ${version}`))
 }
 
 export function showVersions (cwd: string) {
   const _require = createRequire(cwd)
-  const getPkgWithVersion = (pkg: string) => {
+  const getPkgWithVersion = (pkg: string, name: string) => {
     try {
       const { version } = _require(`${pkg}/package.json`)
       if (version) {
-        return pkg + '@' + version
+        return name + ' ' + version
       }
     } catch { /* not found */ }
     return pkg
   }
   console.log(
-    green(getPkgWithVersion('nuxt')),
+    green(getPkgWithVersion('nuxt', 'Nuxt')),
     gray('running with'),
-    gray(getPkgWithVersion('nitropack'))
+    gray(getPkgWithVersion('nitropack', 'Nitro'))
   )
 }
