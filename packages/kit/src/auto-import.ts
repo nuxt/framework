aaa9ff41cf13e68f33ceef2c8da2bb100c2c1d43
Pooya Parsa <pooya@pi0.io>
2022-09-02 13:00:27 +0200

    fix: improve `autoImports` deprecation dx

--- packages/kit/src/auto-import.ts
+++ packages/kit/src/auto-import.ts
@@ -2,20 +2,32 @@ import { Import } from 'unimport'
 import { useNuxt } from './context'
 import { assertNuxtCompatibility } from './compatibility'
 
-export function addAutoImport (imports: Import | Import[]) {
+export function addImports (imports: Import | Import[]) {
   assertNuxtCompatibility({ bridge: true })
 
-  useNuxt().hook('imports:extend', (_imports) => {
+  // TODO: Use imports:* when widely adopted
+  useNuxt().hook('autoImports:extend', (_imports) => {
     _imports.push(...(Array.isArray(imports) ? imports : [imports]))
-  })
+  }, { allowDeprecated: true })
 }
 
-export function addAutoImportDir (dirs: string | string[]) {
+/**
+ * @deprecated Please use `addImports` instead with nuxt>=3.0.0-rc.9
+ */
+export const addAutoImport = addImports
+
+export function addImportsDir (dirs: string | string[]) {
   assertNuxtCompatibility({ bridge: true })
 
-  useNuxt().hook('imports:dirs', (_dirs: string[]) => {
+  // TODO: Use imports:* when widely adopted
+  useNuxt().hook('autoImports:dirs', (_dirs: string[]) => {
     for (const dir of (Array.isArray(dirs) ? dirs : [dirs])) {
       _dirs.push(dir)
     }
-  })
+  }, { allowDeprecated: true })
 }
+
+/**
+ * @deprecated Please use `addImportsDir` instead with nuxt>=3.0.0-rc.9
+ */
+export const addAutoImportDir = addImportsDir
