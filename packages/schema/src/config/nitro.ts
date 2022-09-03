386908bcba33532d6e102a656164db30d51bcfd3
Tobias Diez <code@tobiasdiez.com>
2022-09-03 13:11:13 +0200

    docs: extend description of server handlers

--- packages/schema/src/config/nitro.ts
+++ packages/schema/src/config/nitro.ts
@@ -15,9 +15,23 @@ export default defineUntypedSchema({
   /**
    * Nitro server handlers.
    *
-   * @see https://nitro.unjs.io/guide/introduction/routing
+   * Each handler admits the following options:
+   * - handler: The path to the file defining the handler.
+   * - route: The route under which the handler is available. This follows the conventions of https://github.com/unjs/radix3.
+   * - method: The kind of requests (`GET`, `POST`, ...) that should be handled.
+   * - middleware: Specifies this is a middleware handler.
+   * - lazy: Specifies to use lazy loading to import the handler.
    *
-   * **Note:** Files from `server/api`, `server/middleware` and `server/routes` will be automatically registred by Nuxt.
+   * @see https://v3.nuxtjs.org/guide/features/server-routes
+   *
+   * @note Files from `server/api`, `server/middleware` and `server/routes` will be automatically registred by Nuxt.
+   *
+   * @example
+   * ```js
+   * serverHandlers: [
+   *   { route: '/path/foo/**:name', handler: '~/server/foohandler.ts' }
+   * ]
+   * ```
    *
    * @type {typeof import('nitropack')['NitroEventHandler'][]}
    * @version 3
