285307ff9ec031abac61bca6edafec6b26a7d424
Sébastien Chopin <seb@nuxtjs.com>
2022-09-05 15:10:40 +0200

    chore(example): add app-config example

---
+++ examples/app-config/app.vue
@@ -0,0 +1,13 @@
+<script setup>
+const config = useAppConfig()
+</script>
+
+<template>
+  <div>
+    <h1>{{ config.title }}</h1>
+    <p>{{ config.description }}</p>
+    <button v-if="config.showButton">
+      I am a button
+    </button>
+  </div>
+</template>
