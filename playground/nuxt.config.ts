3919b27b4569dfad50cc9db0e3cf91a66718eb3e
Pooya Parsa <pooya@pi0.io>
2022-09-15 14:49:25 +0200

    revert playground

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,11 +1,2 @@
-import { useNitro } from '@nuxt/kit'
-
 export default defineNuxtConfig({
-  modules: [
-    (_, nuxt) => {
-      nuxt.hook('ready', () => {
-        console.log(useNitro())
-      })
-    }
-  ]
 })
