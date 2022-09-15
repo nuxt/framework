19783fb977d2e01b232dc24095eb05f71908f519
Pooya Parsa <pooya@pi0.io>
2022-09-15 18:20:46 +0200

    avoid overriding state in plugin

--- packages/nuxt/src/app/plugins/payload.client.ts
+++ packages/nuxt/src/app/plugins/payload.client.ts
@@ -10,7 +10,6 @@ export default defineNuxtPlugin((nuxtApp) => {
     const payload = await loadPayload(url)
     if (!payload) { return }
     Object.assign(nuxtApp.payload.data, payload.data)
-    Object.assign(nuxtApp.payload.state, payload.state)
   }
   nuxtApp.hooks.hook('link:prefetch', async (to) => {
     await prefetchPayload(to)
