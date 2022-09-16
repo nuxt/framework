72e77f0b59bc541eb16716645c90518b599dab2d
Christian Preston <christianpreston@ymail.com>
2022-09-16 16:52:32 -0400

    feat: stub `build-module`

---
+++ packages/nuxi/src/commands/build-module.ts
@@ -0,0 +1,29 @@
+import { execa } from 'execa'
+import consola from 'consola'
+import { resolve } from 'pathe'
+import { getPkg } from '../utils/packageManagers'
+import { defineNuxtCommand } from './index'
+
+// Stub `build-module` until we have proper support for it
+// @see https://github.com/nuxt/module-builder/issues/32
+export default defineNuxtCommand({
+  meta: {
+    name: 'build-module',
+    usage: 'npx nuxi build-module [--stub] [rootDir]',
+    description: 'Helper command for using `@nuxt/module-builder`'
+  },
+  async invoke (args) {
+    // Execute `@nuxt/module-builder` locally if possible
+    const rootDir = resolve(args._[0] || '.')
+    const hasLocal = getPkg('@nuxt/module-builder', rootDir)
+    const execArgs = Object.entries({
+      '--stub': args.stub
+    }).filter(([, value]) => value).map(([key]) => key)
+
+    if (!hasLocal) {
+      return consola.error('Missing `@nuxt/module-builder` dependency, please install it first to use this command')
+    }
+
+    await execa('nuxt-module-build', execArgs, { preferLocal: true, stdio: 'inherit', cwd: rootDir })
+  }
+})
--- packages/nuxi/src/utils/packageManagers.ts
+++ packages/nuxi/src/utils/packageManagers.ts
@@ -1,5 +1,7 @@
 import { execSync } from 'node:child_process'
-import { existsSync } from 'node:fs'
+import { existsSync, readFileSync } from 'node:fs'
+import { createRequire } from 'node:module'
+import destr from 'destr'
 import { resolve } from 'pathe'
 import { findup } from './fs'
 
@@ -24,4 +26,50 @@ export function getPackageManager (rootDir: string) {
 
 export function getPackageManagerVersion (name: string) {
   return execSync(`${name} --version`).toString('utf8').trim()
+}
+
+/**
+ * Find package.json and read it
+ *
+ * @example
+ * ```ts
+ * findPackage('nuxt', '/path/to/project')
+ * ```
+ * @param name - package name
+ * @param rootDir - root directory
+ */
+export function getPkg (name: string, rootDir: string) {
+  // Assume it is in {rootDir}/node_modules/${name}/package.json
+  let pkgPath = resolve(rootDir, 'node_modules', name, 'package.json')
+
+  // Try to resolve for more accuracy
+  const _require = createRequire(rootDir)
+  try { pkgPath = _require.resolve(name + '/package.json') } catch (_err) {
+    // console.log('not found:', name)
+  }
+
+  return readJSONSync(pkgPath)
+}
+
+/**
+ * findPackage - Read package.json file
+ *
+ * @param rootDir - file path
+ */
+export function findPackage (rootDir: string) {
+  return findup(rootDir, (dir) => {
+    const p = resolve(dir, 'package.json')
+    if (existsSync(p)) {
+      return readJSONSync(p)
+    }
+  }) || {}
+}
+
+export function readJSONSync (filePath: string) {
+  try {
+    return destr(readFileSync(filePath, 'utf-8'))
+  } catch (err) {
+    // TODO: Warn error
+    return null
+  }
 }
