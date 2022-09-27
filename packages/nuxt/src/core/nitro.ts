35216505ae1e88f63cd781e4c9f07f684ca70f98
Daniel Roe <daniel@roe.dev>
2022-09-27 15:09:03 +0100

    fix: update scanDirs

--- packages/nuxt/src/core/nitro.ts
+++ packages/nuxt/src/core/nitro.ts
@@ -28,7 +28,7 @@ export async function initNitro (nuxt: Nuxt & { _nitro?: Nitro }) {
       projectRoot: nuxt.options.rootDir,
       filename: join(nuxt.options.rootDir, '.nuxt/stats', '{name}.html')
     },
-    scanDirs: nuxt.options._layers.map(layer => layer.config.srcDir).filter(Boolean).map(dir => join(dir!, 'server')),
+    scanDirs: nuxt.options._layers.map(layer => (layer.config.serverDir || layer.config.srcDir) && resolve(layer.cwd, layer.config.serverDir || resolve(layer.config.srcDir, 'server'))).filter(Boolean),
     renderer: resolve(distDir, 'core/runtime/nitro/renderer'),
     errorHandler: resolve(distDir, 'core/runtime/nitro/error'),
     nodeModulesDirs: nuxt.options.modulesDir,
