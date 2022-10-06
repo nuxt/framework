1f6fb14c40a2293fc730932e4b7ba4b4d187225e
Sacha STAFYNIAK <sacha@digisquad.io>
2022-10-06 12:51:53 +0200

    fix(dev-only): ensure content is not shown on build

--- packages/nuxt/src/app/components/dev-only.mjs
+++ packages/nuxt/src/app/components/dev-only.mjs
@@ -3,6 +3,9 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'DevOnly',
   setup (_, { slots }) {
-    return () => slots.default?.()
+    if (process.dev) {
+      return () => slots.default?.()
+    }
+    return () => null
   }
 })
