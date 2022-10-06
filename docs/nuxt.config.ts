b82efc7bdec03b755013a32c49fabd34a53b8dba
Daniel Roe <daniel@roe.dev>
2022-10-06 13:26:12 +0200

    chore: migrate docs to pnpm as well

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -2,6 +2,9 @@ import { join } from 'pathe'
 
 export default defineNuxtConfig({
   extends: process.env.WEBSITE_THEME ? join(process.env.WEBSITE_THEME, 'theme') : '@nuxt-themes/website',
+  build: {
+    transpile: [/content-edge/]
+  },
   nitro: {
     prerender: {
       routes: ['/', '/404.html', '/guide/directory-structure/app.config']
