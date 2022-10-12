e614a650a5f5400c14032cdbcdb0e1a3c3d07583
Sébastien Chopin <seb@nuxtjs.com>
2022-10-12 12:49:06 +0200

    docs: add app.config route to pre-render

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,3 +1,8 @@
 export default defineNuxtConfig({
-  extends: '@nuxt-themes/website'
+  extends: '@nuxt-themes/website',
+  nitro: {
+    prerender: {
+      routes: ['/guide/directory-structure/app.config']
+    }
+  }
 })
