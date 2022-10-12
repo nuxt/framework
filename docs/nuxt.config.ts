6a8f4aff7e59cfee22ba8701a6c21b8623e6748e
Daniel Roe <daniel@roe.dev>
2022-10-12 12:08:24 +0100

    docs: add `/` first

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
   extends: '@nuxt-themes/website',
   nitro: {
     prerender: {
-      routes: ['/404.html', '/guide/directory-structure/app.config']
+      routes: ['/', '/404.html', '/guide/directory-structure/app.config']
     }
   }
 })
