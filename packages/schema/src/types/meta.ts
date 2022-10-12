53d6656bec8420b4c21dbe6f1583903206a29929
Daniel Roe <daniel@roe.dev>
2022-10-12 10:16:47 +0100

    style: de-semify

--- packages/schema/src/types/meta.ts
+++ packages/schema/src/types/meta.ts
@@ -2,14 +2,14 @@ import type { HeadObjectPlain, HeadObject } from '@vueuse/head'
 
 export interface HeadAugmentations {
   // runtime type modifications
-  base?: {};
-  link?: {};
-  meta?: {};
-  style?: {};
-  script?: {};
-  noscript?: {};
-  htmlAttrs?: {};
-  bodyAttrs?: {};
+  base?: {}
+  link?: {}
+  meta?: {}
+  style?: {}
+  script?: {}
+  noscript?: {}
+  htmlAttrs?: {}
+  bodyAttrs?: {}
 }
 
 export type MetaObjectRaw = HeadObjectPlain<HeadAugmentations>
@@ -37,18 +37,18 @@ export interface MetaObject {
    *
    * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/title
    */
-  title?: HeadObject<HeadAugmentations>['title'];
+  title?: HeadObject<HeadAugmentations>['title']
   /**
    * Generate the title from a template.
    */
-  titleTemplate?: HeadObject<HeadAugmentations>['titleTemplate'];
+  titleTemplate?: HeadObject<HeadAugmentations>['titleTemplate']
   /**
    * The <base> HTML element specifies the base URL to use for all relative URLs in a document.
    * There can be only one <base> element in a document.
    *
    * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/base
    */
-  base?: HeadObject<HeadAugmentations>['base'];
+  base?: HeadObject<HeadAugmentations>['base']
   /**
    * The <link> HTML element specifies relationships between the current document and an external resource.
    * This element is most commonly used to link to stylesheets, but is also used to establish site icons
@@ -56,43 +56,43 @@ export interface MetaObject {
    *
    * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/link#attr-as
    */
-  link?: HeadObject<HeadAugmentations>['link'];
+  link?: HeadObject<HeadAugmentations>['link']
   /**
    * The <meta> element represents metadata that cannot be expressed in other HTML elements, like <link> or <script>.
    *
    * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/meta
    */
-  meta?: HeadObject<HeadAugmentations>['meta'];
+  meta?: HeadObject<HeadAugmentations>['meta']
   /**
    * The <style> HTML element contains style information for a document, or part of a document.
    * It contains CSS, which is applied to the contents of the document containing the <style> element.
    *
    * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/style
    */
-  style?: HeadObject<HeadAugmentations>['style'];
+  style?: HeadObject<HeadAugmentations>['style']
   /**
    * The <script> HTML element is used to embed executable code or data; this is typically used to embed or refer to JavaScript code.
    *
    * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script
    */
-  script?: HeadObject<HeadAugmentations>['script'];
+  script?: HeadObject<HeadAugmentations>['script']
   /**
    * The <noscript> HTML element defines a section of HTML to be inserted if a script type on the page is unsupported
    * or if scripting is currently turned off in the browser.
    *
    * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/noscript
    */
-  noscript?: HeadObject<HeadAugmentations>['noscript'];
+  noscript?: HeadObject<HeadAugmentations>['noscript']
   /**
    * Attributes for the <html> HTML element.
    *
    * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/html
    */
-  htmlAttrs?: HeadObject<HeadAugmentations>['htmlAttrs'];
+  htmlAttrs?: HeadObject<HeadAugmentations>['htmlAttrs']
   /**
    * Attributes for the <body> HTML element.
    *
    * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/body
    */
-  bodyAttrs?: HeadObject<HeadAugmentations>['bodyAttrs'];
+  bodyAttrs?: HeadObject<HeadAugmentations>['bodyAttrs']
 }
