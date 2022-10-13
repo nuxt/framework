52ad910abeb5e36e078da025f3c47deb4b0ee873
Harlan Wilton <harlan@harlanzw.com>
2022-10-14 01:05:54 +1100

    fix(nuxt): on-demand head components

--- packages/nuxt/src/head/module.ts
+++ packages/nuxt/src/head/module.ts
@@ -15,8 +15,36 @@ export default defineNuxtModule({
     // Add #head alias
     nuxt.options.alias['#head'] = runtimeDir
 
-    // Add generic plugin
-    addPlugin({ src: resolve(runtimeDir, 'plugin') })
+    const componentsPath = resolve(runtimeDir, 'components')
+    const headComponents = [
+      'Script',
+      'NoScript',
+      'Link',
+      'Base',
+      'Title',
+      'Meta',
+      'Style',
+      'Head',
+      'Html',
+      'Body'
+    ].map(c => ({
+      // kebab case version of these tags is not valid
+      kebabName: c,
+      pascalName: c,
+      export: c,
+      preload: false,
+      prefetch: false,
+      filePath: componentsPath,
+      import: `require(${JSON.stringify(componentsPath)})['${c}']`,
+      chunkName: `components/${c}`,
+      shortPath: componentsPath
+    }))
+    nuxt.hook('components:extend', (components) => {
+      components.push(...headComponents)
+    })
+
+    // Add mixin plugin
+    addPlugin({ src: resolve(runtimeDir, 'mixin-plugin') })
 
     // Add library specific plugin
     addPlugin({ src: resolve(runtimeDir, 'lib/vueuse-head.plugin') })
