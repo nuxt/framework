2bf1e245454cb119e3ddf001bec0cb6bda16d8ee
Daniel Roe <daniel@roe.dev>
2022-10-14 11:41:57 +0100

    fix: remove duplicate nitro plugin

--- packages/nuxt/src/core/runtime/nitro/plugins/debug.ts
+++
@@ -1,8 +0,0 @@
-import { createDebugger } from 'hookable'
-import type { NitroAppPlugin } from 'nitropack'
-
-export default <NitroAppPlugin> function (nitro) {
-  createDebugger(nitro.hooks, {
-    tag: 'nitro app'
-  })
-}
