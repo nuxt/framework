4489c355e313988b67ce9eaab707a4f61f7aa1ab
Pooya Parsa <pooya@pi0.io>
2022-10-16 11:38:13 +0200

    fallback to npx if not locally found

--- packages/nuxi/src/commands/build-module.ts
+++ packages/nuxi/src/commands/build-module.ts
@@ -4,27 +4,30 @@ import { resolve } from 'pathe'
 import { tryResolveModule } from '../utils/cjs'
 import { defineNuxtCommand } from './index'
 
-// Stub `build-module` until we have proper support for it
-// @see https://github.com/nuxt/module-builder/issues/32
+const MODULE_BUILDER_PKG = '@nuxt/module-builder'
+
 export default defineNuxtCommand({
   meta: {
     name: 'build-module',
     usage: 'npx nuxi build-module [--stub] [rootDir]',
-    description: 'Helper command for using `@nuxt/module-builder`'
+    description: `Helper command for using ${MODULE_BUILDER_PKG}`
   },
   async invoke (args) {
-    // Execute `@nuxt/module-builder` locally if possible
+    // Find local installed version
     const rootDir = resolve(args._[0] || '.')
-    const hasLocal = tryResolveModule('@nuxt/module-builder/package.json', rootDir)
+    const hasLocal = tryResolveModule(`${MODULE_BUILDER_PKG}/package.json`, rootDir)
 
     const execArgs = Object.entries({
       '--stub': args.stub
     }).filter(([, value]) => value).map(([key]) => key)
 
+    let cmd = 'nuxt-module-build'
     if (!hasLocal) {
-      return consola.error('Missing `@nuxt/module-builder` dependency, please install it first to use this command')
+      consola.warn(`Cannot find locally installed version of \`${MODULE_BUILDER_PKG}\` (>=0.2.0). Falling back to \`npx ${MODULE_BUILDER_PKG}\``)
+      cmd = 'npx'
+      execArgs.unshift(MODULE_BUILDER_PKG)
     }
 
-    await execa('nuxt-module-build', execArgs, { preferLocal: true, stdio: 'inherit', cwd: rootDir })
+    await execa(cmd, execArgs, { preferLocal: true, stdio: 'inherit', cwd: rootDir })
   }
 })
