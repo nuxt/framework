f16ae77e14b7b51b70736318df5c7da7c73a4aea
Daniel Roe <daniel@roe.dev>
2022-10-17 10:01:41 +0100

    refactor(nuxt): use h3 early hints util

--- packages/nuxt/src/core/runtime/nitro/renderer.ts
+++ packages/nuxt/src/core/runtime/nitro/renderer.ts
@@ -1,7 +1,7 @@
 import { createRenderer, renderResourceHeaders } from 'vue-bundle-renderer/runtime'
 import type { RenderResponse } from 'nitropack'
 import type { Manifest } from 'vite'
-import { appendHeader, getQuery } from 'h3'
+import { appendHeader, getQuery, writeEarlyHints } from 'h3'
 import devalue from '@nuxt/devalue'
 import { joinURL } from 'ufo'
 import { renderToString as _renderToString } from 'vue/server-renderer'
@@ -160,7 +160,7 @@ export default defineRenderHandler(async (event) => {
   if (!isRenderingPayload && !process.env.prerender && event.res.socket) {
     const { link } = renderResourceHeaders({}, renderer.rendererContext)
     // TODO: use https://github.com/nodejs/node/pull/44180 when we drop support for node 16
-    event.res.socket!.write(`HTTP/1.1 103 Early Hints\r\nLink: ${link}\r\n\r\n`, 'utf-8')
+    writeEarlyHints(event, link)
   }
 
   const _rendered = await renderer.renderToString(ssrContext).catch((err) => {
