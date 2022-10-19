44418ddb9a3a5fe2df0f8bf308aa414c6d4f5aaf
Pooya Parsa <pooya@pi0.io>
2022-10-19 14:06:48 +0200

    refactor and improvements for default scroll behavior

--- packages/nuxt/src/pages/routing/default-router.options.ts
+++ packages/nuxt/src/pages/routing/default-router.options.ts
@@ -1,60 +1,60 @@
 import type { RouterConfig } from '@nuxt/schema'
+import type { RouterScrollBehavior } from 'vue-router'
 import { nextTick } from 'vue'
 import { useNuxtApp } from '#app'
+
+type ScrollPosition = Exclude<Awaited<ReturnType<RouterScrollBehavior>>, false|void>
 
 // https://router.vuejs.org/api/#routeroptions
 export default <RouterConfig>{
   scrollBehavior (to, from, savedPosition) {
     const nuxtApp = useNuxtApp()
-    // If the returned position is falsy or an empty object, will retain current scroll position
-    let position
-    const isRouteChanged = to !== from
 
-    // savedPosition is only available for popstate navigations (back button)
+    // By default when the returned position is falsy or an empty object, vue-router will retain the current scroll position
+    let position: ScrollPosition
+
+    // SavedPosition is only available for popstate navigations (back button)
     if (savedPosition) {
       position = savedPosition
-    } else if (isRouteChanged) {
+    } else if (to !== from /* Route changed */) {
       position = { left: 0, top: 0 }
     }
 
-    // hash routes on the same page, no page hook is fired so resolve here
-    if (to.path === from.path) {
+    // Hash routes on the same page, no page hook is fired so resolve here
+    if (to.path !== from.path) {
       if (from.hash && !to.hash) {
         return { left: 0, top: 0 }
-      } else if (to.hash) {
+      }
+      if (to.hash) {
         return {
           el: to.hash,
-          top: getHashElementScrollMarginTop()
+          top: _getHashElementScrollMarginTop(to.hash)
         }
       }
     }
 
-    // if either to or from has no transition then wait for page:finish
+    // Wait for page:transition:finish or page:finish depending on transitions enabled or not
     const hasTransition = to.meta.pageTransition !== false && from.meta.pageTransition !== false
-    const hookAwait = hasTransition ? 'page:transition:finish' : 'page:finish'
-
-    function getHashElementScrollMarginTop () {
-      // vue-router does not incorporate scroll-margin-top on its own.
-      const elem = document.querySelector(to.hash)
-
-      if (elem) {
-        return parseFloat(getComputedStyle(elem).scrollMarginTop)
-      }
-      return 0
-    }
-
+    const hookToWait = hasTransition ? 'page:transition:finish' : 'page:finish'
     return new Promise((resolve) => {
-      nuxtApp.hooks.hookOnce(hookAwait, async () => {
+      nuxtApp.hooks.hookOnce(hookToWait, async () => {
         await nextTick()
-
         if (to.hash) {
           position = {
             el: to.hash,
-            top: getHashElementScrollMarginTop()
+            top: _getHashElementScrollMarginTop(to.hash)
           }
         }
         resolve(position)
       })
     })
   }
+}
+
+function _getHashElementScrollMarginTop (selector: string): number {
+  const elem = document.querySelector(selector)
+  if (elem) {
+    return parseFloat(getComputedStyle(elem).scrollMarginTop)
+  }
+  return 0
 }
