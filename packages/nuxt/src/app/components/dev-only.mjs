e92889b026a6e0f897e146e0d64441ec8fd26f25
Pooya Parsa <pooya@pi0.io>
2022-10-26 14:35:15 +0200

    small changes

--- packages/nuxt/src/app/components/dev-only.mjs
+++ packages/nuxt/src/app/components/dev-only.mjs
@@ -2,9 +2,9 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'DevOnly',
-  setup (_, { slots }) {
+  setup (_, props) {
     if (process.dev) {
-      return () => slots.default?.()
+      return () => props.slots.default?.()
     }
     return () => null
   }
--- playground/app.vue
+++ playground/app.vue
@@ -5,8 +5,6 @@
   <!-- Edit this file to play around with Nuxt but never commit changes! -->
   <div>
     Nuxt 3 Playground
-
-    <DevOnly>Development mode</DevOnly>
   </div>
 </template>
 
