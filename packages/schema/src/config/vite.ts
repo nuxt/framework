bf0013e23a7be7534642d4c0306fe4912de8eb2c
Pooya Parsa <pooya@pi0.io>
2022-11-03 22:52:38 +0100

    add workspaceDir too

--- packages/schema/src/config/vite.ts
+++ packages/schema/src/config/vite.ts
@@ -69,6 +69,7 @@ export default defineUntypedSchema({
             await get('buildDir'),
             await get('srcDir'),
             await get('rootDir'),
+            await get('workspaceDir'),
             ...(await get('modulesDir')),
             ...val ?? []
           ]
