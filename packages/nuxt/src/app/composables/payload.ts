bbdf9c32a453a5bb7b47e9d598a1944d526872de
Pooya Parsa <pooya@pi0.io>
2022-11-03 23:16:20 +0100

    fix(nuxt): ensure payload url has no protocol

--- packages/nuxt/src/app/composables/payload.ts
+++ packages/nuxt/src/app/composables/payload.ts
@@ -37,12 +37,12 @@ export function preloadPayload (url: string, opts: LoadPayloadOptions = {}) {
 // --- Internal ---
 
 function _getPayloadURL (url: string, opts: LoadPayloadOptions = {}) {
-  const parsed = parseURL(url)
-  if (parsed.search) {
+  const u = new URL(url, 'http://localhost')
+  if (u.search) {
     throw new Error('Payload URL cannot contain search params: ' + url)
   }
   const hash = opts.hash || (opts.fresh ? Date.now() : '')
-  return joinURL(useRuntimeConfig().app.baseURL, parsed.pathname, hash ? `_payload.${hash}.js` : '_payload.js')
+  return joinURL(useRuntimeConfig().app.baseURL, u.pathname, hash ? `_payload.${hash}.js` : '_payload.js')
 }
 
 async function _importPayload (payloadURL: string) {
