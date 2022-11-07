e855ecdff115d74238c48dedc55d556812926239
julien huang <julien.huang@outlook.fr>
2022-11-08 00:35:07 +0100

    feat(nuxt): allow fallback-placeholder slot for ServerPlaceholder

--- packages/nuxt/src/app/components/server-placeholder.ts
+++ packages/nuxt/src/app/components/server-placeholder.ts
@@ -2,7 +2,9 @@ import { defineComponent, createElementBlock } from 'vue'
 
 export default defineComponent({
   name: 'ServerPlaceholder',
-  render () {
+  render (ctx: any) {
+    const slot = ctx.$slots.fallback || ctx.$slots.placeholder
+    if (slot) { return slot() }
     return createElementBlock('div')
   }
 })
