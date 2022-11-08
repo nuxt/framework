1fb6ec26f6061b84e0f6f7f82785b0abb09a290e
Daniel Roe <daniel@roe.dev>
2022-11-08 13:12:01 +0100

    fix: lowercase type output

--- packages/nuxt/src/app/composables/ssr.ts
+++ packages/nuxt/src/app/composables/ssr.ts
@@ -2,7 +2,7 @@
 import type { H3Event } from 'h3'
 import { useNuxtApp, NuxtApp } from '../nuxt'
 
-export function useRequestHeaders<K extends string = string> (include: K[]): Record<K, string | undefined>
+export function useRequestHeaders<K extends string = string> (include: K[]): Record<Lowercase<K>, string | undefined>
 export function useRequestHeaders (): Readonly<Record<string, string | undefined>>
 export function useRequestHeaders (include?: any[]) {
   if (process.client) { return {} }
