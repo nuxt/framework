71a4a2b6bfe839f040d3b66ebce72706de9e0e68
Harlan Wilton <harlan@harlanzw.com>
2022-11-11 20:35:14 +1100

    chore: fix PR artifact

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,19 +1,3 @@
 export default defineNuxtConfig({
-  app: {
-    head: {
-      title: 'nuxt',
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { key: 'description', name: 'description', content: '' }
-      ],
-      script: [
-        {
-          src: 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js',
-          body: true
-        }
-      ],
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
-    }
-  }
+
 })
