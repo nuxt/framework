60375691776bc1e76f499802fcebbcf10633e749
Sébastien Chopin <seb@nuxtjs.com>
2022-11-15 14:05:34 +0100

    chore: update deps

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -2,6 +2,9 @@ import { withTrailingSlash } from 'ufo'
 
 export default defineNuxtConfig({
   extends: process.env.WEBSITE_THEME ? withTrailingSlash(process.env.WEBSITE_THEME) : 'nuxt-website-theme',
+  build: {
+    transpile: [/nuxt-website-theme/]
+  },
   nitro: {
     esbuild: {
       options: {
