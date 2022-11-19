51e7cf45e2bbfd7204abde9f29b52bd5ddfb6064
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-11-19 08:24:35 +0330

    test: fix computed layout test

--- test/fixtures/basic/pages/with-computed-layout.vue
+++ test/fixtures/basic/pages/with-computed-layout.vue
@@ -1,15 +1,11 @@
 <script setup>
-const test = ref('custom')
 definePageMeta({
-  layout: computed(() => test.value)
+  layout: computed(() => 'custom')
 })
 </script>
 
 <template>
   <div>
     <div>with-computed-layout.vue</div>
-    <button @click="layout = layout ? null : 'custom'">
-      Switch layout
-    </button>
   </div>
 </template>
