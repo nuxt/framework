ed9b7ea553f34899dd827d678817b7d843cefbd0
Mahdi Boomeri <mahdi.b2or@gmail.com>
2022-11-22 16:56:51 +0330

    feat: `useNuxtData` composable

--- packages/nuxt/src/app/composables/asyncData.ts
+++ packages/nuxt/src/app/composables/asyncData.ts
@@ -268,6 +268,11 @@ export function useLazyAsyncData<
   return useAsyncData(key, handler, { ...options, lazy: true }, null)
 }
 
+export function useNuxtData (key?: string) {
+  const nuxt = useNuxtApp()
+  return key ? nuxt.payload.data[key] : nuxt.payload.data
+}
+
 export async function refreshNuxtData (keys?: string | string[]): Promise<void> {
   if (process.server) {
     return Promise.resolve()
