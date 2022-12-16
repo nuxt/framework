e2d27aeccac1610721afa497e706a9cd73e94a58
Anthony Fu <anthonyfu117@hotmail.com>
2022-12-16 11:38:05 +0100

    feat: display building info on build

--- packages/vite/src/client.ts
+++ packages/vite/src/client.ts
@@ -150,7 +150,9 @@ export async function buildClient (ctx: ViteBuildContext) {
   } else {
     // Build
     const start = Date.now()
+    logger.restoreAll()
     await vite.build(clientConfig)
+    logger.wrapAll()
     await ctx.nuxt.callHook('vite:compiled')
     logger.info(`Client built in ${Date.now() - start}ms`)
   }
