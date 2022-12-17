7b34b13e36eab9c568798937e85767281bf78b4e
Mehdi HosseinZade <arshen@outlook.com>
2022-12-17 13:20:49 +0000

    feat(kit): add extendRouteRules

--- packages/kit/src/pages.ts
+++ packages/kit/src/pages.ts
@@ -1,4 +1,5 @@
 import type { NuxtHooks } from '@nuxt/schema'
+import { NitroRouteConfig } from 'nitropack'
 import { useNuxt } from './context'
 import { isNuxt2 } from './compatibility'
 
@@ -10,4 +11,16 @@ export function extendPages (cb: NuxtHooks['pages:extend']) {
   } else {
     nuxt.hook('pages:extend', cb)
   }
+}
+
+export function extendRouteRules (route: string, rule: NitroRouteConfig) {
+  const nuxt = useNuxt()
+  if (!nuxt.options.routeRules) {
+    nuxt.options.routeRules = {}
+  }
+  nuxt.options.routeRules[route] = rule
+  if (!nuxt.options.nitro.routeRules) {
+    nuxt.options.nitro.routeRules = {}
+  }
+  nuxt.options.nitro.routeRules[route] = rule
 }
