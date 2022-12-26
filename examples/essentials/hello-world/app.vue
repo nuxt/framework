88523484beb5d1973002d8b180e136f8a0f973fa
Anthony Jekanyika <anthonyjek1950@gmail.com>
2022-12-26 21:54:58 +0200

    Added lang="ts" so that it can run on Typescript

--- examples/essentials/hello-world/app.vue
+++ examples/essentials/hello-world/app.vue
@@ -1,5 +1,5 @@
-<script setup>
-const version = 2 + 1
+<script setup lang="ts">
+  const version: number = 2 + 1
 </script>
 
 <template>
@@ -9,9 +9,9 @@ const version = 2 + 1
 </template>
 
 <style scoped>
-.hello {
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 3rem;
-  padding: 10rem;
-}
+  .hello {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 3rem;
+    padding: 10rem;
+  }
 </style>
