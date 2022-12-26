5e18db95fd40b01930d5e2676b6021dc20a9400d
Anthony Jekanyika <anthonyjek1950@gmail.com>
2022-12-26 21:53:47 +0200

    Expanded code to make it easier for beginners to understand

--- examples/composables/use-fetch/server/api/[...hello].ts
+++ examples/composables/use-fetch/server/api/[...hello].ts
@@ -1,4 +1,13 @@
-export default defineEventHandler(event => ({
-  path: '/api/' + event.context.params.hello,
-  query: getQuery(event)
-}))
+export default defineEventHandler(event => {
+  const { hello } = event.context.params;
+  const query = getQuery(event);
+
+  return {
+    path: '/api/' + hello,
+    query
+  }
+})
+
+/*
+  Do not forget that with Sugar Syntax the object { "query": query } is the same as the object { query }
+*/
