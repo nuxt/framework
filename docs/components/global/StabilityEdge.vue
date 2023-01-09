473b2d5d74ccba13e77cabfaa040df7b493ec297
Daniel Roe <daniel@roe.dev>
2023-01-09 10:08:04 +0000

    docs: temporarily add stabilityedge component back pending upstream

---
+++ docs/components/global/StabilityEdge.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  title: { type: String, default: 'This feature' }
+})
+</script>
+
+<template>
+  <Alert icon="🧪">
+    {{ title }} is available on edge channel. Check out the
+    <Link to="/docs/guide/going-further/edge-channel/">
+      Edge Channel Documentation
+    </Link> to beta test.
+    <slot />
+  </Alert>
+</template>
