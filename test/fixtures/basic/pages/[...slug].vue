396540f6759598d702573f90a3167a4f78fe9491
Daniel Roe <daniel@roe.dev>
2023-01-14 00:15:48 +0000

    test: fix type

--- test/fixtures/basic/pages/[...slug].vue
+++ test/fixtures/basic/pages/[...slug].vue
@@ -2,7 +2,7 @@
   <div>
     <div>[...slug].vue</div>
     <div>404 at {{ $route.params.slug[0] }}</div>
-    <div>Middleware ran: {{ !!$route.meta.override?.includes('extended middleware') }}</div>
+    <div>Middleware ran: {{ !!($route.meta.override as any)?.includes('extended middleware') }}</div>
   </div>
 </template>
 
