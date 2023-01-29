fa67593b533175a280a5c25e539450f98eb357ab
julien huang <julien.huang@outlook.fr>
2023-01-30 00:08:53 +0100

    test(nuxt): retrieve fixtures test from nuxt-runtime-compiler

---
+++ test/fixtures/runtime-compiler/nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  vue: {
+    runtimeCompiler: true
+  },
+  experimental: {
+    externalVue: false
+  }
+})
