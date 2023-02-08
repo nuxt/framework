2ecfce302a32f3e45e0d2c4277b453abfb3b2469
Julien Huang <julien.huang@outlook.fr>
2023-02-08 22:30:38 +0100

    Apply suggestions from code review
    Co-authored-by: Daniel Roe <daniel@roe.dev>

--- packages/nuxt/src/components/client-fallback-auto-id.ts
+++ packages/nuxt/src/components/client-fallback-auto-id.ts
@@ -9,7 +9,8 @@ interface LoaderOptions {
   transform?: ComponentsOptions['transform'],
   rootDir: string
 }
-
+const CLIENT_FALLBACK_RE = /<(ClientFallback|client-fallback)( [^>]*)?>/
+const CLIENT_FALLBACK_GLOBAL_RE = /<(ClientFallback|client-fallback)( [^>]*)?>/g
 export const clientFallbackAutoIdPlugin = createUnplugin((options: LoaderOptions) => {
   const exclude = options.transform?.exclude || []
   const include = options.transform?.include || []
@@ -27,13 +28,13 @@ export const clientFallbackAutoIdPlugin = createUnplugin((options: LoaderOptions
       return isVueTemplate(id)
     },
     transform (code, id) {
-      if (!/[cC]lient-?[fF]allback/.test(code)) { return }
+      if (!CLIENT_FALLBACK_RE.test(code)) { return }
 
       const s = new MagicString(code)
       const relativeID = isAbsolute(id) ? relative(options.rootDir, id) : id
       let count = 0
 
-      s.replace(/<([cC]lient-?[fF]allback)( [^>]*)?>/g, (full, name, attrs) => {
+      s.replace(CLIENT_FALLBACK_GLOBAL_RE, (full, name, attrs) => {
         count++
         if (/ :?uid=/g.test(attrs)) { return full }
         return `<${name} :uid="'${hash(relativeID)}' + JSON.stringify($props) + '${count}'"  ${attrs ?? ''}>`
